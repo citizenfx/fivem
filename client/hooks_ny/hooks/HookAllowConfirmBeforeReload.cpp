b42af04f04bdcb8d33d0da15c4a61dc1ba266c1e
NTAuthority <bas@fourdeltaone.net>
2014-06-07 16:25:39 +0200

    fix not being able to press enter on certain loading screen messages
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=eae60ea85250d11a0470f6102f1eaf6a491a0db4

---
+++ client/hooks_ny/hooks/HookAllowConfirmBeforeReload.cpp
@@ -0,0 +1,29 @@
+// allows confirming a 'SET_MSG_FOR_LOADING_SCREEN' even if the game isn't set to reload
+#include "StdInc.h"
+#include "HookCallbacks.h"
+
+static bool& drawingLoadMsg = *(bool*)0x11DE801;
+
+static bool WRAPPER ConfirmMenuResult(int a1, int a2, int a3, int a4, int a5, int a6, int a7) { EAXJMP(0x788F30); }
+
+static HookFunction hookFunction([] ()
+{
+	static hook::inject_call<void, int> injectCall(0x4212F0);
+
+	injectCall.inject([] (int)
+	{
+		if (drawingLoadMsg)
+		{
+			if (ConfirmMenuResult(8, 1, 2, 1, 0, 0, 0))
+			{
+				HookCallbacks::RunCallback(StringHash("msgConfirm"), nullptr);
+
+				drawingLoadMsg = false;
+			}
+		}
+		else
+		{
+			injectCall.call();
+		}
+	});
+});
