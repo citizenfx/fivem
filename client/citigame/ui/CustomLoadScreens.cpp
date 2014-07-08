c264e3a1491d618b97031d54a21f07c00fe4c723
NTAuthority <bas@fourdeltaone.net>
2014-07-08 22:40:10 +0200

    fix another crash related to loading screen cleanup
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=19478efb77141f65323a3074734237f85c2f21dd

--- client/citigame/ui/CustomLoadScreens.cpp
+++ client/citigame/ui/CustomLoadScreens.cpp
@@ -3,8 +3,10 @@
 #include "CustomLoadScreens.h"
 #include "CrossLibraryInterfaces.h"
 #include <GameInit.h>
+#include <mutex>
 
 static std::shared_ptr<NUIWindow> g_loadScreen;
+static std::mutex g_loadScreenMutex;
 
 void CustomLoadScreens::PrepareSwitchTo(std::string url)
 {
@@ -22,17 +24,25 @@ static InitFunction initFunction([] ()
 {
 	g_hooksDLL->SetHookCallback(StringHash("loadsFrame"), [] (void*)
 	{
+		g_loadScreenMutex.lock();
+
 		if (g_loadScreen.get())
 		{
 			g_loadScreen->UpdateFrame();
 		}
+
+		g_loadScreenMutex.unlock();
 	});
 
 	g_hooksDLL->SetHookCallback(StringHash("loadsClean"), [] (void*)
 	{
+		g_loadScreenMutex.lock();
+
 		if (g_loadScreen.get())
 		{
 			g_loadScreen = nullptr;
 		}
+
+		g_loadScreenMutex.unlock();
 	});
 });
