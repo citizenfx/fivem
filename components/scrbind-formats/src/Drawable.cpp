0fda813ea291603817bab489bbc49ef7d42fce78
NTAuthority <bas@dotbas.net>
2015-01-02 22:00:00 +0100

    some more 'progress' on scrbind-formats
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=d0fe75f54781c1adcef55024b4f7901f09f4b0a0

--- components/scrbind-formats/src/Drawable.cpp
+++ components/scrbind-formats/src/Drawable.cpp
@@ -7,6 +7,7 @@
 
 #include "StdInc.h"
 #include "scrBind.h"
+#include <memory>
 
 // setup gtaDrawable format
 #if defined(GTA_NY)
@@ -17,6 +18,156 @@
 
 using namespace rage::RAGE_FORMATS_GAME;
 
+class ScriptShader : public fwRefCountable
+{
+
+};
+
+class ScriptGeometry : public fwRefCountable
+{
+private:
+	fwRefContainer<ScriptShader> m_shader;
+
+	uint16_t* m_indices;
+
+	uint32_t m_numIndices;
+
+	void* m_vertices;
+
+	uint32_t m_vertexStride;
+
+	uint32_t m_vertexCount;
+
+	uint32_t m_vertexUsageMask;
+
+	uint64_t m_vertexTypeMask;
+
+public:
+	ScriptGeometry();
+
+	~ScriptGeometry();
+
+	inline void SetShader(ScriptShader* shader)
+	{
+		if (!scrBindIsSafePointer(shader))
+		{
+			return;
+		}
+
+		m_shader = shader;
+	}
+
+	inline void* AllocateIndices(uint32_t size)
+	{
+		if (m_indices)
+		{
+			delete[] m_indices;
+		}
+
+		m_indices = new uint16_t[size];
+
+		scrBindAddSafePointer(m_indices);
+
+		return m_indices;
+	}
+
+	inline void* AllocateVertices(uint32_t count, uint32_t stride)
+	{
+		if (m_vertices)
+		{
+			delete[] m_vertices;
+		}
+
+		m_vertices = new char[count * stride];
+
+		scrBindAddSafePointer(m_vertices);
+
+		return m_vertices;
+	}
+
+	inline void SetVertexFormat(uint32_t usageMask, uint32_t typeMaskLow, uint32_t typeMaskHigh)
+	{
+		m_vertexTypeMask = ((uint64_t)typeMaskHigh << 32) | typeMaskLow;
+		m_vertexUsageMask = usageMask;
+	}
+
+	inline bool IsValid()
+	{
+		if (!m_shader.GetRef())
+		{
+			return false;
+		}
+
+		if (!m_indices)
+		{
+			return false;
+		}
+
+		if (!m_vertices)
+		{
+			return false;
+		}
+
+		return true;
+	}
+};
+
+class ScriptModel : public fwRefCountable
+{
+private:
+	Vector4 m_geometryBounds;
+
+	std::vector<fwRefContainer<ScriptGeometry>> m_geometries;
+
+	uint32_t m_setFlags;
+
+#if 0
+	Vector4 m_aabbMin;
+	Vector4 m_aabbMax;
+#endif
+
+public:
+	ScriptModel();
+
+	inline void SetBounds(float x, float y, float z, float radius)
+	{
+		m_geometryBounds = Vector4(x, y, z, radius);
+		m_setFlags |= 1;
+	}
+
+	inline ScriptGeometry* AddGeometry()
+	{
+		fwRefContainer<ScriptGeometry> geometry = new ScriptGeometry();
+		m_geometries.push_back(geometry);
+
+		// return pointer
+		scrBindAddSafePointer(geometry.GetRef());
+
+		return geometry.GetRef();
+	}
+
+	inline bool IsValid()
+	{
+		if ((m_setFlags & 1) != 1)
+		{
+			return false;
+		}
+
+		if (m_geometries.size() == 0)
+		{
+			return false;
+		}
+
+		return true;
+	}
+};
+
+ScriptModel::ScriptModel()
+	: m_setFlags(0)
+{
+
+}
+
 class ScriptLodGroup
 {
 private:
@@ -24,6 +175,8 @@ private:
 	Vector3 m_maxBound;
 	Vector3 m_center;
 	float m_radius;
+
+	fwRefContainer<ScriptModel> m_models[4];
 
 	uint32_t m_setFlags;
 
@@ -52,6 +205,23 @@ public:
 	{
 		m_radius = radius;
 		m_setFlags |= 8;
+	}
+
+	inline ScriptModel* CreateModel(int lod)
+	{
+		if (lod < 0 || lod >= _countof(m_models))
+		{
+			return nullptr;
+		}
+
+		m_models[lod] = new ScriptModel();
+
+		// return the pointer
+		ScriptModel* model = m_models[lod].GetRef();
+
+		scrBindAddSafePointer(model);
+
+		return model;
 	}
 
 	inline bool IsValid()
@@ -108,7 +278,8 @@ static InitFunction initFunction([] ()
 		.AddMethod("SET_LODGROUP_BOUNDS_MIN", &ScriptLodGroup::SetMinBound)
 		.AddMethod("SET_LODGROUP_BOUNDS_MAX", &ScriptLodGroup::SetMaxBound)
 		.AddMethod("SET_LODGROUP_CENTER", &ScriptLodGroup::SetCenter)
-		.AddMethod("SET_LODGROUP_RADIUS", &ScriptLodGroup::SetRadius);
+		.AddMethod("SET_LODGROUP_RADIUS", &ScriptLodGroup::SetRadius)
+		.AddMethod("CREATE_LODGROUP_MODEL", &ScriptLodGroup::CreateModel);
 
 	/*scrEngine::OnScriptInit.Connect([] ()
 	{
