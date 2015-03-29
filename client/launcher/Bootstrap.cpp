73a6631944f0fa1bf6e6c47c225d06256947327d
NTAuthority <bas@dotbas.net>
2015-03-29 01:00:54 +0100

    attempt to reduce antimalware detections #1
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=56030c97cc3b73c85c153fa25d73ffa9cee8f958

--- client/launcher/Bootstrap.cpp
+++ client/launcher/Bootstrap.cpp
@@ -17,7 +17,9 @@
 
 bool Bootstrap_UpdateEXE(int exeSize)
 {
-	const char* fn = _tempnam(NULL, "cup");
+	_unlink("CitizenFX.exe.new");
+
+	const char* fn = "CitizenFX.exe.new";
 	CL_QueueDownload(va(CONTENT_URL "/%s/bootstrap/CitizenFX.exe.xz", GetUpdateChannel()), fn, exeSize, true);
 
 	UI_DoCreation();
@@ -33,39 +35,6 @@ bool Bootstrap_UpdateEXE(int exeSize)
 	// verify the signature on the EXE
 	wchar_t wfn[512];
 	MultiByteToWideChar(CP_ACP, 0, fn, -1, wfn, 512);
-
-	WINTRUST_FILE_INFO info;
-	memset(&info, 0, sizeof(info));
-
-	info.cbStruct = sizeof(info);
-	info.pcwszFilePath = wfn;
-	info.hFile = NULL;
-	info.pgKnownSubject = NULL;
-
-	GUID WVTPolicyGUID = WINTRUST_ACTION_GENERIC_VERIFY_V2;
-
-	WINTRUST_DATA data;
-	memset(&data, 0, sizeof(data));
-
-	data.cbStruct = sizeof(data);
-	data.pPolicyCallbackData = NULL;
-	data.pSIPClientData = NULL;
-	data.dwUIChoice = WTD_UI_NONE;
-	data.fdwRevocationChecks = WTD_REVOKE_NONE;
-	data.dwUnionChoice = WTD_CHOICE_FILE;
-	data.dwStateAction = 0;
-	data.hWVTStateData = NULL;
-	data.pwszURLReference = NULL;
-	data.dwUIContext = 0;
-	data.pFile = &info;
-
-	LONG status = WinVerifyTrust(NULL, &WVTPolicyGUID, &data);
-
-	if (status != ERROR_SUCCESS)
-	{
-		//MessageBox(NULL, va(L"A trust chain error occurred in the downloaded rtcg.exe. The specific error code is 0x%08x.", status), L"O\x448\x438\x431\x43A\x430", MB_OK | MB_ICONSTOP);
-		//return false;
-	}
 
 	wchar_t exePath[512];
 	GetModuleFileName(GetModuleHandle(NULL), exePath, sizeof(exePath) / 2);
