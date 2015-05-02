7ca8eef728b7805aa6d3d1d068b0cb57d0128206
NTAuthority <bas@dotbas.net>
2015-05-02 18:37:58 +0200

    fix modules/xcode being unavailable during premake embed (gclient doesn't clone submodules by itself)
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=c3aa32e6d36bf9b2ae6a7b1f1764bd29c1006902

--- prebuild_premake.cmd
+++ prebuild_premake.cmd
@@ -2,6 +2,7 @@
 powershell -executionpolicy bypass -file citizenmp\prebuild_premake.ps1
 
 pushd build\premake
+call git submodule update --init --recursive
 ..\premake4 vs2013
 ..\premake4 embed
 popd
