3c94f3f9a837c298612a50028ee8bfdf317ea0ba
NTAuthority <bas@dotbas.net>
2015-05-03 06:09:28 +0200

    by popular request for pre-release messing around: a 'scripthookv' compatibility layer and an asi.asi loader
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=9dadb59e2f432784552f35fd71574253f0b36a16

---
+++ components/asi-five/src/Component.cpp
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the CitizenFX project - http://citizen.re/
+ *
+ * See LICENSE and MENTIONS in the root of the source tree for information
+ * regarding licensing.
+ */
+
+#include "StdInc.h"
+#include "ComponentLoader.h"
+
+class ComponentInstance : public Component
+{
+public:
+	virtual bool Initialize();
+
+	virtual bool DoGameLoad(void* module);
+
+	virtual bool Shutdown();
+};
+
+bool ComponentInstance::Initialize()
+{
+	InitFunctionBase::RunAll();
+
+	return true;
+}
+
+bool ComponentInstance::DoGameLoad(void* module)
+{
+	HookFunction::RunAll();
+
+	if (!LoadLibraryW(MakeRelativeCitPath(L"asi.asi").c_str()))
+	{
+		FatalError("couldn't load asi.asi");
+	}
+
+	return true;
+}
+
+bool ComponentInstance::Shutdown()
+{
+	return true;
+}
+
+extern "C" __declspec(dllexport) Component* CreateComponent()
+{
+	return new ComponentInstance();
+}
---
+++ components/scripthookv/src/VishCompat.cpp
@@ -0,0 +1,228 @@
+/*
+ * This file is part of the CitizenFX project - http://citizen.re/
+ *
+ * See LICENSE and MENTIONS in the root of the source tree for information
+ * regarding licensing.
+ */
+
+#include "StdInc.h"
+#include <scrEngine.h>
+#include <InputHook.h>
+
+#include <memory>
+
+#pragma comment(lib, "winmm.lib")
+#include <mmsystem.h>
+
+class FishScript
+{
+private:
+	HANDLE m_fiber;
+	uint32_t m_wakeAt;
+	void(*m_function)();
+
+private:
+	void Run();
+	
+public:
+	inline FishScript(void(*function)())
+	{
+		m_fiber = nullptr;
+		m_function = function;
+		m_wakeAt = timeGetTime();
+	}
+
+	void Tick();
+
+	void Yield(uint32_t time);
+
+	inline void(*GetFunction())()
+	{
+		return m_function;
+	}
+};
+
+static HANDLE g_mainFiber;
+static FishScript* g_fishScript;
+
+void FishScript::Tick()
+{
+	if (g_mainFiber == nullptr)
+	{
+		g_mainFiber = ConvertThreadToFiber(nullptr);
+	}
+
+	if (timeGetTime() < m_wakeAt)
+	{
+		return;
+	}
+
+	if (m_fiber)
+	{
+		g_fishScript = this;
+		SwitchToFiber(m_fiber);
+		g_fishScript = nullptr;
+	}
+	else
+	{
+		m_fiber = CreateFiber(NULL, [] (LPVOID handler)
+		{
+			reinterpret_cast<FishScript*>(handler)->Run();
+		}, this);
+	}
+}
+
+void FishScript::Run()
+{
+	m_function();
+}
+
+void FishScript::Yield(uint32_t time)
+{
+	m_wakeAt = timeGetTime() + time;
+	SwitchToFiber(g_mainFiber);
+}
+
+class FishThread : public GtaThread
+{
+private:
+	std::vector<std::shared_ptr<FishScript>> m_scripts;
+
+public:
+	virtual void DoRun() override
+	{
+		std::vector<std::shared_ptr<FishScript>> thisIterScripts(m_scripts);
+
+		for (auto& script : thisIterScripts)
+		{
+			script->Tick();
+		}
+	}
+
+	void AddScript(void(*fn)());
+
+	void RemoveScript(void(*fn)());
+};
+
+void FishThread::AddScript(void(*fn)())
+{
+	m_scripts.push_back(std::make_shared<FishScript>(fn));
+}
+
+void FishThread::RemoveScript(void(*fn)())
+{
+	for (auto it = m_scripts.begin(); it != m_scripts.end(); it++)
+	{
+		if ((*it)->GetFunction() == fn)
+		{
+			m_scripts.erase(it);
+			return;
+		}
+	}
+}
+
+static FishThread g_fish;
+
+void DLL_EXPORT scriptWait(unsigned long waitTime)
+{
+	g_fishScript->Yield(waitTime);
+}
+
+void DLL_EXPORT scriptRegister(HMODULE hMod, void(*function)())
+{
+	g_fish.AddScript(function);
+}
+
+void DLL_EXPORT scriptUnregister(void(*function)())
+{
+	g_fish.RemoveScript(function);
+}
+
+class FishNativeContext : public NativeContext
+{
+public:
+	FishNativeContext()
+		: NativeContext()
+	{
+
+	}
+
+	void Reset()
+	{
+		m_nArgCount = 0;
+		m_nDataCount = 0;
+	}
+
+	inline void* GetResultPointer()
+	{
+		return m_pReturn;
+	}
+};
+
+static FishNativeContext g_context;
+static uint64_t g_hash;
+
+void DLL_EXPORT nativeInit(uint64_t hash)
+{
+	g_context.Reset();
+	g_hash = hash;
+}
+
+void DLL_EXPORT nativePush64(uint64_t value)
+{
+	g_context.Push(value);
+}
+
+DLL_EXPORT uint64_t* nativeCall()
+{
+	auto fn = rage::scrEngine::GetNativeHandler(g_hash);
+
+	if (fn != 0)
+	{
+		__try
+		{
+			fn(&g_context);
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{
+			__debugbreak();
+		}
+	}
+
+	return reinterpret_cast<uint64_t*>(g_context.GetResultPointer());
+}
+
+typedef void(*TKeyboardFn)(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, BOOL isWithAlt, BOOL wasDownBefore, BOOL isUpNow);
+
+static std::set<TKeyboardFn> g_keyboardFunctions;
+
+void DLL_EXPORT keyboardHandlerRegister(TKeyboardFn function)
+{
+	g_keyboardFunctions.insert(function);
+}
+
+void DLL_EXPORT keyboardHandlerUnregister(TKeyboardFn function)
+{
+	g_keyboardFunctions.erase(function);
+}
+
+static InitFunction initFunction([] ()
+{
+	rage::scrEngine::OnScriptInit.Connect([] ()
+	{
+		rage::scrEngine::CreateThread(&g_fish);
+	});
+
+	InputHook::OnWndProc.Connect([] (HWND, UINT wMsg, WPARAM wParam, LPARAM lParam, bool&, LRESULT& result)
+	{
+		if (wMsg == WM_KEYDOWN || wMsg == WM_KEYUP || wMsg == WM_SYSKEYDOWN || wMsg == WM_SYSKEYUP)
+		{
+			auto functions = g_keyboardFunctions;
+
+			for (auto& function : functions)
+			{
+				function(wParam, lParam & 0xFFFF, (lParam >> 16) & 0xFF, (lParam >> 24) & 1, (wMsg == WM_SYSKEYDOWN || wMsg == WM_SYSKEYUP), (lParam >> 30) & 1, (wMsg == WM_SYSKEYUP || wMsg == WM_KEYUP));
+			}
+		}
+	});
+});
