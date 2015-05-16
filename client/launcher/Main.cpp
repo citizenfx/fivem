2b9278691d7e6a4f96fd0b6fe926a2d573665f10
NTAuthority <bas@dotbas.net>
2015-05-16 18:50:14 +0200

    really lame check for version 350
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=194f191f3a1f917584b8b934fe717b3d7e10e46e

--- client/launcher/Main.cpp
+++ client/launcher/Main.cpp
@@ -15,6 +15,8 @@ void EnsureGamePath();
 
 bool InitializeExceptionHandler();
 
+#pragma comment(lib, "version.lib")
+
 void main()
 {
 	// initialize the CRT
@@ -89,6 +91,39 @@ void main()
 		MessageBox(nullptr, L"Could not find the game executable (" GAME_EXECUTABLE L") at the configured path. Please check your CitizenFX.ini file.", PRODUCT_NAME, MB_OK | MB_ICONERROR);
 		ExitProcess(1);
 	}
+
+#ifdef GTA_FIVE
+	{
+		DWORD versionInfoSize = GetFileVersionInfoSize(gameExecutable.c_str(), nullptr);
+
+		if (versionInfoSize)
+		{
+			std::vector<uint8_t> versionInfo(versionInfoSize);
+
+			if (GetFileVersionInfo(gameExecutable.c_str(), 0, versionInfo.size(), &versionInfo[0]))
+			{
+				void* fixedInfoBuffer;
+				UINT fixedInfoSize;
+
+				VerQueryValue(&versionInfo[0], L"\\", &fixedInfoBuffer, &fixedInfoSize);
+
+				VS_FIXEDFILEINFO* fixedInfo = reinterpret_cast<VS_FIXEDFILEINFO*>(fixedInfoBuffer);
+				
+				if ((fixedInfo->dwFileVersionLS >> 16) != 350)
+				{
+					MessageBox(nullptr, va(L"The found GTA executable (%s) has version %d.%d.%d.%d, but only 1.0.350 is currently supported. Please obtain this version, and try again.",
+										   gameExecutable.c_str(),
+										   (fixedInfo->dwFileVersionMS >> 16),
+										   (fixedInfo->dwFileVersionMS & 0xFFFF),
+										   (fixedInfo->dwFileVersionLS >> 16),
+										   (fixedInfo->dwFileVersionLS & 0xFFFF)), PRODUCT_NAME, MB_OK | MB_ICONERROR);
+
+					ExitProcess(1);
+				}
+			}
+		}
+	}
+#endif
 
 	// main updater stuff
 
