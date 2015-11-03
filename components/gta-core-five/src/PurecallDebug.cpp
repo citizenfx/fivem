8fa1c6bdb797ac30982d67be5b572b458a292651
NTAuthority <bas@dotbas.net>
2015-11-03 18:23:07 +0100

    [gta:core:five] replace ERR_SYS_PURE __purecall handler with a custom, more detailed one (for fixing the issues people reported)
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=0b4b039b324a8be91cb0e887500d648bd9e382d6

---
+++ components/gta-core-five/src/PurecallDebug.cpp
@@ -0,0 +1,56 @@
+/*
+* This file is part of the CitizenFX project - http://citizen.re/
+*
+* See LICENSE and MENTIONS in the root of the source tree for information
+* regarding licensing.
+*/
+
+#include "StdInc.h"
+#include "Hooking.h"
+
+#include <typeinfo>
+
+struct VirtualBase
+{
+	virtual ~VirtualBase() {}
+};
+
+struct VirtualDerivative : public VirtualBase
+{
+	virtual ~VirtualDerivative() override {}
+};
+
+static void PurecallHandler(VirtualBase* self)
+{
+	// get the type name of the object
+	const char* typeName = va("unknown (vtable %p)", *(void**)self);
+
+	try
+	{
+		typeName = typeid(*self).name();
+	}
+	catch (std::__non_rtti_object&)
+	{
+		
+	}
+
+	// get the module base
+	const char* moduleBaseString = "";
+	HMODULE module;
+
+	if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCWSTR)_ReturnAddress(), &module))
+	{
+		char filename[MAX_PATH];
+		GetModuleFileNameA(module, filename, _countof(filename));
+
+		moduleBaseString = va(" - %s+%X", strrchr(filename, '\\') + 1, (char*)_ReturnAddress() - (char*)module);
+	}
+
+	// format a fatal error message with the right data
+	FatalError("Pure virtual function call in GTA function (type %s, called from %p%s)", typeName, _ReturnAddress(), moduleBaseString);
+}
+
+static HookFunction hookFunction([] ()
+{
+	hook::jump(hook::pattern("48 83 EC 28 B9 79 38 F4 43 E8").count(1).get(0).get<void>(0), PurecallHandler);
+});
