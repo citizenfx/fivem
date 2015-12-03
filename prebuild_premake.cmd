820819cc2b7d6fd68ebdc995407b21dbe899805a
NTAuthority <bas@dotbas.net>
2015-12-03 18:50:58 +0100

    [build tools] skip prebuild_premake if premake already exists
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=09a08c173caedc9bbfc87af27767d10076297329

--- prebuild_premake.cmd
+++ prebuild_premake.cmd
@@ -1,6 +1,8 @@
 @echo off
 rem powershell -executionpolicy bypass -file citizenmp\prebuild_premake.ps1
 
+if exist build\premake\bin\release\premake5.exe goto dontbuild
+
 pushd build\premake
 call git submodule update --init --recursive
 rem ..\premake5 vs2015
@@ -8,6 +10,8 @@ rem ..\premake5 embed
 del src\host\scripts.c
 nmake /f Bootstrap.mak windows
 popd
+
+:dontbuild
 
 rem call "%VCINSTALLDIR%\vcvarsall.bat" amd64_x86
 rem set PLATFORM=
