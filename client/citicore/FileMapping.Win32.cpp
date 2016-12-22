add459040f34246af9f14e6e378647aa98a4eb02
guava <guavaflavored@bobmail.info>
2016-12-22 03:40:34 +0100

    maybe-fix
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=adf75dec87c848bd5ed68fd59aaaf60caa280d80

--- client/citicore/FileMapping.Win32.cpp
+++ client/citicore/FileMapping.Win32.cpp
@@ -101,6 +101,11 @@ NTSTATUS NTAPI LdrLoadDllStub(const wchar_t* fileName, uint32_t* flags, UNICODE_
 		moduleName = &newString;
 	}
 
+	if (moduleNameStr.find(L"fraps64.dll") != std::string::npos || moduleNameStr.find(L"avghooka.dll") != std::string::npos)
+	{
+		return 0xC0000135;
+	}
+
 	return g_origLoadDll(fileName, flags, moduleName, handle);
 }
 
