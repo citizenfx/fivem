07336c5136f401e5d822eb4e136c3f8979e29fcd
guava <guavaflavored@bobmail.info>
2017-01-09 23:43:14 +0100

    don't regen udis86 optable
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=d8fc1c184c006207b3959b78e7f9f9db19527e57

--- prebuild_udis86.cmd
+++ prebuild_udis86.cmd
@@ -1,3 +1,3 @@
 @echo off
 cd vendor\udis86
-python scripts/ud_itab.py docs/x86/optable.xml ./libudis86/
+if not exist libudis86\itab.c ( python scripts/ud_itab.py docs/x86/optable.xml ./libudis86/ )
