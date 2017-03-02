b6b5c98ec409e924f77f1feaf7256f8d35de8426
guava <guavaflavored@bobmail.info>
2017-03-02 11:25:26 +0100

    extra adhesive censoring
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=458bf90d4702b8d81b87071640260d613d0347be

--- client/shared/Error.h
+++ client/shared/Error.h
@@ -15,8 +15,14 @@ inline uint32_t const_uint32()
 	return I;
 }
 
-#define GlobalError(f, ...) do { if (GlobalErrorReal(__FILE__, __LINE__, const_uint32<fnv1a_t<4>::Hash(f)>(), f, __VA_ARGS__) < 0) { *(int*)0 = 0; } } while(false)
-#define FatalError(f, ...) do { if (FatalErrorReal(__FILE__, __LINE__, const_uint32<fnv1a_t<4>::Hash(f)>(), f, __VA_ARGS__) < 0) { *(int*)0 = 0; } } while(false)
+#ifdef COMPILING_ADHESIVE
+#define _CFX_FILE "adhesive"
+#else
+#define _CFX_FILE __FILE__
+#endif
+
+#define GlobalError(f, ...) do { if (GlobalErrorReal(_CFX_FILE, __LINE__, const_uint32<fnv1a_t<4>::Hash(f)>(), f, __VA_ARGS__) < 0) { *(int*)0 = 0; } } while(false)
+#define FatalError(f, ...) do { if (FatalErrorReal(_CFX_FILE, __LINE__, const_uint32<fnv1a_t<4>::Hash(f)>(), f, __VA_ARGS__) < 0) { *(int*)0 = 0; } } while(false)
 #else
 void GlobalError(const char* string, const fmt::ArgList& formatList);
 void FatalError(const char* string, const fmt::ArgList& formatList);
