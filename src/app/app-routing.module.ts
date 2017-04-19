4e6584103d9a1aa5a3d6e9a44f2462eca3c8a872
oslk <peach@fivem.net>
2017-04-19 14:49:00 +0200

    route / to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { ServerListComponent } from './servers/ui/server-list.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
