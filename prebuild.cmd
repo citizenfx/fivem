c72604822d2a717864d2d929dfe0e4b2f0853a8a
samarium <samarium@fivem.net>
2017-05-07 11:03:44 +0200

    build Lua natives on prebuild
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=904fba64e436814bbcf689e0ec9339ef01580cc6

--- prebuild.cmd
+++ prebuild.cmd
@@ -1,3 +1,10 @@
 @echo off
 call code\prebuild_udis86.cmd
 call code\prebuild_misc.cmd
+
+cd %~dp0
+
+pushd ext\natives\
+powershell -executionpolicy unrestricted .\generate_natives.ps1
+copy /y out\* ..\..\data\citizen\scripting\lua
+popd
