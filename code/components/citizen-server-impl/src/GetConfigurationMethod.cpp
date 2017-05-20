1713fcaaa87d7452fbd9bed68a79083fa258cc50
meitnerium <meitnerium@fivem.net>
2017-05-20 18:45:52 +0200

    getConfiguration: filter by passed resources, if any

--- code/components/citizen-server-impl/src/GetConfigurationMethod.cpp
+++ code/components/citizen-server-impl/src/GetConfigurationMethod.cpp
@@ -15,8 +15,32 @@ static InitFunction initFunction([]()
 		{
 			json resources = json::array();
 
+			std::string_view filterValues = postMap["resources"];
+			std::set<std::string_view> filters;
+
+			if (!filterValues.empty())
+			{
+				int lastPos = 0;
+				int pos = -1;
+
+				do 
+				{
+					lastPos = pos + 1;
+					pos = filterValues.find_first_of(';', pos + 1);
+
+					auto thisValue = filterValues.substr(lastPos, (pos - lastPos));
+
+					filters.insert(thisValue);
+				} while (pos != std::string::npos);
+			}
+
 			resman->ForAllResources([&](fwRefContainer<fx::Resource> resource)
 			{
+				if (!filters.empty() && filters.find(resource->GetName()) == filters.end())
+				{
+					return;
+				}
+
 				if (resource->GetState() != fx::ResourceState::Started)
 				{
 					return;
