26bb6321cdd33169c6c5f58b98272045fa6dfd7d
ytterbium <ytterbium@fivem.net>
2017-05-22 10:03:59 +0200

    fix prebuild_natives.cmd

--- prebuild_natives.cmd
+++ prebuild_natives.cmd
@@ -2,5 +2,5 @@
 
 pushd ext\natives\
 powershell -executionpolicy unrestricted .\generate_natives.ps1
-copy /y out\* ..\..\data\citizen\scripting\lua
+copy /y out\* ..\..\data\shared\citizen\scripting\lua
 popd
