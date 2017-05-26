a2b58eb60b80cb914b3c091ce018612557d4e585
ytterbium <ytterbium@fivem.net>
2017-05-26 21:21:59 +0200

    generic resource callback component

---
+++ code/components/citizen-scripting-core/include/ResourceCallbackComponent.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <ResourceManager.h>
+
+#include <msgpack.hpp>
+
+#include <fxScripting.h>
+
+#include <mutex>
+
+namespace fx
+{
+	class ResourceCallbackScriptRuntime : public fx::OMClass<ResourceCallbackScriptRuntime, IScriptRuntime, IScriptRefRuntime>
+	{
+	private:
+		fx::Resource* m_resource;
+
+		IScriptHost* m_scriptHost;
+
+		std::map<int32_t, std::function<void(const msgpack::unpacked&)>> m_refs;
+
+		std::recursive_mutex m_refMutex;
+
+		int32_t m_refIdx;
+
+	public:
+		ResourceCallbackScriptRuntime(fx::Resource* resource, IScriptHost* scriptHost);
+
+		NS_DECL_ISCRIPTRUNTIME;
+
+		NS_DECL_ISCRIPTREFRUNTIME;
+
+		std::string AddCallbackRef(const std::function<void(const msgpack::unpacked&)>& resultCallback);
+	};
+
+	class ResourceCallbackComponent : public fwRefCountable
+	{
+	private:
+		fwRefContainer<Resource> m_resource;
+
+		ResourceManager* m_manager;
+
+		ResourceCallbackScriptRuntime* m_scriptRuntime;
+
+	public:
+		struct CallbackRef
+		{
+			std::string reference;
+
+			template<typename Packer>
+			void msgpack_pack(Packer& pk) const
+			{
+				pk
+					.pack_ext(reference.size(), 10)
+					.pack_ext_body(reference.c_str(), reference.size());
+			}
+		};
+
+	public:
+		ResourceCallbackComponent(ResourceManager* resource);
+
+		inline ResourceCallbackScriptRuntime* GetScriptRuntime()
+		{
+			return m_scriptRuntime;
+		}
+
+		virtual CallbackRef CreateCallback(const std::function<void(const msgpack::unpacked&)>&);
+	};
+}
+
+DECLARE_INSTANCE_TYPE(fx::ResourceCallbackComponent);
---
+++ code/components/citizen-scripting-core/src/ResourceCallbackComponent.cpp
@@ -0,0 +1,160 @@
+#include "StdInc.h"
+#include <ResourceCallbackComponent.h>
+#include <ResourceScriptingComponent.h>
+
+namespace fx
+{
+ResourceCallbackScriptRuntime::ResourceCallbackScriptRuntime(fx::Resource* resource, IScriptHost* scriptHost)
+	: m_resource(resource), m_scriptHost(scriptHost), m_refIdx(1)
+{
+
+}
+
+result_t ResourceCallbackScriptRuntime::Create(IScriptHost* host)
+{
+	return FX_S_OK;
+}
+
+result_t ResourceCallbackScriptRuntime::Destroy()
+{
+	return FX_S_OK;
+}
+
+void* ResourceCallbackScriptRuntime::GetParentObject()
+{
+	return m_resource;
+}
+
+void ResourceCallbackScriptRuntime::SetParentObject(void*)
+{
+
+}
+
+int ResourceCallbackScriptRuntime::GetInstanceId()
+{
+	return 'CBRT';
+}
+
+result_t ResourceCallbackScriptRuntime::CallRef(int32_t refIdx, char* argsSerialized, uint32_t argsLength, char** retvalSerialized, uint32_t* retvalLength)
+{
+	// preset retval to be null
+	{
+		static msgpack::sbuffer sb;
+		sb.clear();
+
+		msgpack::packer<msgpack::sbuffer> packer(sb);
+		packer.pack_array(0);
+
+		*retvalSerialized = sb.data();
+		*retvalLength = sb.size();
+	}
+
+	// assume we have the ref still
+	std::function<void(const msgpack::unpacked&)> cb;
+
+	{
+		std::unique_lock<std::recursive_mutex> lock(m_refMutex);
+
+		auto it = m_refs.find(refIdx);
+
+		if (it == m_refs.end())
+		{
+			return FX_E_INVALIDARG;
+		}
+
+		cb = it->second;
+	}
+
+	// unpack
+	msgpack::unpacked unpacked;
+	msgpack::unpack(unpacked, argsSerialized, argsLength);
+
+	// convert
+	cb(unpacked);
+
+	return FX_S_OK;
+}
+
+result_t ResourceCallbackScriptRuntime::DuplicateRef(int32_t refIdx, int32_t* outRef)
+{
+	std::unique_lock<std::recursive_mutex> lock(m_refMutex);
+
+	auto it = m_refs.find(refIdx);
+
+	if (it == m_refs.end())
+	{
+		return FX_E_INVALIDARG;
+	}
+
+	int32_t idx = m_refIdx;
+	m_refs[idx] = it->second;
+
+	m_refIdx++;
+
+	*outRef = idx;
+
+	return FX_S_OK;
+}
+
+result_t ResourceCallbackScriptRuntime::RemoveRef(int32_t refIdx)
+{
+	std::unique_lock<std::recursive_mutex> lock(m_refMutex);
+	m_refs.erase(refIdx);
+
+	return FX_S_OK;
+}
+
+std::string ResourceCallbackScriptRuntime::AddCallbackRef(const std::function<void(const msgpack::unpacked&)>& resultCallback)
+{
+	std::unique_lock<std::recursive_mutex> lock(m_refMutex);
+
+	// add the ref to the list
+	int32_t idx = m_refIdx;
+	m_refs[idx] = resultCallback;
+
+	m_refIdx++;
+
+	// canonicalize the ref
+	char* refString;
+	m_scriptHost->CanonicalizeRef(idx, GetInstanceId(), &refString);
+
+	// turn into a std::string and free
+	std::string retval = refString;
+	fwFree(refString);
+
+	// return the value
+	return retval;
+}
+
+ResourceCallbackComponent::ResourceCallbackComponent(fx::ResourceManager* manager)
+	: m_manager(manager)
+{
+	m_resource = manager->CreateResource("_cfx_internal");
+	assert(m_resource->Start());
+
+	fwRefContainer<fx::ResourceScriptingComponent> scriptingComponent = m_resource->GetComponent<fx::ResourceScriptingComponent>();
+	fx::OMPtr<ResourceCallbackScriptRuntime> runtime = fx::MakeNew<ResourceCallbackScriptRuntime>(m_resource.GetRef(), scriptingComponent->GetScriptHost().GetRef());
+
+	// convert to IScriptRuntime
+	fx::OMPtr<IScriptRuntime> baseRuntime;
+	runtime.As(&baseRuntime);
+
+	scriptingComponent->AddRuntime(baseRuntime);
+
+	// and set the local runtime
+	m_scriptRuntime = runtime.GetRef();
+}
+
+auto ResourceCallbackComponent::CreateCallback(const std::function<void(const msgpack::unpacked &)>& cb) -> CallbackRef
+{
+	return CallbackRef{ this->GetScriptRuntime()->AddCallbackRef(cb) };
+}
+}
+
+static InitFunction initFunction([]()
+{
+	fx::ResourceManager::OnInitializeInstance.Connect([](fx::ResourceManager* manager)
+	{
+		manager->SetComponent(new fx::ResourceCallbackComponent(manager));
+	});
+});
