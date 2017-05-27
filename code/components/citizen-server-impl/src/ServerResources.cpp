8988f7e132e34d6caeb7fbd532491f2c38e393f5
ytterbium <ytterbium@fivem.net>
2017-05-27 08:54:30 +0200

    auto-start sessionmanager

--- code/components/citizen-server-impl/src/ServerResources.cpp
+++ code/components/citizen-server-impl/src/ServerResources.cpp
@@ -216,6 +216,15 @@ static InitFunction initFunction([]()
 
 		ScanResources(instance);
 
+		{
+			auto sessionmanager = resman->GetResource("sessionmanager");
+
+			if (sessionmanager.GetRef())
+			{
+				sessionmanager->Start();
+			}
+		}
+
 		static auto commandRef = instance->AddCommand("start", [=](const std::string& resourceName)
 		{
 			if (resourceName.empty())
