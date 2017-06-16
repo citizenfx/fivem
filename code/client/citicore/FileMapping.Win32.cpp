3a077483285c391f61e9a475ea224e001c93e190
mendelevium <mendelevium@fivem.net>
2017-06-16 21:22:39 +0200

    dll blacklist: add nvspcap

--- code/client/citicore/FileMapping.Win32.cpp
+++ code/client/citicore/FileMapping.Win32.cpp
@@ -112,6 +112,8 @@ NTSTATUS NTAPI LdrLoadDllStub(const wchar_t* fileName, uint32_t* flags, UNICODE_
 		moduleNameStr.find(L"overlay.x64.dll") != std::string::npos ||
 		// HideMyIP, causes LoopbackTcpServer and libuv to crash
 		moduleNameStr.find(L"hmipcore64.dll") != std::string::npos ||
+		// NVIDIA SHARE/ShadowPlay capture DLL, high correlation with ERR_GFX_D3D_INIT failures
+		moduleNameStr.find(L"nvspcap") != std::string::npos ||
 		// Ad Muncher, causes LoopbackTcpServer to crash
 		moduleNameStr.find(L"am64-34121.dll") != std::string::npos
 	)
