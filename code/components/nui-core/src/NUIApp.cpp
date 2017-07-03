6f0308cb4a49c19a0d24c76aca067b940b401a8e
barium <barium@fivem.net>
2017-07-03 19:05:44 +0200

    nui: disable gpu vsync

--- code/components/nui-core/src/NUIApp.cpp
+++ code/components/nui-core/src/NUIApp.cpp
@@ -45,6 +45,7 @@ void NUIApp::OnBeforeCommandLineProcessing(const CefString& process_type, CefRef
 {
 	command_line->AppendSwitch("enable-experimental-web-platform-features");
 	command_line->AppendSwitch("in-process-gpu");
+	command_line->AppendSwitch("disable-gpu-vsync");
 	command_line->AppendSwitchWithValue("default-encoding", "utf-8");
 }
 
