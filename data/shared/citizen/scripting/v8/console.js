0113bdda3180120019f56ebe2429a6a600284cc5
Alexander Kukhta <alexridersx@gmail.com>
2017-07-21 07:17:52 +0300

    citizen:scripting:v8 WHATWG and TypeScript lib definitions compliant console implementation

--- data/shared/citizen/scripting/v8/console.js
+++ data/shared/citizen/scripting/v8/console.js
@@ -1,6 +1,14 @@
 // Global console
 
 (function (global) {
+    const percent = '%'.charCodeAt(0);
+
+    const stringSpecifier = 's'.charCodeAt(0);
+    const decimalSpecifier = 'd'.charCodeAt(0);
+    const floatSpecifier = 'f'.charCodeAt(0);
+    const oSmallSpecifier = 'o'.charCodeAt(0);
+    const oBigSpecifier = 'O'.charCodeAt(0);
+
     const typedArrays = [
         Uint8Array,
         Uint16Array,
@@ -23,8 +31,8 @@
                 super(options.message);
             } else {
                 super(
-                    `${Console._formatArgs(options.actual).slice(0, 128)} ` +
-                    `${options.operator} ${Console._formatArgs(options.expected).slice(0, 128)}`
+                    `${format(options.actual).slice(0, 128)} ` +
+                    `${options.operator} ${format(options.expected).slice(0, 128)}`
                 );
             }
 
@@ -47,8 +55,8 @@
         const lines = ['Map {'];
 
         for (const [key, value] of map) {
-            const keyFormatted = Console._formatArgs(key);
-            const valueFormatted = Console._formatArgs(value);
+            const keyFormatted = format(key);
+            const valueFormatted = format(value);
 
             lines[lines.length] = indentNewLines(`${keyFormatted} => ${valueFormatted}`, '  ');
         }
@@ -60,7 +68,7 @@
         const lines = ['Set {'];
 
         for (const value of set) {
-            lines[lines.length] = indentNewLines(Console._formatArgs(value), '  ');
+            lines[lines.length] = indentNewLines(format(value), '  ');
         }
 
         return lines.concat('}').join('\n');
@@ -80,78 +88,193 @@
         return `${array.constructor.name}[ ${array.join(' ')} ]`;
     }
 
+    function formatValue(arg) {
+        switch (true) {
+            case arg instanceof WeakMap:
+                return 'WeakMap {}';
+
+            case arg instanceof WeakSet:
+                return 'WeakSet {}';
+
+            case arg instanceof Map:
+                return formatMap(arg);
+
+            case arg instanceof Set:
+                return formatSet(arg);
+
+            case isTypedArray(arg):
+                return formatTypedArray(arg);
+
+            case Array.isArray(arg):
+            case arg.toString() === "[object Object]":
+                return JSON.stringify(arg, null, 2);
+
+            case arg.toString() === "[object Error]" || arg instanceof Error:
+                return arg.stack || Error.prototype.toString.call(arg);
+
+            default:
+                return arg.toString();
+        }
+    }
+
+    function formatSpecifier(specifier, value) {
+        switch (specifier) {
+            case stringSpecifier:
+                return '' + value;
+
+            case decimalSpecifier:
+                return parseInt(value, 10).toString();
+
+            case floatSpecifier:
+                return parseFloat(value).toString();
+
+            case oSmallSpecifier:
+            case oBigSpecifier:
+                return formatValue(value);
+
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * WHATWG Spec compliant formatter
+     * 
+     * @see https://console.spec.whatwg.org/#formatter
+     * @param {*} message 
+     * @param {*} args 
+     */
+    function format(message = undefined, ...args) {
+        if (typeof message === "undefined") {
+            return '';
+        }
+
+        const totalArgs = args.length;
+
+        if (totalArgs === 0) {
+            return formatValue(message);
+        }
+
+        const result = [];
+        let usedArgs = 0;
+
+        if (typeof message !== "string") {
+            result[result.length] = formatValue(message);
+        } else {
+            const messageLastIndex = message.length - 1;
+            let formattedMessage = '';
+            let i = 0;
+
+            for (; i < messageLastIndex; i++) {
+                const char = message.charAt(i);
+
+                if (char.charCodeAt(0) === percent && totalArgs > usedArgs) {
+                    const result = formatSpecifier(message.charCodeAt(i + 1), args[usedArgs]);
+
+                    if (result !== false) {
+                        formattedMessage += result;
+
+                        i++;
+                        usedArgs++;
+
+                        continue;
+                    }
+                }
+
+                formattedMessage += char;
+            }
+
+            if (i === messageLastIndex) {
+                formattedMessage += message[messageLastIndex];
+            }
+
+            result[result.length] = formattedMessage;
+        }        
+
+        return result.concat(args.slice(usedArgs).map(formatValue)).join(' ');
+    }
+
     class Console {
         constructor() {
             this._trace = global['Citizen'].trace || global['print'];
 
             this._timers = new Map();
+            this._counters = new Map();
 
             this.log = this.log.bind(this);
             this.info = this.info.bind(this);
             this.warn = this.warn.bind(this);
             this.time = this.time.bind(this);
+            this.clear = this.clear.bind(this);
+            this.group = this.group.bind(this);
             this.error = this.error.bind(this);
             this.trace = this.trace.bind(this);
             this.table = this.table.bind(this);
+            this.dirxml = this.dirxml.bind(this);
+            this.select = this.select.bind(this);
             this.assert = this.assert.bind(this);
             this.timeEnd = this.timeEnd.bind(this);
+            this.groupEnd = this.groupEnd.bind(this);
+            this.groupCollapseed = this.groupCollapseed.bind(this);
+            this.msIsIndependentlyComposed = this.msIsIndependentlyComposed.bind(this);
         }
 
-        static _formatArgs(args) {
-            if (!Array.isArray(args)) {
-                args = [args];
-            }
-
-            return args.map(arg => {
-                switch (true) {
-                    case arg instanceof WeakMap:
-                        return 'WeakMap {}';
-
-                    case arg instanceof WeakSet:
-                        return 'WeakSet {}';
-
-                    case arg instanceof Map:
-                        return formatMap(arg);
-
-                    case arg instanceof Set:
-                        return formatSet(arg);
-
-                    case isTypedArray(arg):
-                        return formatTypedArray(arg);
-
-                    case Array.isArray(arg):
-                    case arg.toString() === "[object Object]":
-                        return JSON.stringify(arg, null, 2);
-
-                    case arg.toString() === "[object Error]" || arg instanceof Error:
-                        return arg.stack || Error.prototype.toString.call(arg);
-
-                    default:
-                        return arg.toString();
-                }
-            }).join(' ');
+        table() {
+            this.error('console.table is not implemented yet');
         }
 
-        log(...args) {
-            this._trace(Console._formatArgs(args));
+        dirxml() {
+            this.error('console.dirxml is not implemented yet');
         }
 
-        info(...args) {
-            this._trace('Info: ' + Console._formatArgs(args));
+        clear() {
+            this.error('console.clear is not implemented yet');
         }
 
-        warn(...args) {
-            this._trace('Warning: ' + Console._formatArgs(args));
+        group() {
+            this.error('console.group is not implemented yet');
         }
 
-        error(...args) {
-            this._trace('Error: ' + Console._formatArgs(args));
+        groupCollapseed() {
+            this.error('console.groupCollapsed is not implemented yet');
         }
 
-        trace(...args) {
+        groupEnd() {
+            this.error('console.groupEnd is not implemented yet');
+        }
+
+        msIsIndependentlyComposed() {
+            this.error('console.msIsIndependentlyComposed is not implemented yet');
+        }
+
+        select() {
+            this.error('console.select is not implemented yet');
+        }
+
+        log(message = undefined, ...optionalParams) {
+            this._trace(format(message, ...optionalParams));
+        }
+
+        debug(message = undefined, ...optionalParams) {
+            this._trace('Debug: ' + format(message, ...optionalParams));
+        }
+
+        info(message = undefined, ...optionalParams) {
+            this._trace('Info: ' + format(message, ...optionalParams));
+        }
+
+        warn(message = undefined, ...optionalParams) {
+            this._trace('Warning: ' + format(message, ...optionalParams));
+        }
+
+        error(message = undefined, ...optionalParams) {
+            this._trace('Error: ' + format(message, ...optionalParams));
+        }
+
+        trace(message = undefined, ...optionalParams) {
             const err = {
                 name: 'Trace',
-                message: Console._formatArgs(args)
+                message: format(message, ...optionalParams)
             };
 
             Error.captureStackTrace(err, trace);
@@ -159,8 +282,16 @@
             this._trace(err.stack);
         }
 
-        table() {
-            this.warn('console.table is not implemented yet');
+        dir(item, options) {
+            this._trace('Dir: ', formatValue(item));
+        }
+
+        count(label) {
+            const counter = this._counters.get(label) || 1;
+
+            this._counters.set(label, counter);
+
+            this.log(`${label}: ${counter}`);
         }
 
         time(label) {
@@ -175,7 +306,7 @@
 
             const duration = Citizen.getTickCount() - this._timers.get(lable);
 
-            this.log(`${label}: ${duration}ms`);
+            this.log(`${label}: ${duration} ms`);
 
             this._times.delete(label);
         }
@@ -185,7 +316,7 @@
                 throw new AssertionError({
                     actual: expression,
                     expected: true,
-                    message: Console._formatArgs(args),
+                    message: format.apply(null, args),
                     operator: '==',
                     stackStartFunction: this.assert
                 });
