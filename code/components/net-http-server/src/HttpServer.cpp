citizenfx/fivem

commit 9f61d23805ff42460ba02f320b5c4525485fb26e

net:http-server: don't process a new request when a request is still pending

code/components/net-http-server/src/HttpServer.cpp
@@ -79,8 +79,10 @@ void HttpServerImpl::OnConnection(fwRefContainer<TcpServerStream> stream)
 	};
 
 	std::shared_ptr<HttpConnectionData> connectionData = std::make_shared<HttpConnectionData>();
+	std::shared_ptr<HttpState> reqState = std::make_shared<HttpState>();
 
-	stream->SetReadCallback([=] (const std::vector<uint8_t>& data)
+	std::function<void(const std::vector<uint8_t>&)> readCallback;
+	readCallback = [=](const std::vector<uint8_t>& data)
 	{
 		// keep a reference to the connection data locally
 		std::shared_ptr<HttpConnectionData> localConnectionData = connectionData;
@@ -109,6 +111,11 @@ void HttpServerImpl::OnConnection(fwRefContainer<TcpServerStream> stream)
 			// depending on the state, perform an action
 			if (localConnectionData->readState == ReadStateRequest)
 			{
+				if (reqState->blocked)
+				{
+					break;
+				}
+
 				// copy the deque into a vector for data purposes
 				std::vector<uint8_t> requestData(readQueue.begin(), readQueue.end());
 
@@ -123,7 +130,7 @@ void HttpServerImpl::OnConnection(fwRefContainer<TcpServerStream> stream)
 				size_t numHeaders = 50;
 
 				int result = phr_parse_request(reinterpret_cast<const char*>(&requestData[0]), requestData.size(), &requestMethod, &requestMethodLength,
-											   &path, &pathLength, &minorVersion, localConnectionData->headers, &numHeaders, localConnectionData->lastLength);
+					&path, &pathLength, &minorVersion, localConnectionData->headers, &numHeaders, localConnectionData->lastLength);
 
 				if (result > 0)
 				{
@@ -146,7 +153,9 @@ void HttpServerImpl::OnConnection(fwRefContainer<TcpServerStream> stream)
 
 					// store the request in a request instance
 					fwRefContainer<HttpRequest> request = new HttpRequest(1, minorVersion, requestMethodStr, pathStr, headerList);
-					fwRefContainer<HttpResponse> response = new HttpResponse(stream, request);
+					fwRefContainer<HttpResponse> response = new HttpResponse(stream, request, reqState);
+
+					reqState->blocked = true;
 
 					for (auto& handler : m_handlers)
 					{
@@ -299,7 +308,14 @@ void HttpServerImpl::OnConnection(fwRefContainer<TcpServerStream> stream)
 				}
 			}
 		}
-	});
+	};
+
+	reqState->ping = [=]()
+	{
+		readCallback({});
+	};
+
+	stream->SetReadCallback(readCallback);
 }
 
 HttpRequest::HttpRequest(int httpVersionMajor, int httpVersionMinor, const std::string& requestMethod, const std::string& path, const HeaderMap& headerList)
@@ -312,8 +328,8 @@ HttpRequest::~HttpRequest()
 	SetDataHandler(std::function<void(const std::vector<uint8_t>&)>());
 }
 
-HttpResponse::HttpResponse(fwRefContainer<TcpServerStream> clientStream, fwRefContainer<HttpRequest> request)
-	: m_clientStream(clientStream), m_ended(false), m_statusCode(200), m_sentHeaders(false), m_request(request), m_closeConnection(false)
+HttpResponse::HttpResponse(fwRefContainer<TcpServerStream> clientStream, fwRefContainer<HttpRequest> request, const std::shared_ptr<HttpState>& reqState)
+	: m_clientStream(clientStream), m_ended(false), m_statusCode(200), m_sentHeaders(false), m_request(request), m_closeConnection(false), m_requestState(reqState)
 {
 
 }
@@ -429,6 +445,12 @@ void HttpResponse::End()
 	if (m_closeConnection)
 	{
 		m_clientStream->Close();
+	}
+
+	if (m_requestState->blocked)
+	{
+		m_requestState->blocked = false;
+		m_requestState->ping();
 	}
 }
 

code/components/net-http-server/include/HttpServer.h
@@ -78,6 +78,15 @@ public:
 	}
 };
 
+struct HttpState
+{
+	// should this request parser be blocked?
+	bool blocked;
+
+	// a function to call when we want to unblock the request
+	std::function<void()> ping;
+};
+
 class
 #ifdef COMPILING_NET_HTTP_SERVER
 	DLL_EXPORT
@@ -99,11 +108,13 @@ private:
 
 	HeaderMap m_headerList;
 
+	std::shared_ptr<HttpState> m_requestState;
+
 private:
 	static std::string GetStatusMessage(int statusCode);
 
 public:
-	HttpResponse(fwRefContainer<TcpServerStream> clientStream, fwRefContainer<HttpRequest> request);
+	HttpResponse(fwRefContainer<TcpServerStream> clientStream, fwRefContainer<HttpRequest> request, const std::shared_ptr<HttpState>& reqState);
 
 	std::string GetHeader(const std::string& name);
 
@@ -143,6 +154,11 @@ public:
 	inline bool HasEnded()
 	{
 		return m_ended;
+	}
+
+	inline std::shared_ptr<HttpState> GetState()
+	{
+		return m_requestState;
 	}
 };
 

commit 5d8476447e73912944fca2d05faede7a5584a675

net:http-server: fix memory/resource leak introduced in 9f61d238
thanks to 'karthic' and others for reporting this bug!

code/components/net-http-server/src/HttpServer.cpp
@@ -331,6 +331,11 @@ void HttpServerImpl::OnConnection(fwRefContainer<TcpServerStream> stream)
 	};
 
 	stream->SetReadCallback(readCallback);
+
+	stream->SetCloseCallback([=]()
+	{
+		reqState->ping = {};
+	});
 }
 
 HttpRequest::HttpRequest(int httpVersionMajor, int httpVersionMinor, const std::string& requestMethod, const std::string& path, const HeaderMap& headerList, const std::string& remoteAddress)