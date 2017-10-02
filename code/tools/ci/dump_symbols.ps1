7fb9597b3f057a1c269b273727e45e3f4e8ce30e
vanadium <vanadium@fivem.net>
2017-10-02 10:53:39 +0200

    build: also dump Breakpad symbols for gta-core-five.dll

--- code/tools/ci/dump_symbols.ps1
+++ code/tools/ci/dump_symbols.ps1
@@ -20,7 +20,7 @@ workflow dump_pdb {
             $basename = $pdb.BaseName
             $outname = [io.path]::ChangeExtension($pdb.FullName, "sym")
 
-            if (!($basename -eq "botan") -and !($basename -eq "gta-core-five")) {
+            if (!($basename -eq "botan")) {
                 Start-Process C:\f\dump_syms.exe -ArgumentList ($pdb.FullName) -RedirectStandardOutput $outname -Wait -WindowStyle Hidden
             }
         }
