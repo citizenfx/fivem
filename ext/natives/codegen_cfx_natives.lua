ed7ede0b4c1e5135e10a53cfa6ffdd7b61631bb8
Deltanic <rdg94@live.nl>
2017-12-25 21:55:41 +0100

    Rename Get/SetVehicleWheelTrackWidth to Get/SetVehicleWheelXOffset as it's technically more correct.

--- ext/natives/codegen_cfx_natives.lua
+++ ext/natives/codegen_cfx_natives.lua
@@ -1299,7 +1299,7 @@ native 'SET_VEHICLE_WHEEL_XROT'
 	apiset 'client'
 	returns 'void'
 
-native 'GET_VEHICLE_WHEEL_TRACK_WIDTH'
+native 'GET_VEHICLE_WHEEL_X_OFFSET'
 	arguments {
 		Vehicle 'vehicle',
 		int 'wheelIndex'
@@ -1312,7 +1312,7 @@ native 'GET_VEHICLE_WHEEL_TRACK_WIDTH'
 	</summary>
 ]]
 
-native 'SET_VEHICLE_WHEEL_TRACK_WIDTH'
+native 'SET_VEHICLE_WHEEL_X_OFFSET'
 	arguments {
 		Vehicle 'vehicle',
 		int 'wheelIndex',
@@ -1323,7 +1323,15 @@ native 'SET_VEHICLE_WHEEL_TRACK_WIDTH'
 	doc [[
 	<summary>
 	Adjusts the offset of the specified wheel relative to the wheel's axle center.
-	Needs to be called every frame in order to function properly, as GTA will reset the track width otherwise.
+	Needs to be called every frame in order to function properly, as GTA will reset the offset otherwise.
+
+	This function can be especially useful to set the track width of a vehicle, for example:
+	```
+	function SetVehicleFrontTrackWidth(vehicle, width)
+		SetVehicleWheelXOffset(vehicle, 0, -width/2)
+		SetVehicleWheelXOffset(vehicle, 1, width/2)
+	end
+	```
 	</summary>
 ]]
 
