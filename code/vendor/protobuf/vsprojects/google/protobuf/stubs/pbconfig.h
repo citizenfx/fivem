93be7722d9814a9b994222c83f604fe058748e2b
bond <bond@lab2.boras.org>
2018-05-09 14:36:25 +0200

    Cleaning up and explaining pbconfig.h

--- code/vendor/protobuf/vsprojects/google/protobuf/stubs/pbconfig.h
+++ code/vendor/protobuf/vsprojects/google/protobuf/stubs/pbconfig.h
@@ -1,15 +1,5 @@
-/* protobuf config.h for MSVC.  On other platforms, this is generated
- * automatically by autoheader / autoconf / configure. */
+// This replaces the shipped one with this that fixes building on linux
 
-// NOTE: if you add new macros in this file manually, please propagate the macro
-// to vsprojects/config.h.
-
-/* the namespace of hash_map/hash_set */
-// Apparently Microsoft decided to move hash_map *back* to the std namespace
-// in MSVC 2010:
-//   http://blogs.msdn.com/vcblog/archive/2009/05/25/stl-breaking-changes-in-visual-studio-2010-beta-1.aspx
-// And.. they are moved back to stdext in MSVC 2013 (haven't checked 2012). That
-// said, use unordered_map for MSVC 2010 and beyond is our safest bet.
 #if _MSC_VER >= 1600
 #define GOOGLE_PROTOBUF_HASH_NAMESPACE std
 #define GOOGLE_PROTOBUF_HASH_MAP_H <unordered_map>
@@ -28,8 +18,6 @@
 #define GOOGLE_PROTOBUF_HASH_MAP_CLASS unordered_map
 #define GOOGLE_PROTOBUF_HASH_SET_H <unordered_set>
 #define GOOGLE_PROTOBUF_HASH_SET_CLASS unordered_set
-#define GOOGLE_PROTOBUF_HAVE_HASH_MAP 1
-#define GOOGLE_PROTOBUF_HAVE_HASH_SET 1
 #define HAVE_PTHREAD 1
 #endif
 
