670b89c21dd217cdb6bceadbb1296752d4f37a61
Emre Cürgül <pongo1999712@gmail.com>
2018-05-28 18:20:50 +0200

    Add more Ped DrivingStyles and new World methods

--- code/client/clrcore/External/Ped.cs
+++ code/client/clrcore/External/Ped.cs
@@ -12,13 +12,19 @@ namespace CitizenFX.Core
 	}
 	public enum DrivingStyle
 	{
+		None = 0,
 		Normal = 786603,
 		IgnoreLights = 2883621,
 		SometimesOvertakeTraffic = 5,
 		Rushed = 1074528293,
 		AvoidTraffic = 786468,
-		AvoidTrafficExtremely = 6
-	}
+		AvoidTrafficExtremely = 6,
+		AvoidHighwaysWhenPossible = 536870912,
+		IgnorePathing = 16777216,
+		IgnoreRoads = 4194304,
+		ShortestPath = 262144,
+		Backwards = 1024
+    }
 	[Flags]
 	public enum VehicleDrivingFlags : uint
 	{
--- code/client/clrcore/External/World.cs
+++ code/client/clrcore/External/World.cs
@@ -606,6 +606,126 @@ namespace CitizenFX.Core
 			return res.ToArray();
 		}
 
+        /// <summary>
+        /// Gets an <c>array</c> of all the <see cref="Prop"/>s on the map.
+        /// </summary>
+        public static Prop[] GetAllProps()
+        {
+            List<Prop> props = new List<Prop>();
+
+            OutputArgument entHandleOut = new OutputArgument();
+            int handle = Function.Call<int>(Hash.FIND_FIRST_OBJECT, entHandleOut);
+            int entHandle = entHandleOut.GetResult<int>();
+
+            Prop prop = (Prop)Entity.FromHandle(entHandle);
+            if (prop != null)
+                props.Add(prop);
+
+            entHandleOut = new OutputArgument();
+            while (Function.Call<bool>(Hash.FIND_NEXT_OBJECT, handle, entHandleOut))
+            {
+                entHandle = entHandleOut.GetResult<int>();
+                prop = (Prop)Entity.FromHandle(entHandle);
+                if (prop != null)
+                    props.Add(prop);
+
+                entHandleOut = new OutputArgument();
+            }
+
+            Function.Call(Hash.END_FIND_OBJECT, handle);
+            return props.ToArray();
+        }
+
+        /// <summary>
+        /// Gets an <c>array</c> of all the <see cref="Ped"/>s on the map.
+        /// </summary>
+        public static Ped[] GetAllPeds()
+        {
+            List<Ped> peds = new List<Ped>();
+
+            OutputArgument entHandleOut = new OutputArgument();
+            int handle = Function.Call<int>(Hash.FIND_FIRST_PED, entHandleOut);
+            int entHandle = entHandleOut.GetResult<int>();
+
+            Ped ped = (Ped)Entity.FromHandle(entHandle);
+            if (ped != null)
+                peds.Add(ped);
+
+            entHandleOut = new OutputArgument();
+            while (Function.Call<bool>(Hash.FIND_NEXT_PED, handle, entHandleOut))
+            {
+                entHandle = entHandleOut.GetResult<int>();
+                ped = (Ped)Entity.FromHandle(entHandle);
+                if (ped != null)
+                    peds.Add(ped);
+
+                entHandleOut = new OutputArgument();
+            }
+
+            Function.Call(Hash.END_FIND_PED, handle);
+            return peds.ToArray();
+        }
+
+        /// <summary>
+        /// Gets an <c>array</c> of all the <see cref="Vehicle"/>s on the map.
+        /// </summary>
+        public static Vehicle[] GetAllVehicles()
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            OutputArgument entHandleOut = new OutputArgument();
+            int handle = Function.Call<int>(Hash.FIND_FIRST_VEHICLE, entHandleOut);
+            int entHandle = entHandleOut.GetResult<int>();
+
+            Vehicle vehicle = (Vehicle)Entity.FromHandle(entHandle);
+            if (vehicle != null)
+                vehicles.Add(vehicle);
+
+            entHandleOut = new OutputArgument();
+            while (Function.Call<bool>(Hash.FIND_NEXT_VEHICLE, handle, entHandleOut))
+            {
+                entHandle = entHandleOut.GetResult<int>();
+                vehicle = (Vehicle)Entity.FromHandle(entHandle);
+                if (vehicle != null)
+                    vehicles.Add(vehicle);
+
+                entHandleOut = new OutputArgument();
+            }
+
+            Function.Call(Hash.END_FIND_VEHICLE, handle);
+            return vehicles.ToArray();
+        }
+
+        /// <summary>
+        /// Gets an <c>array</c> of all the <see cref="Pickup"/>s on the map.
+        /// </summary>
+        public static Pickup[] GetAllPickups()
+        {
+            List<Pickup> pickups = new List<Pickup>();
+
+            OutputArgument entHandleOut = new OutputArgument();
+            int handle = Function.Call<int>(Hash.FIND_FIRST_PICKUP, entHandleOut);
+            int entHandle = entHandleOut.GetResult<int>();
+
+            Pickup pickup = new Pickup(entHandle);
+            if (pickup.Exists())
+                pickups.Add(pickup);
+
+            entHandleOut = new OutputArgument();
+            while (Function.Call<bool>(Hash.FIND_NEXT_PICKUP, handle, entHandleOut))
+            {
+                entHandle = entHandleOut.GetResult<int>();
+                pickup = new Pickup(entHandle);
+                if (pickup.Exists())
+                    pickups.Add(pickup);
+
+                entHandleOut = new OutputArgument();
+            }
+
+            Function.Call(Hash.END_FIND_PICKUP, handle);
+            return pickups.ToArray();
+        }
+
         // CFX-TODO
         /*
 		/// <summary>
@@ -829,14 +949,14 @@ namespace CitizenFX.Core
 		}
         */
 
-		/// <summary>
-		/// Gets the closest <see cref="ISpatial"/> to a given position in the World.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="position">The position to check against.</param>
-		/// <param name="spatials">The spatials to check.</param>
-		/// <returns>The closest <see cref="ISpatial"/> to the <paramref name="position"/></returns>
-		public static T GetClosest<T>(Vector3 position, params T[] spatials) where T : ISpatial
+        /// <summary>
+        /// Gets the closest <see cref="ISpatial"/> to a given position in the World.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="position">The position to check against.</param>
+        /// <param name="spatials">The spatials to check.</param>
+        /// <returns>The closest <see cref="ISpatial"/> to the <paramref name="position"/></returns>
+        public static T GetClosest<T>(Vector3 position, params T[] spatials) where T : ISpatial
 		{
 			ISpatial closest = null;
 			float closestDistance = 3e38f;
