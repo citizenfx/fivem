10962cea887ff23b3ae1614b7152ca44c2ccbb35
promethium <promethium@fivem.net>
2018-05-29 19:03:35 +0200

    ci: new build host is called avalon

--- code/tools/ci/build.ps1
+++ code/tools/ci/build.ps1
@@ -262,7 +262,7 @@ if (!$DontBuild)
         throw "Failed to build the code."
     }
 
-    if (($env:COMPUTERNAME -eq "BUILDVM") -and (!$IsServer)) {
+    if ((($env:COMPUTERNAME -eq "BUILDVM") -or ($env:COMPUTERNAME -eq "AVALON")) -and (!$IsServer)) {
         Start-Process -NoNewWindow powershell -ArgumentList "-ExecutionPolicy unrestricted .\tools\ci\dump_symbols.ps1 -BinRoot $BinRoot"
     }
 }
