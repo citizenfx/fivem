7214e274c7766260b47d2264cf6bea3d91ef13cd
vanadium <vanadium@fivem.net>
2018-07-08 18:46:41 +0200

    nui: disable CEF logging again

--- code/components/nui-core/src/NUIInitialize.cpp
+++ code/components/nui-core/src/NUIInitialize.cpp
@@ -111,7 +111,7 @@ void FinalizeInitNUI()
 	cSettings.remote_debugging_port = 13172;
 	cSettings.pack_loading_disabled = false; // true;
 	cSettings.windowless_rendering_enabled = false; // true;
-	cSettings.log_severity = LOGSEVERITY_DEFAULT;
+	cSettings.log_severity = LOGSEVERITY_DISABLE;
 	
 	CefString(&cSettings.browser_subprocess_path).FromWString(MakeCfxSubProcess(L"ChromeBrowser"));
 
