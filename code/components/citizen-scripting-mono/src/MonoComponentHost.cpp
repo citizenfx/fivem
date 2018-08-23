4f71b57f5eaaeb5b2b1c7c3b5dd19b768631de9b
Kevin Poirot <Hellslicer@users.noreply.github.com>
2018-08-23 13:52:44 +0200

    citizen:scripting:mono: use new profiler API
    MonoGCEvent was refactored to MonoProfilerGCEvent since mono-5.8.0.103
    https://github.com/mono/mono/commit/ea4e4a9ef6fc42570a23026adbe826cf7248290e

--- code/components/citizen-scripting-mono/src/MonoComponentHost.cpp
+++ code/components/citizen-scripting-mono/src/MonoComponentHost.cpp
@@ -143,7 +143,7 @@ static std::shared_mutex g_memoryUsagesMutex;
 
 static bool g_requestedMemoryUsage;
 
-static void gc_event(MonoProfiler *profiler, MonoGCEvent event, int generation)
+static void gc_event(MonoProfiler *profiler, MonoProfilerGCEvent event, int generation)
 {
 	switch (event) {
 	case MONO_GC_EVENT_PRE_START_WORLD:
