c6428da40867cf39f4a058404682bfad1147647c
IceHax <vecchiotom1@gmail.com>
2018-08-31 17:17:57 +0200

    discord: add ability to use custom applications and add SET_RICH_PRESENCE_ASSET and SET_RICH_PRESENCE_ID natives
    this is useful for people who want to change fivem's rich presence image, it can be set from scripts and also from the CitizenFX.ini file (DiscordId and DiscordImage)

--- code/components/discord/src/DiscordPresence.cpp
+++ code/components/discord/src/DiscordPresence.cpp
@@ -1,4 +1,7 @@
 #include <StdInc.h>
+#include <windows.h> 
+#include <tchar.h>
+#include <stdio.h> 
 #include <discord-rpc.h>
 
 #include <nutsnbolts.h>
@@ -16,7 +19,13 @@ static std::string g_richPresenceValues[8];
 
 static std::string g_richPresenceOverride;
 
+static std::string g_richPresenceOverrideAsset;
+
 static time_t g_startTime = time(nullptr);
+
+static std::string DiscordId;
+
+static std::string DiscordImage;
 
 static void UpdatePresence()
 {
@@ -40,13 +49,20 @@ static void UpdatePresence()
 		{
 			line1 = g_richPresenceOverride;
 		}
-
+		
 		DiscordRichPresence discordPresence;
 		memset(&discordPresence, 0, sizeof(discordPresence));
 		discordPresence.state = line1.c_str();
 		discordPresence.details = line2.c_str();
 		discordPresence.startTimestamp = g_startTime;
-		discordPresence.largeImageKey = "fivem_large";
+		if (!g_richPresenceOverrideAsset.empty())
+		{
+		discordPresence.largeImageKey = g_richPresenceOverrideAsset.c_str();
+		}
+		else {
+			discordPresence.largeImageKey = DiscordImage.c_str();
+		}
+		
 		Discord_UpdatePresence(&discordPresence);
 
 		g_richPresenceChanged = false;
@@ -57,7 +73,41 @@ static InitFunction initFunction([]()
 {
 	DiscordEventHandlers handlers;
 	memset(&handlers, 0, sizeof(handlers));
-	Discord_Initialize("382624125287399424", &handlers, 1, nullptr);
+	std::wstring fpath = MakeRelativeCitPath(L"CitizenFX.ini");
+	
+	if (GetFileAttributes(fpath.c_str()) == INVALID_FILE_ATTRIBUTES)
+	{
+		DiscordId = "382624125287399424";
+		DiscordImage = "fivem_large";
+	}
+	else {
+		wchar_t path[512];
+		const wchar_t* pathKey = L"DiscordId";
+		GetPrivateProfileString(L"Game", pathKey, NULL, path, _countof(path), fpath.c_str());
+		if (wcscmp(path, L"") == 0)
+		{
+			DiscordId = "382624125287399424";
+		}
+		else {
+			char DiscordIdS[512];
+			wcstombs(DiscordIdS, path, sizeof(DiscordIdS));
+			DiscordId = DiscordIdS;
+		}
+		
+		pathKey = L"DiscordImage";
+		GetPrivateProfileString(L"Game", pathKey, NULL, path, _countof(path), fpath.c_str());
+		if (wcscmp(path, L"") == 0)
+		{
+			DiscordImage = "fivem_large";
+		}
+		else {
+			char DiscordImageS[512];
+			wcstombs(DiscordImageS, path, sizeof(DiscordImageS));
+			DiscordImage = DiscordImageS;
+		}
+		
+	}
+	Discord_Initialize(DiscordId.c_str(), &handlers, 1, nullptr);
 
 	OnRichPresenceSetTemplate.Connect([](const std::string& text)
 	{
@@ -105,6 +155,42 @@ static InitFunction initFunction([]()
 		{
 			g_richPresenceOverride = "";
 		}
+
+		g_richPresenceChanged = true;
+	});
+
+	fx::ScriptEngine::RegisterNativeHandler("SET_RICH_PRESENCE_ASSET", [](fx::ScriptContext& context)
+	{
+		const char* str = context.GetArgument<const char*>(0);
+
+		if (str)
+		{
+			g_richPresenceOverrideAsset = str;
+		}
+		else
+		{
+			g_richPresenceOverrideAsset = "";
+		}
+
+		g_richPresenceChanged = true;
+	});
+
+	fx::ScriptEngine::RegisterNativeHandler("SET_RICH_PRESENCE_ID", [](fx::ScriptContext& context)
+	{
+		const char* str = context.GetArgument<const char*>(0);
+
+		if (str)
+		{
+			DiscordId = str;
+		}
+		else
+		{
+			DiscordId = "382624125287399424";
+		}
+		Discord_Shutdown();
+		DiscordEventHandlers handlers;
+		memset(&handlers, 0, sizeof(handlers));
+		Discord_Initialize(DiscordId.c_str(), &handlers, 1, nullptr);
 
 		g_richPresenceChanged = true;
 	});
