01d1ba191a0e940e49e0aa2bc6a4a020c63a1020
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-11-29 22:27:46 +0100

    Add WeatherTypeHash enum to World.cs

--- code/client/clrcore/External/World.cs
+++ code/client/clrcore/External/World.cs
@@ -132,6 +132,25 @@ namespace CitizenFX.Core
 		Christmas,
 		Halloween
 	}
+	public enum WeatherTypeHash
+	{
+		Unknown = -1,
+		ExtraSunny = -1750463879,
+		Clear = 916995460,
+		Neutral = -1530260698,
+		Smog = 282916021,
+		Foggy = -1368164796,
+		Clouds = 821931868,
+		Overcast = -1148613331,
+		Clearing = 1840358669,
+		Raining = 1420204096,
+		ThunderStorm = -1233681761,
+		Blizzard = 669657108,
+		Snowing = -273223690,
+		Snowlight = 603685163,
+		Christmas = -1429616491,
+		Halloween = -921030142
+	}
 
 	public enum CloudHat
 	{
