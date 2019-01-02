dadbe25198a2a2cf55d34e4624fd323e346d3988
beryllium <beryllium@fivem.net>
2019-01-02 21:18:01 +0100

    natives: don't silence curl [ci-skip]

--- prebuild_natives.cmd
+++ prebuild_natives.cmd
@@ -5,8 +5,8 @@ set path=C:\msys64\usr\bin;%path%
 pacman --noconfirm --needed -S make curl
 
 pushd ext\natives\
-curl -sLo out\natives_global.lua http://runtime.fivem.net/doc/natives.lua
-curl -sLo out\natives_cfx.lua http://runtime.fivem.net/doc/natives_cfx.lua
+curl -Lo out\natives_global.lua http://runtime.fivem.net/doc/natives.lua
+curl -Lo out\natives_cfx.lua http://runtime.fivem.net/doc/natives_cfx.lua
 make -j4
 xcopy /y out\*.lua ..\..\data\shared\citizen\scripting\lua
 xcopy /y out\*.js ..\..\data\shared\citizen\scripting\v8
