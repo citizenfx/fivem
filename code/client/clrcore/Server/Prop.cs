e69d21c25b9fc907b6054d8f9bb696e1fc2eaa44
BlackFlash5 <BlackFlash5@users.noreply.github.com>
2019-02-24 09:21:24 +0100

    Added server-side entitys (#187)
    * Added server-side entitys
    
    * Fixed wrong documentation on IsPositionFrozen
    
    * Fixed wrong type on Owner-property
    
    * Moved files to correct path
    
    * Return Player instead of Ped from the owner
    
    Makes more sense, because the owner of an entity is a player, not the ped of a player
    
    * Replaced Handle with m_handle

---
+++ code/client/clrcore/Server/Prop.cs
@@ -0,0 +1,13 @@
+using System;
+using CitizenFX.Core.Native;
+using System.Security;
+
+namespace CitizenFX.Core
+{
+	public sealed class Prop : Entity
+	{
+		public Prop(int handle) : base(handle)
+		{
+		}
+	}
+}
