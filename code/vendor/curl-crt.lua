362295c88384159f596ca78618cf46e804924b80
xenon <xenon@fivem.net>
2019-03-13 09:23:24 +0100

    vendor: update curl/cpr/openssl, switch curl configuration to use openssl

--- code/vendor/curl-crt.lua
+++ code/vendor/curl-crt.lua
@@ -29,24 +29,37 @@ return {
 		end
 
 		language "C"
-		kind "StaticLib"
 
 		flags "NoRuntimeChecks"
 
 		if not a then
 			staticruntime 'On'
+			kind "StaticLib"
+			
+			defines { 'CURL_STATICLIB' }
+		else
+			kind "SharedLib"
+			targetname "cfx_curl_x86_64"
 		end
 
 		-- nghttp2
 		add_dependencies 'vendor:nghttp2'
 
 		-- all the disables except http/file
-		defines { 'CURL_STATICLIB', 'BUILDING_LIBCURL', 'USE_IPV6', 'CURL_DISABLE_TFTP', 'CURL_DISABLE_FTP', 'CURL_DISABLE_LDAP', 'CURL_DISABLE_TELNET',
+		defines { 'BUILDING_LIBCURL', 'USE_IPV6', 'CURL_DISABLE_TFTP', 'CURL_DISABLE_FTP', 'CURL_DISABLE_LDAP', 'CURL_DISABLE_TELNET',
 				  'CURL_DISABLE_DICT', 'CURL_DISABLE_RTSP', 'CURL_DISABLE_POP3', 'CURL_DISABLE_IMAP', 'CURL_DISABLE_SMTP',
 				  'CURL_DISABLE_RTMP', 'CURL_DISABLE_GOPHER', 'CURL_DISABLE_SMB', 'USE_IPV6', 'USE_NGHTTP2' }
 
 		if os.istarget('windows') then
-			defines { 'USE_WINDOWS_SSPI', 'USE_SCHANNEL' }
+			defines { 'USE_WINDOWS_SSPI', 'USE_OPENSSL', 'OPENSSL_NO_ENGINE' }
+			
+			if a then
+				add_dependencies 'vendor:openssl_crypto'
+				add_dependencies 'vendor:openssl_ssl'
+			else
+				add_dependencies 'vendor:openssl_crypto_crt'
+				add_dependencies 'vendor:openssl_ssl_crt'
+			end
 		else
 			defines { 'USE_OPENSSL' }
 		end
