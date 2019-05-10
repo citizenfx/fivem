5a6a2a8a19dbda372c645c71bf73f74b12c356fe
Vegard Bendiktsen <glitchdetector@gmail.com>
2019-05-10 20:27:09 +0200

    Added missing l10n config

--- ext/cfx-ui/src/app/app.module.ts
+++ ext/cfx-ui/src/app/app.module.ts
@@ -49,7 +49,8 @@ const l10nConfig: L10nConfig = {
 	locale: {
 		languages: [
 			{ code: 'en', dir: 'ltr' },
-			{ code: 'fr', dir: 'ltr' }
+			{ code: 'fr', dir: 'ltr' },
+			{ code: 'no', dir: 'ltr' }
 		],
 		language: 'en'
 	},
