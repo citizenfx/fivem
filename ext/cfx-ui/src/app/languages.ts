0b8cfe061a494f7139df7719542558b562fa3bb1
Mickey Gee <mail@mickeygee.com>
2019-05-11 13:42:55 +0200

    Add Danish translation (locale-da)

--- ext/cfx-ui/src/app/languages.ts
+++ ext/cfx-ui/src/app/languages.ts
@@ -13,6 +13,10 @@ const languages = [
         name: 'nl',
         displayName: 'Nederlands'
     },
+    {
+        name: 'da',
+        displayName: 'Dansk'
+    },
 ];
 
 export class Languages {
