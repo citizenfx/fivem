0fc8f470274db1c8cf0aa110d25fd5eb82776254
francium <francium@fivem.net>
2019-06-23 08:17:32 +0200

    rage: code renaming

--- code/components/gta-core-five/src/GameProfiling.cpp
+++ code/components/gta-core-five/src/GameProfiling.cpp
@@ -5,28 +5,31 @@
 
 #include <optick.h>
 
-static bool(*rageInitGame)(int argc, const char** argv);
-static bool(**rageGameLoop)();
-static void(*rageShutdownGame)();
-
-int GameMain(int argc, const char** argv)
+namespace rage
 {
-	if (rageInitGame(argc, argv))
-	{
-		while (true)
-		{
-			OPTICK_FRAME("Main");
+	static bool(*CommonMain_Prologue)(int argc, const char** argv);
+	static bool(**g_pProjectMainOrDoOneLoop)();
+	static void(*CommonMain_Epilogue)();
 
-			if (!(*rageGameLoop)())
+	int CommonMain(int argc, const char** argv)
+	{
+		if (CommonMain_Prologue(argc, argv))
+		{
+			while (true)
 			{
-				break;
+				OPTICK_FRAME("Main");
+
+				if (!(*g_pProjectMainOrDoOneLoop)())
+				{
+					break;
+				}
 			}
+
+			CommonMain_Epilogue();
 		}
 
-		rageShutdownGame();
+		return 0;
 	}
-
-	return 0;
 }
 
 static void(*g_origRenderFrame)(void*);
@@ -56,11 +59,11 @@ static HookFunction hookFunction([]()
 	{
 		auto location = hook::get_pattern<char>("84 C0 74 0F FF 15 ? ? ? ? 84 C0 75 F6 E8", -9);
 
-		hook::set_call(&rageInitGame, location + 4);
-		rageGameLoop = hook::get_address<decltype(rageGameLoop)>(location + 15);
-		hook::set_call(&rageShutdownGame, location + 0x17);
+		hook::set_call(&rage::CommonMain_Prologue, location + 4);
+		rage::g_pProjectMainOrDoOneLoop = hook::get_address<decltype(rage::g_pProjectMainOrDoOneLoop)>(location + 15);
+		hook::set_call(&rage::CommonMain_Epilogue, location + 0x17);
 
-		hook::jump(location, GameMain);
+		hook::jump(location, rage::CommonMain);
 	}
 
 	// render thread
