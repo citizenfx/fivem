6f767dff292c2ce8b9a37d31d3b0ab60ed5e722f
Źmićer Rubinštejn <rubinsteindb@gmail.com>
2019-07-22 23:43:24 +0300

    Fixing setTick return type

--- data/shared/citizen/scripting/v8/index.d.ts
+++ data/shared/citizen/scripting/v8/index.d.ts
@@ -77,7 +77,7 @@ declare function TriggerClientEvent(eventName: string, target: number|string, ..
 
 declare function removeEventListener(eventName: string, callback: Function): void
 
-declare function setTick(callback: Function): void
+declare function setTick(callback: Function): number
 declare function clearTick(id: number): void
 
 declare var exports: any;
