d1ec7d4459a33e6d4828f94ba9105f96c17f2876
francium <francium@fivem.net>
2019-08-03 10:09:28 +0200

    tools/ci: hardwire Linux Mono version

--- code/tools/ci/build_server_2.sh
+++ code/tools/ci/build_server_2.sh
@@ -32,7 +32,7 @@ apk del curl
 apk add --no-cache libc++ curl=7.63.0-r99 libssl1.1 libunwind libstdc++ zlib c-ares icu-libs v8
 
 # install compile-time dependencies
-apk add --no-cache --virtual .dev-deps libc++-dev curl-dev=7.63.0-r99 clang clang-dev build-base linux-headers openssl-dev python2 py2-pip lua5.3 lua5.3-dev mono-dev libmono mono-corlib mono mono-reference-assemblies-4.x mono-reference-assemblies-facades mono-csc c-ares-dev v8-dev
+apk add --no-cache --virtual .dev-deps libc++-dev curl-dev=7.63.0-r99 clang clang-dev build-base linux-headers openssl-dev python2 py2-pip lua5.3 lua5.3-dev mono-dev=5.16.1.0-r9990 libmono=5.16.1.0-r9990 mono-corlib=5.16.1.0-r9990 mono=5.16.1.0-r9990 mono-reference-assemblies-4.x=5.16.1.0-r9990 mono-reference-assemblies-facades=5.16.1.0-r9990 mono-csc=5.16.1.0-r9990 c-ares-dev v8-dev
 
 # install ply
 pip install ply
