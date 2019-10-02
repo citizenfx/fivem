c5eaae94521d369b771e5cb185f67628e81fee18
Samuel Ds <samuelds.pro@gmail.com>
2019-10-02 19:18:26 +0200

    Update data/shared/citizen/scripting/v8/timer.js
    Add catch
    
    Co-Authored-By: Kevin Poirot <Hellslicer@users.noreply.github.com>

--- data/shared/citizen/scripting/v8/timer.js
+++ data/shared/citizen/scripting/v8/timer.js
@@ -93,7 +93,7 @@
             function() {
                 try {
                     callback(...argsForCallback);
-                }
+                } catch { }
             },
             interval
         );
