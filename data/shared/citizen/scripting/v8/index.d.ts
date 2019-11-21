6e054c6cb9c087a8e8e80eadd482b4aa4acd38c1
pichotm <8015411+PichotM@users.noreply.github.com>
2019-11-21 16:03:39 -0500

    index.d.ts: add missing declarations
    - resultAsLong
    - resultAsObject
    - clearTick
    - source

--- data/shared/citizen/scripting/v8/index.d.ts
+++ data/shared/citizen/scripting/v8/index.d.ts
@@ -10,6 +10,8 @@ interface ResultAsInteger {}
 interface ResultAsFloat {}
 interface ResultAsString {}
 interface ResultAsVector {}
+interface ResultAsLong {}
+interface ResultAsObject {}
 
 type InputArgument =
     string |
@@ -23,7 +25,9 @@ type InputArgument =
     ResultAsInteger |
     ResultAsFloat |
     ResultAsString |
-    ResultAsVector;
+    ResultAsVector |
+    ResultAsLong |
+    ResultAsObject;
 
 interface CitizenInterface {
     trace(...args: string[]): void
@@ -51,6 +55,8 @@ interface CitizenInterface {
     resultAsFloat(): ResultAsFloat
     resultAsString(): ResultAsString
     resultAsVector(): ResultAsVector
+    resultAsLong(): ResultAsLong
+    rsesultAsObject(): ResultAsObject
 
     makeRefFunction(refFunction: Function): string
 }
@@ -80,5 +86,8 @@ declare function TriggerLatentClientEvent(eventName: string, target: number|stri
 declare function removeEventListener(eventName: string, callback: Function): void
 
 declare function setTick(callback: Function): void
+declare function clearTick(callback: Function): void
 
 declare var exports: any;
+
+declare var source: string;
