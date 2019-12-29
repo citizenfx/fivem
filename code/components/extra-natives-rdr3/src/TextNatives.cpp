8c72907df648ebdd487b7cb748224815ef6b307d
Miyu <robensyahirzamaulana08@gmail.com>
2019-12-29 23:25:35 +0700

    Added SET_TEXT_JUSTIFICATION and SET_TEXT_WRAP native
    Adds Text Justification and Text Wrap native to draw text right justified / to the right of the screen

--- code/components/extra-natives-rdr3/src/TextNatives.cpp
+++ code/components/extra-natives-rdr3/src/TextNatives.cpp
@@ -2,16 +2,55 @@
 #include <ScriptEngine.h>
 #include <Hooking.h>
 
-static int* g_textFontForCurrentCommand;
+struct ccolor
+{
+	uint8_t	r, g, b, a;
+};
+
+struct text_info_t
+{
+	ccolor	color;
+	float	text_scale;
+	float	text_scale2;
+	float	wrap_start;
+	float	wrap_end;
+	int		font;
+	uint8_t pad[0x4];
+	WORD	justification;
+};
+
+static text_info* g_TextInfo;
 
 static HookFunction textNativesFunc([]()
 {
-	g_textFontForCurrentCommand = hook::get_address<int*>(hook::get_pattern("48 69 C1 08 7D 00 00", 17));
+	g_TextInfoPtr = hook::get_address<text_info_t*>(hook::get_pattern("48 8D 05 ? ? ? ? 48 89 44 24 ? 8B 05 ? ? ? ? 89 44 24 28 8B 05 ? ? ? ? 89 44 24 20", 3));
 
 	fx::ScriptEngine::RegisterNativeHandler("SET_TEXT_FONT_FOR_CURRENT_COMMAND", [](fx::ScriptContext& scriptContext)
 	{
 		auto fontId = scriptContext.GetArgument<int>(0);
 
-		*g_textFontForCurrentCommand = fontId;
+		g_TextInfo->font = fontId;
+	});
+	
+	fx::ScriptEngine::RegisterNativeHandler("SET_TEXT_JUSTIFICATION", [](fx::ScriptContext& scriptContext)
+	{
+		/* Text Flags are:
+			0 = Centre,
+			1 = None/Left,
+			2 = Right
+		*/
+		
+		auto justification = scriptContext.GetArgument<int>(0);
+
+		g_TextInfo->justification = justification;
+	});
+	
+	fx::ScriptEngine::RegisterNativeHandler("SET_TEXT_WRAP", [](fx::ScriptContext& scriptContext)
+	{
+		auto start = scriptContext.GetArgument<float>(0);
+		auto end = scriptContext.GetArgument<float>(1);
+		
+		g_TextInfo->wrap_start = start;
+		g_TextInfo->wrap_end = end;
 	});
 });
