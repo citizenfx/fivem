0105b71a041892c9b4f2ea39f3d384a256619e2e
zirconium <zirconium@fivem.net>
2020-01-27 16:28:05 +0100

    prebuild: add ply globally

--- code/prebuild_misc.cmd
+++ code/prebuild_misc.cmd
@@ -1,6 +1,7 @@
 @echo off
 setlocal
-python -m easy_install jinja2
+:: TODO: make mojo wrappers that use this from deps
+python -m easy_install jinja2 ply
 set PYTHONPATH=code\tools\idl\deps\
 mkdir code\tools\idl\deps
 python -m easy_install -d code\tools\idl\deps\ ply jinja2
