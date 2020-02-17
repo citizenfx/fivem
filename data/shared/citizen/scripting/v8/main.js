44e9f08cb511d622f6bb7a10eecf2a67ee99ca39
zirconium <zirconium@fivem.net>
2020-02-17 17:28:44 +0100

    fix: v8: undo accidentally staged change

--- data/shared/citizen/scripting/v8/main.js
+++ data/shared/citizen/scripting/v8/main.js
@@ -297,7 +297,7 @@ const EXT_LOCALFUNCREF = 11;
 		return frames;
 	}
 	
-	//Error.prepareStackTrace = prepareStackTrace;
+	Error.prepareStackTrace = prepareStackTrace;
 	
 	class StackDumpError {
 		constructor(bs, be) {
