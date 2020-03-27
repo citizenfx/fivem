babb627a8b645b05ac3288eab3e1018a6d2a496f
zirconium <zirconium@fivem.net>
2020-03-27 22:42:44 +0100

    fix: Discord authentication in consent mode (early WIP)

--- code/components/discord/src/DiscordIdentityData.cpp
+++ code/components/discord/src/DiscordIdentityData.cpp
@@ -1,10 +1,9 @@
 #include <StdInc.h>
 
 #ifdef GTA_FIVE
-#include <LegitimacyAPI.h>
+#define CNL_ENDPOINT "https://lambda.fivem.net"
 
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
+#include <LegitimacyAPI.h>
 
 #include <json.hpp>
 
@@ -12,164 +11,168 @@
 
 #include <HttpClient.h>
 
-using client = websocketpp::client<websocketpp::config::asio_client>;
-using message_ptr = websocketpp::config::asio_client::message_type::ptr;
-
-constexpr auto MIN_PORT = 6463;
-constexpr auto MAX_PORT = 6472;
-
 using json = nlohmann::json;
 
 static HookFunction initFunction([]()
 {
 	std::thread([]()
 	{
-		try
+		HANDLE hPipe = INVALID_HANDLE_VALUE;
+
+		for (int i = 0; i < 10; i++)
 		{
-			client c;
-			c.init_asio();
+			hPipe = CreateFileW(va(L"\\\\.\\pipe\\discord-ipc-%d", i), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
-			static int curPort;
-
-			auto try_port = [&c](int port)
+			if (hPipe != INVALID_HANDLE_VALUE)
 			{
-				curPort = port;
+				break;
+			}
+		}
 
-				websocketpp::lib::error_code ec;
-				auto con = c.get_connection(fmt::sprintf("ws://127.0.0.1:%d/?v=1", port), ec);
+		if (hPipe == NULL || hPipe == INVALID_HANDLE_VALUE)
+		{
+			return;
+		}
 
-				if (!ec)
-				{
-					// this enables top secret internal RPC mode
-					con->append_header("origin", "https://discordapp.com");
-					c.connect(con);
-				}
-			};
+		auto writePipe = [hPipe](int opCode, const json& data)
+		{
+			auto dataStr = data.dump();
 
-			c.set_fail_handler([&try_port](websocketpp::connection_hdl hdl)
+			std::vector<uint8_t> dataBuffer(dataStr.length() + 4 + 4);
+			*(uint32_t*)&dataBuffer[0] = opCode;
+			*(uint32_t*)&dataBuffer[4] = dataStr.length();
+			memcpy(&dataBuffer[8], dataStr.data(), dataStr.length());
+
+			DWORD bytesWritten;
+			WriteFile(hPipe, dataBuffer.data(), dataBuffer.size(), &bytesWritten, NULL);
+		};
+
+		writePipe(0 /* HANDSHAKE */, json::object({
+			{ "v", 1},
+			{ "client_id", "382624125287399424"}
+		}));
+
+		bool close = false;
+
+		auto closeConnection = [&writePipe, &close, hPipe]()
+		{
+			writePipe(2, {});
+			CloseHandle(hPipe);
+		};
+
+		auto handleMsg = [&writePipe, &closeConnection](int opCode, const json& data)
+		{
+			switch (opCode)
 			{
-				if (curPort == MAX_PORT)
+			case 1: // FRAME
+				if (data["evt"] == "READY")
 				{
-					return;
-				}
+					auto userId = data["data"]["user"]["id"].get<std::string>();
 
-				try_port(curPort + 1);
-			});
-
-			c.set_message_handler([&c](websocketpp::connection_hdl hdl, message_ptr msg)
-			{
-				auto obj = json::parse(msg->get_payload());
-
-				auto cmd = obj.value("cmd", "");
-				if (cmd == "DISPATCH")
-				{
-					auto evt = obj.value("evt", "");
-
-					if (evt == "READY")
-					{
-						// send overlay handshake
-						auto overlaySubscribe = json::object({
-							{"cmd", "SUBSCRIBE"},
-							{"args", json::object()},
-							{"evt", "OVERLAY"},
-							{"nonce", "nonce"}
-						});
-
-						auto overlayInit = json::object({
-							{"cmd", "OVERLAY"},
-							{"args", json::object(
-								{
-									{"type", "CONNECT"},
-									{"pid", -1}
-								}
-							)},
-							{"nonce", "nonce2"}
-						});
-
-						websocketpp::lib::error_code ec;
-						c.send(hdl, overlaySubscribe.dump(), websocketpp::frame::opcode::text, ec);
-						c.send(hdl, overlayInit.dump(), websocketpp::frame::opcode::text, ec);
-					}
-					else if (evt == "OVERLAY")
-					{
-						auto& data = obj["data"];
-
-						if (data["type"] == "DISPATCH")
+					// check with CnL if we have access
+					Instance<::HttpClient>::Get()->DoPostRequest(
+						CNL_ENDPOINT "/api/validate/discord",
 						{
-							auto& payloads = data["payloads"];
+							{ "entitlementId", ros::GetEntitlementSource() },
+							{ "userId", userId }
+						},
+						[writePipe, closeConnection](bool success, const char* data, size_t length)
+					{
+						if (!success && strstr(data, "HTTP 4") != nullptr)
+						{
+							writePipe(1 /* FRAME */, json::object({
+								{ "cmd", "AUTHORIZE" },
+								{ "args", json::object({
+									{ "scopes", json::array({"identify"}) },
+									{ "client_id", "382624125287399424" },
+									{ "redirect_url", "https://cfx.re" },
+									{ "prompt", "none" },
+								}) },
+								{ "nonce", "nonce1" },
+								}));
+						}
+						else
+						{
+							closeConnection();
+						}
+					});
+				}
+				else if (data["nonce"] == "nonce1")
+				{
+					if (data["evt"] == "ERROR")
+					{
+						// user probably denied auth request in Discord UI
+						// #TODO: store this and only ask again when asked by CfxUI
+						closeConnection();
+					}
+					else
+					{
+						auto code = data["data"]["code"].get<std::string>();
 
-							for (auto& payload : payloads)
+						Instance<::HttpClient>::Get()->DoPostRequest(
+							CNL_ENDPOINT "/api/validate/discord",
 							{
-								if (payload["type"] == "OVERLAY_INITIALIZE")
-								{
-									auto token = payload["token"].get<std::string>();
+								{ "entitlementId", ros::GetEntitlementSource() },
+								{ "authCode", code }
+							},
+							[](bool, const char*, size_t)
+						{
+							
+						});
 
-									auto authRequest = json::object({
-										{ "permissions", 0 },
-										{ "authorize", true }
-									});
+						closeConnection();
+					}
+				}
+				break;
+			case 3: // PING
+				writePipe(4 /* PONG */, data);
+				break;
+			}
+		};
 
-									c.close(hdl, 0, "OK");
+		while (true)
+		{
+			if (close)
+			{
+				break;
+			}
 
-									HttpRequestOptions options;
-									options.headers.insert({ "Content-Type", "application/json" });
-									options.headers.insert({ "Authorization", token });
+			// read from pipe
+			struct  
+			{
+				uint32_t opCode;
+				uint32_t length;
+			} pktHdr;
 
-									Instance<HttpClient>::Get()->DoPostRequest(
-										"https://discordapp.com/api/v6/oauth2/authorize?client_id=530784411080458270&response_type=code&scope=identify",
-										authRequest.dump(),
-										options,
-										[](bool result, const char* data, size_t len)
-									{
-										if (result)
-										{
-											try
-											{
-												auto obj = json::parse(std::string(data, len));
-												auto location = obj.value("location", "");
+			DWORD bytesAvail = 0;
 
-												if (!location.empty())
-												{
-													auto uri = skyr::make_url(location);
-													
-													if (uri)
-													{
-														auto queryStr = uri->search();
-														queryStr = queryStr.substr(queryStr.find_first_of("=") + 1);
+			if (PeekNamedPipe(hPipe, NULL, 0, NULL, &bytesAvail, NULL) && bytesAvail > 0)
+			{
+				DWORD numRead = 0;
+				ReadFile(hPipe, &pktHdr, sizeof(pktHdr), &numRead, NULL);
 
-														Instance<::HttpClient>::Get()->DoPostRequest(
-															"https://lambda.fivem.net/api/validate/discord",
-															{
-																{ "entitlementId", ros::GetEntitlementSource() },
-																{ "authCode", queryStr }
-															},
-															[](bool, const char*, size_t)
-														{
+				if (numRead == sizeof(pktHdr) && pktHdr.length > 0)
+				{
+					std::vector<uint8_t> buf(pktHdr.length);
+					ReadFile(hPipe, buf.data(), buf.size(), &numRead, NULL);
 
-														});
-													}
-												}
-											}
-											catch (std::exception& e)
-											{
+					if (numRead == buf.size())
+					{
+						try
+						{
+							handleMsg(pktHdr.opCode, json::parse(buf));
+						}
+						catch (json::exception& e)
+						{
 
-											}
-										}
-									});
-								}
-							}
 						}
 					}
 				}
-			});
-
-			try_port(MIN_PORT);
-
-			c.run();
-		}
-		catch (websocketpp::exception const& e)
-		{
-			trace("discord auth failed: %s\n", e.what());
+			}
+			else
+			{
+				Sleep(50);
+			}
 		}
 	}).detach();
 });
