2aca421ad1a2f523ddd63f19de701df60eaa8096
zirconium <zirconium@fivem.net>
2020-04-05 09:47:09 +0200

    feature: build txAdmin for Linux as well

--- ext/system-resources/build.sh
+++ ext/system-resources/build.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
+apk add --no-cache nodejs npm
+
 cd ../webadmin/server/
 dotnet publish -c Release
 cd ../../system-resources/
 
-cd ../monitor/server/
-dotnet publish -c Release
-cd ../../system-resources/
+cd ../txAdmin/
+npm i
+./node_modules/.bin/webpack --config webpack.config.js --progress
+cd ../system-resources/
 
 mkdir -p data/webadmin/wwwroot/
 mkdir -p data/webadmin/server/bin/Release/netstandard2.0/publish/
@@ -16,7 +19,4 @@ cp -a ../webadmin/fxmanifest.lua data/webadmin/
 cp -a ../webadmin/wwwroot/ data/webadmin/
 cp -a ../webadmin/server/bin/Release/netstandard2.0/publish/ data/webadmin/server/bin/Release/netstandard2.0/
 
-mkdir -p data/monitor/server/bin/Release/netstandard2.0/publish/
-cp -a ../monitor/fxmanifest.lua data/monitor/
-
-cp -a ../monitor/server/bin/Release/netstandard2.0/publish/ data/monitor/server/bin/Release/netstandard2.0/
+cp -a ../txAdmin/dist data/monitor
