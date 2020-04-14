88adf683f2c272c2c9247aee00576994ebbf88d5
zirconium <zirconium@fivem.net>
2020-04-14 19:59:19 +0200

    fix: add MarkupSafe=1.1.1 to prebuild for python2 compatibility

--- code/prebuild_misc.cmd
+++ code/prebuild_misc.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 :: TODO: make mojo wrappers that use this from deps
-python -m easy_install jinja2==2.11.1 ply
+python -m easy_install jinja2==2.11.1 ply MarkupSafe==1.1.1
 set PYTHONPATH=code\tools\idl\deps\
 mkdir code\tools\idl\deps
-python -m easy_install -d code\tools\idl\deps\ ply jinja2==2.11.1
+python -m easy_install -d code\tools\idl\deps\ ply jinja2==2.11.1 MarkupSafe==1.1.1
 endlocal
