ca5d185499d84cbc4f875dfebacb325e10f66cb7
zirconium <zirconium@fivem.net>
2020-04-15 08:05:09 +0200

    fix: reorder python dependencies so resolution doesn't break

--- code/prebuild_misc.cmd
+++ code/prebuild_misc.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 :: TODO: make mojo wrappers that use this from deps
-python -m easy_install jinja2==2.11.1 ply MarkupSafe==1.1.1
+python -m easy_install  MarkupSafe==1.1.1 jinja2==2.11.1 ply
 set PYTHONPATH=code\tools\idl\deps\
 mkdir code\tools\idl\deps
-python -m easy_install -d code\tools\idl\deps\ ply jinja2==2.11.1 MarkupSafe==1.1.1
+python -m easy_install -d code\tools\idl\deps\ MarkupSafe==1.1.1 ply jinja2==2.11.1
 endlocal
