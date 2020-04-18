ad7675451b9e4865fb5bf197785a02e1dee17365
zirconium <zirconium@fivem.net>
2020-04-18 17:08:58 +0200

    fix: prevent loading of reshade <3.1 post-load as well

--- code/components/rage-graphics-five/src/ReShadeFixups.cpp
+++ code/components/rage-graphics-five/src/ReShadeFixups.cpp
@@ -10,6 +10,8 @@
 #include "Hooking.h"
 #include "DrawCommands.h"
 
+#pragma comment(lib, "version.lib")
+
 static HMODULE LoadLibraryAHook(const char* libName)
 {
 	if (strcmp(libName, "dxgi.dll") == 0)
@@ -18,11 +20,32 @@ static HMODULE LoadLibraryAHook(const char* libName)
 
 		if (GetFileAttributes(refPath.c_str()) != INVALID_FILE_ATTRIBUTES)
 		{
-			auto hMod = LoadLibraryW(refPath.c_str());
+			// check if viable
+			DWORD versionInfoSize = GetFileVersionInfoSize(refPath.c_str(), nullptr);
 
-			if (hMod)
+			if (versionInfoSize)
 			{
-				return hMod;
+				std::vector<uint8_t> versionInfo(versionInfoSize);
+
+				if (GetFileVersionInfo(refPath.c_str(), 0, versionInfo.size(), &versionInfo[0]))
+				{
+					void* fixedInfoBuffer;
+					UINT fixedInfoSize = 0;
+
+					VerQueryValue(&versionInfo[0], L"\\", &fixedInfoBuffer, &fixedInfoSize);
+
+					VS_FIXEDFILEINFO* fixedInfo = reinterpret_cast<VS_FIXEDFILEINFO*>(fixedInfoBuffer);
+
+					if (fixedInfo->dwProductVersionMS >= 0x30001)
+					{
+						auto hMod = LoadLibraryW(refPath.c_str());
+
+						if (hMod)
+						{
+							return hMod;
+						}
+					}
+				}
 			}
 		}
 	}
