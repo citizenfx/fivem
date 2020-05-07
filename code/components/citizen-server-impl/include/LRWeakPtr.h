1f3100e071f459492245d4d9f6d752474ff1c6dc
Karl <klrehm123@gmail.com>
2020-05-07 17:45:07 -0400

    create LRWeakPtr.h - left/right weak_ptr allowing for...
    ...lock free reads/writes - reads always and writes if writes are done in one thread

---
+++ code/components/citizen-server-impl/include/LRWeakPtr.h
@@ -0,0 +1,35 @@
+#include <memory>
+#include <xenium/left_right.hpp>
+
+template<typename T>
+struct LRWeakPtr
+{
+	using TWeakPtr = std::weak_ptr<T>;
+
+	void reset()
+	{
+		_ptr.update([](auto& weakPtr)
+			{
+				weakPtr.reset();
+			});
+	}
+
+	void update(const TWeakPtr& newPtr)
+	{
+		_ptr.update([](auto& weakPtr)
+			{
+				weakPtr = newPtr;
+			});
+	}
+
+	std::shared_ptr<T> lock()
+	{
+		return _ptr.read([](auto& weakPtr)
+			{
+				return weakPtr.lock();
+			});
+	}
+
+private:
+	xenium::left_right<TWeakPtr> _ptr;
+};
