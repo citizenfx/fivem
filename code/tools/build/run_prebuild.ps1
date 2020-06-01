3e4af1cd08f938a452965f331cbfa73f951f71ea
zirconium <zirconium@fivem.net>
2020-06-01 11:18:44 +0200

    fix: no, 'Python 2.7.15' is not an error

--- code/tools/build/run_prebuild.ps1
+++ code/tools/build/run_prebuild.ps1
@@ -1,11 +1,12 @@
 $InstRoot = "$PSScriptRoot\..\..\..\"
-$ErrorActionPreference = "Stop"
 
 if (([string](python --version 2>&1)).StartsWith("Python 3")) {
     "PREBUILD : error PY27 : `python` in PATH is Python 3, not Python 2.
 Please set PATH for MSBuild/VS to contain Python 2.7 before Python 3.x." | Write-Host
     return 1;
 }
+
+$ErrorActionPreference = "Stop"
 
 if (!(Test-Path $InstRoot\code\client\clrcore\NativesFive.cs)) {
     Invoke-Expression "$InstRoot\prebuild_natives.cmd"
