e10de6fd63e11a8a54c7fba13e6748610b479053
zirconium <zirconium@fivem.net>
2020-06-25 23:36:37 +0200

    tweak: cfx-ui: update remaining npm packages

--- ext/cfx-ui/src/polyfills.ts
+++ ext/cfx-ui/src/polyfills.ts
@@ -41,8 +41,7 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
