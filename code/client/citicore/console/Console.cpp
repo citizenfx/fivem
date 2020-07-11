39956c369ab85b1b88c244ae65f3e198f59a203c
zirconium <zirconium@fivem.net>
2020-07-11 21:02:33 +0200

    feature: console buffer/tokenization supporting JSON syntax validation

--- code/client/citicore/console/Console.cpp
+++ code/client/citicore/console/Console.cpp
@@ -12,6 +12,91 @@
 
 #include <console/ConsoleWriter.h>
 
+#if __has_include(<json.hpp>)
+#define CONSOLE_JSON_PARSE
+
+#include <json.hpp>
+
+using json = nlohmann::json;
+
+/*!
+Input adapter for a (caching) istream. Ignores a UFT Byte Order Mark at
+beginning of input. Does not support changing the underlying std::streambuf
+in mid-input. Maintains underlying std::istream and std::streambuf to support
+subsequent use of standard std::istream operations to process any input
+characters following those used in parsing the JSON input.  Clears the
+std::istream flags; any input errors (e.g., EOF) will be detected by the first
+subsequent call for input from the std::istream.
+*/
+template<typename TChar>
+class input_stream_adapter_any
+{
+public:
+	using char_type = TChar;
+
+	~input_stream_adapter_any()
+	{
+		// clear stream flags; we use underlying streambuf I/O, do not
+		// maintain ifstream flags, except eof
+		if (is)
+		{
+			is->clear(is->rdstate() & std::ios::eofbit);
+		}
+	}
+
+	explicit input_stream_adapter_any(std::basic_istream<TChar>& i)
+		: is(&i), sb(i.rdbuf())
+	{
+	}
+
+	// delete because of pointer members
+	input_stream_adapter_any(const input_stream_adapter_any&) = delete;
+	input_stream_adapter_any& operator=(input_stream_adapter_any&) = delete;
+	input_stream_adapter_any& operator=(input_stream_adapter_any&& rhs) = delete;
+
+	input_stream_adapter_any(input_stream_adapter_any&& rhs)
+		: is(rhs.is), sb(rhs.sb)
+	{
+		rhs.is = nullptr;
+		rhs.sb = nullptr;
+	}
+
+	// std::istream/std::streambuf use std::char_traits<char>::to_int_type, to
+	// ensure that std::char_traits<char>::eof() and the character 0xFF do not
+	// end up as the same value, eg. 0xFFFFFFFF.
+	auto get_character()
+	{
+		auto res = sb->sbumpc();
+		// set eof manually, as we don't use the istream interface.
+		if (res == EOF)
+		{
+			is->clear(is->rdstate() | std::ios::eofbit);
+		}
+		return res;
+	}
+
+private:
+	/// the associated input stream
+	std::basic_istream<TChar>* is = nullptr;
+	std::basic_streambuf<TChar>* sb = nullptr;
+};
+
+namespace nlohmann::detail
+{
+template<typename TChar>
+inline input_stream_adapter_any<TChar> input_adapter(std::basic_istream<TChar>& stream)
+{
+	return input_stream_adapter_any<TChar>(stream);
+}
+
+template<typename TChar>
+inline input_stream_adapter_any<TChar> input_adapter(std::basic_istream<TChar>&& stream)
+{
+	return input_stream_adapter_any<TChar>(stream);
+}
+}
+#endif
+
 static console::IWriter* g_writer;
 extern bool GIsPrinting();
 
@@ -182,13 +267,35 @@ void Context::ExecuteBuffer()
 		while (m_commandBuffer.length() > 0)
 		{
 			// parse the command up to the first occurrence of a newline/semicolon
-			int i        = 0;
+			size_t i     = 0;
 			bool inQuote = false;
 
 			size_t cbufLength = m_commandBuffer.length();
 
 			for (i = 0; i < cbufLength; i++)
 			{
+#ifdef CONSOLE_JSON_PARSE
+				if (!inQuote && (m_commandBuffer[i] == '{' || m_commandBuffer[i] == '['))
+				{
+					std::stringstream oss;
+					oss << m_commandBuffer.substr(i);
+					oss.seekg(0);
+
+					try
+					{
+						json j;
+						oss >> j;
+
+						i += oss.tellg();
+						i -= 1;
+						continue;
+					}
+					catch (json::exception&)
+					{
+					}
+				}
+#endif
+
 				if (m_commandBuffer[i] == '"')
 				{
 					inQuote = !inQuote;
@@ -435,6 +542,31 @@ ProgramArguments Tokenize(const std::string& lineUtf8)
 
 		// there's a new argument on the block
 		std::basic_stringstream<typename ProgramArguments::TCharType> arg;
+
+#ifdef CONSOLE_JSON_PARSE
+		if ((line[i] == '{' || line[i] == '['))
+		{
+			std::basic_stringstream<typename ProgramArguments::TCharType> oss;
+			oss << line.substr(i);
+			oss.seekg(0);
+
+			try
+			{
+				json j;
+				oss >> j;
+
+				int start = i;
+				i += oss.tellg();
+
+				args.push_back(line.substr(start, i - start));
+
+				continue;
+			}
+			catch (json::exception&)
+			{
+			}
+		}
+#endif
 
 		// quoted strings
 		if (line[i] == U'"')
