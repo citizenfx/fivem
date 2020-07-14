ad9b018b8abe4f3aa2bfb8dc98c254dc39a74ca9
zirconium <zirconium@fivem.net>
2020-07-14 02:03:46 +0200

    Revert "Revert "fix: correctly propagate/display RCDv2 fetch errors for early-load""
    This reverts commit a928633c9330af617e5475c4e831a39a858bd44f.

--- code/components/gta-streaming-five/src/PatchStreamingPreparation.cpp
+++ code/components/gta-streaming-five/src/PatchStreamingPreparation.cpp
@@ -299,12 +299,12 @@ static void* pgStreamerRead(uint32_t handle, datResourceChunk* outChunks, int nu
 					}
 					else
 					{
-						std::string error;
+						std::string errorRef;
 						ICoreGameInit* init = Instance<ICoreGameInit>::Get();
 
-						init->GetData("gta-core-five:loadCaller", &error);
+						init->GetData("gta-core-five:loadCaller", &errorRef);
 
-						FatalError("Failed to request %s: %s. %s", fileName, vfs::GetLastError(vfs::GetDevice(fileName)), error);
+						FatalError("Failed to request %s: %s. %s", fileName, error, errorRef);
 					}
 				};
 
