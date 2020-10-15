a7972af5595ce81394ea3d21ea2c1a585ab556e9
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-10-15 12:45:03 +0200

    Update component.lua

--- code/components/devtools-rdr3/component.lua
+++ code/components/devtools-rdr3/component.lua
@@ -1,1 +1,18 @@
+includedirs {
+	'../devtools-five/include/',
+}
 
+
+return function()
+	configuration {}
+	filter {}
+
+	files {
+		'components/devtools-five/include/Local.h',
+		'components/devtools-five/include/imguilistview.h',
+		'components/devtools-five/src/ResourceTimeWarnings.cpp',
+		'components/devtools-five/src/imguilistview.cpp',
+		'components/devtools-five/src/Component.cpp',
+		'components/devtools-five/src/NetEventLog.cpp',
+	}
+end
