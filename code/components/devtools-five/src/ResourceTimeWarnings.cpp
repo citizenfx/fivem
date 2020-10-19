539d5df6570b49dd82e04e03909ca2ca2fd6c418
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-10-19 08:50:06 +0200

    Update ResourceTimeWarnings.cpp

--- code/components/devtools-five/src/ResourceTimeWarnings.cpp
+++ code/components/devtools-five/src/ResourceTimeWarnings.cpp
@@ -325,7 +325,7 @@ static InitFunction initFunction([]()
 
 					ImGui::Text("%s", resource->GetName().c_str());
 					ImGui::NextColumn();
-		
+
 					auto metric = metrics.find(resourceName);
 
 					if (metric != metrics.end() && metric->second)
