460044c3c639e6c8260c6e754460e05780749316
blattersturm <peachypies@protonmail.ch>
2020-12-04 23:44:17 +0100

    fix(conhost/v2): don't link xinput9_1_0.dll for server compatibility

--- code/components/conhost-v2/src/backends/imgui_impl_win32.cpp
+++ code/components/conhost-v2/src/backends/imgui_impl_win32.cpp
@@ -24,6 +24,8 @@
 
 #pragma comment(lib, "dwmapi")
 
+#define IMGUI_IMPL_WIN32_DISABLE_GAMEPAD
+
 // Using XInput library for gamepad (with recent Windows SDK this may leads to executables which won't run on Windows 7)
 #ifndef IMGUI_IMPL_WIN32_DISABLE_GAMEPAD
 #include <XInput.h>
