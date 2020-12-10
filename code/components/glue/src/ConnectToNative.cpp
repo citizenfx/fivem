citizenfx/fivem

commit afc568a98d07a734c03ee839c8372556565b8647

fix(glue): register protocol in game process, not master
Performance reordering might've made it so that glue doesn't participate in early startup.

code/components/glue/src/ConnectToNative.cpp
@@ -1012,7 +1012,7 @@ void Component_RunPreInit()
 {
 	static HostSharedData<CfxState> hostData("CfxInitState");
 
-	if (hostData->IsMasterProcess())
+	if (hostData->IsGameProcess())
 	{
 		ProtocolRegister();
 	}

commit 5306a40bf66e8f8c432ae1028cbe4e07543496c1

fix(glue): fix protocol handler, again
This broke even more in afc568a98d07a734c03ee839c8372556565b8647 since it would register the game subprocess, and in _DEBUG it would just not run at all.

code/components/glue/src/ConnectToNative.cpp
@@ -973,12 +973,11 @@ static void ProtocolRegister()
 		return; \
 	}
 
-	HKEY key;
-	wchar_t path[MAX_PATH];
-	wchar_t command[1024];
+	static HostSharedData<CfxState> hostData("CfxInitState");
 
-	GetModuleFileNameW(NULL, path, sizeof(path));
-	swprintf_s(command, L"\"%s\" \"%%1\"", path);
+	HKEY key;
+	wchar_t command[1024];
+	swprintf_s(command, L"\"%s\" \"%%1\"", hostData->gameExePath);
 
 	CHECK_STATUS(RegCreateKeyW(HKEY_CURRENT_USER, L"SOFTWARE\\Classes\\fivem", &key));
 	CHECK_STATUS(RegSetValueExW(key, NULL, 0, REG_SZ, (BYTE*)L"FiveM", 6 * 2));
@@ -1023,7 +1022,11 @@ void Component_RunPreInit()
 {
 	static HostSharedData<CfxState> hostData("CfxInitState");
 
+#ifndef _DEBUG
 	if (hostData->IsGameProcess())
+#else
+	if (hostData->IsMasterProcess())
+#endif
 	{
 		ProtocolRegister();
 	}