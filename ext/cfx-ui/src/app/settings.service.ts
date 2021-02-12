b292bacbc4870144e3442226c0e532b56162816b
Ktoś <43894510+Ktos93@users.noreply.github.com>
2021-02-12 20:14:40 +0100

    fix formating

--- ext/cfx-ui/src/app/settings.service.ts
+++ ext/cfx-ui/src/app/settings.service.ts
@@ -238,18 +238,18 @@ export class SettingsService {
 				category: '#SettingsCat_Account',
 			});
 		}
-			this.addSetting('updateChannel', {
-				name: '#Settings_UpdateChannel',
-				description: '#Settings_UpdateChannelDesc',
-				type: 'switch',
-				getCb: () => this.gameService.getConvar('ui_updateChannel'),
-				setCb: (value) => this.gameService.setConvar('ui_updateChannel', value),
-				options: {
-					'production': 'Release',
-					'canary': 'Canary (Experimental/Unstable)',
-				},
-				category: '#SettingsCat_Game',
-			});
+		this.addSetting('updateChannel', {
+			name: '#Settings_UpdateChannel',
+			description: '#Settings_UpdateChannelDesc',
+			type: 'switch',
+			getCb: () => this.gameService.getConvar('ui_updateChannel'),
+			setCb: (value) => this.gameService.setConvar('ui_updateChannel', value),
+			options: {
+				'production': 'Release',
+				'canary': 'Canary (Experimental/Unstable)',
+			},
+			category: '#SettingsCat_Game',
+		});
 	}
 
 	public addSetting(label: string, setting: Setting) {
