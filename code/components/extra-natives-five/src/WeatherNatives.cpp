4ddeb62043d38a70814ec164d9567b9678e0ca6d
blattersturm <peachypies@protonmail.ch>
2021-02-15 09:14:59 +0100

    feat(natives/five): SET_WEATHER_OWNED_BY_NETWORK native to allow custom weather blending

--- code/components/extra-natives-five/src/WeatherNatives.cpp
+++ code/components/extra-natives-five/src/WeatherNatives.cpp
@@ -7,6 +7,20 @@
 #include <fxScripting.h>
 
 static bool g_forceSnowPass;
+static bool* g_weatherNetFlag;
+
+template<typename T>
+static void AddStopCallback(T&& stopCb)
+{
+	fx::OMPtr<IScriptRuntime> runtime;
+
+	if (FX_SUCCEEDED(fx::GetCurrentScriptRuntime(&runtime)))
+	{
+		fx::Resource* resource = reinterpret_cast<fx::Resource*>(runtime->GetParentObject());
+
+		resource->OnStop.Connect(stopCb);
+	}
+}
 
 static InitFunction initFunction([]()
 {
@@ -15,17 +29,20 @@ static InitFunction initFunction([]()
 		bool value = context.GetArgument<bool>(0);
 		g_forceSnowPass = value;
 
-		fx::OMPtr<IScriptRuntime> runtime;
-
-		if (FX_SUCCEEDED(fx::GetCurrentScriptRuntime(&runtime)))
+		AddStopCallback([]()
 		{
-			fx::Resource* resource = reinterpret_cast<fx::Resource*>(runtime->GetParentObject());
+			g_forceSnowPass = false;
+		});
+	});
 
-			resource->OnStop.Connect([]()
-			{
-				g_forceSnowPass = false;
-			});
-		}
+	fx::ScriptEngine::RegisterNativeHandler("SET_WEATHER_OWNED_BY_NETWORK", [](fx::ScriptContext& context)
+	{
+		*g_weatherNetFlag = context.GetArgument<bool>(0);
+
+		AddStopCallback([]()
+		{
+			*g_weatherNetFlag = true;
+		});
 	});
 });
 
@@ -44,9 +61,18 @@ static uint32_t getWeatherByNameHook(void* a1, const char* a2)
 
 static HookFunction hookFunction([]()
 {
-	auto location = hook::get_pattern<char>("C6 45 20 00 E8 ? ? ? ? 48 8D 15");
-	hook::set_call(&getWeatherByName, location + 23);
-	hook::call(location + 23, getWeatherByNameHook);
+	{
+		auto location = hook::get_pattern<char>("C6 45 20 00 E8 ? ? ? ? 48 8D 15");
+		hook::set_call(&getWeatherByName, location + 23);
+		hook::call(location + 23, getWeatherByNameHook);
 
-	g_currentWeather = hook::get_address<int*>(location + 30);
+		g_currentWeather = hook::get_address<int*>(location + 30);
+	}
+
+	// 2060/2189 might be obfuscated or different
+	if (xbr::IsGameBuild<1604>())
+	{
+		auto location = hook::get_pattern("C6 44 24 20 00 E8 ? ? ? ? C6 05 ? ? ? ? 01 E8", 12);
+		g_weatherNetFlag = hook::get_address<bool*>(location) + 1; // address offset
+	}
 });
---
+++ ext/native-decls/SetWeatherOwnedByNetwork.md
@@ -0,0 +1,18 @@
+---
+ns: CFX
+apiset: client
+game: gta5
+---
+## SET_WEATHER_OWNED_BY_NETWORK
+
+```c
+void SET_WEATHER_OWNED_BY_NETWORK(BOOL network);
+```
+
+Sets whether or not the weather should be owned by the network subsystem.
+
+To be able to use [_SET_WEATHER_TYPE_TRANSITION](#_0x578C752848ECFA0C), this has to be set to false.
+
+## Parameters
+* **network**: true to let the network control weather, false to not use network weather behavior.
+
