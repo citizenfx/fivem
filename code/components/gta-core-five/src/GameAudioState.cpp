c87fb9562a4f9c9032e527e9428c06965a58e1df
blattersturm <peachypies@protonmail.ch>
2021-03-02 19:37:21 +0100

    tweak(game/five): rage::g_audUseFrameLimiter toggle, code side

--- code/components/gta-core-five/src/GameAudioState.cpp
+++ code/components/gta-core-five/src/GameAudioState.cpp
@@ -1,6 +1,8 @@
 #include <StdInc.h>
 #include <Hooking.h>
 
+#include <CoreConsole.h>
+
 static bool* audioNotFocused;
 static int* muteOnFocusLoss; 
 
@@ -9,9 +11,23 @@ bool DLL_EXPORT ShouldMuteGameAudio()
 	return *audioNotFocused && *muteOnFocusLoss;
 }
 
+namespace rage
+{
+bool* g_audUseFrameLimiter;
+}
+
 static HookFunction hookFunction([]()
 {
-	auto location = hook::get_pattern<char>("75 17 40 38 2D ? ? ? ? 74 0E 39 2D", 5);
-	audioNotFocused = hook::get_address<bool*>(location);
-	muteOnFocusLoss = hook::get_address<int*>(location + 8);
+	{
+		auto location = hook::get_pattern<char>("75 17 40 38 2D ? ? ? ? 74 0E 39 2D", 5);
+		audioNotFocused = hook::get_address<bool*>(location);
+		muteOnFocusLoss = hook::get_address<int*>(location + 8);
+	}
+	
+	{
+		auto location = hook::get_pattern("80 3D ? ? ? ? 00 0F 84 ? 00 00 00 80 BB", 2);
+		rage::g_audUseFrameLimiter = hook::get_address<bool*>(location) + 1;
+	}
+
+	static ConVar<bool> audUseFrameLimiter("game_useAudioFrameLimiter", ConVar_Archive, true, rage::g_audUseFrameLimiter);
 });
