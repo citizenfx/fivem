3b6b8b619f56ba7d0f96319d1d3a0dc6fd5ca429
liquid <liquid@l1q.pw>
2021-03-24 01:32:59 -0500

    feat(streaming-five): Add support for CEventGroupPed events
    Passes the related ped and the source ped (cause of event) to gameEventTriggered
    
    Co-Authored-By: gottfriedleibniz <37632412+gottfriedleibniz@users.noreply.github.com>

--- code/components/gta-streaming-five/src/GameEventHooks.cpp
+++ code/components/gta-streaming-five/src/GameEventHooks.cpp
@@ -28,14 +28,41 @@ namespace rage
 
 		virtual bool m_40(rage::fwEvent* other) = 0;
 	};
+
+	class aiEvent : public fwEvent 
+	{
+	public:
+		fwEntity* GetSource() 
+		{
+			const auto vtable = *reinterpret_cast<uintptr_t*>(this);
+			return (*reinterpret_cast<fwEntity*(**)(rage::aiEvent*)>(vtable + 27 * sizeof(uintptr_t)))(this);
+		}
+	};
 }
+
+class CEventGroup {};
+
+class CEventGroupPed : public CEventGroup 
+{
+public:
+	fwEntity* GetPed() 
+	{
+		static const auto pedOffset = *hook::pattern("48 8B C6 48 89 BE ? ? ? ?").count(1).get(0).get<uint32_t>(6);
+		return *reinterpret_cast<fwEntity**>(this + pedOffset);
+	}
+};
+
+static hook::cdecl_stub<uint32_t(void*)> getScriptGuidForEntity([]() 
+{
+	return hook::get_pattern("48 F7 F9 49 8B 48 08 48 63 D0 C1 E0 08 0F B6 1C 11 03 D8", -0x68);
+});
 
 void*(*g_eventCall1)(void* group, void* event);
 void*(*g_eventCall2)(void* group, void* event);
 void*(*g_eventCall3)(void* group, void* event);
 
-template<decltype(g_eventCall1)* TFunc>
-void* HandleEventWrap(void* group, rage::fwEvent* event)
+template<decltype(g_eventCall1)* TFunc, bool aiEvent = false>
+void* HandleEventWrap(CEventGroup* group, rage::fwEvent* event)
 {
 	if (event)
 	{
@@ -47,14 +74,35 @@ void* HandleEventWrap(void* group, rage::fwEvent* event)
 			strcpy(data.name, &eventName[6]);
 			data.numArguments = 0;
 
-			// brute-force the argument count
-			// since these functions should early exit, most cost here will be VMT dispatch
-			for (int i = 0; i < _countof(data.arguments); i++)
+			if (aiEvent) 
 			{
-				if (event->GetArguments(data.arguments, i * sizeof(uintptr_t)))
+				const auto ped = static_cast<CEventGroupPed*>(group)->GetPed();
+
+				if (ped != nullptr) 
 				{
-					data.numArguments = i;
-					break;
+					data.arguments[data.numArguments++] = getScriptGuidForEntity(ped);
+
+					// The source entity involved in this AI event, if one exists,
+					// *should* be accessible by vtable lookup.
+					const auto sourceEntity = static_cast<rage::aiEvent*>(event)->GetSource();
+
+					if (sourceEntity != nullptr) 
+					{
+						data.arguments[data.numArguments++] = getScriptGuidForEntity(sourceEntity);
+					}
+				}
+			} 
+			else 
+			{
+				// brute-force the argument count
+				// since these functions should early exit, most cost here will be VMT dispatch
+				for (int i = 0; i < _countof(data.arguments); i++) 
+				{
+					if (event->GetArguments(data.arguments, i * sizeof(uintptr_t))) 
+					{
+						data.numArguments = i;
+						break;
+					}
 				}
 			}
 
@@ -77,13 +125,14 @@ static HookFunction hookFunction([]()
 
 	// 8-bit event pools
 
-	// these are for ped events, we don't handle getting entities/positions from aiEvent instances yet
-	/*{
+	{
 		auto matches = hook::pattern("83 BF ? ? 00 00 ? 75 ? 48 8B CF E8 ? ? ? ? 83 BF").count(2);
 
-		MH_CreateHook(matches.get(0).get<void>(-0x36), HandleEventWrap<&g_eventCall1>, (void**)&g_eventCall1);
-		MH_CreateHook(matches.get(1).get<void>(-0x36), HandleEventWrap<&g_eventCall2>, (void**)&g_eventCall2);
-	}*/
+		MH_CreateHook(matches.get(0).get<void>(-0x36), HandleEventWrap<&g_eventCall1, true>, (void**)&g_eventCall1);
+
+		// we can't read out any data from this event call yet
+		// MH_CreateHook(matches.get(1).get<void>(-0x36), HandleEventWrap<&g_eventCall2, true>, (void**)&g_eventCall2);
+	}
 
 	{
 		MH_CreateHook(hook::get_pattern("81 BF ? ? 00 00 ? ?  00 00 75 ? 48 8B CF E8", -0x36), HandleEventWrap<&g_eventCall3>, (void**)&g_eventCall3);
@@ -95,14 +144,11 @@ static HookFunction hookFunction([]()
 	OnTriggerGameEvent.Connect([](const GameEventMetaData& data)
 	{
 		std::stringstream argStr;
-
 		argStr << "(" << data.numArguments << ") ";
-
 		for (int i = 0; i < data.numArguments; i++)
 		{
 			argStr << " " << data.arguments[i];
 		}
-
 		trace("game event %s, args %s\n", data.name, argStr.str());
 	});
 	*/
