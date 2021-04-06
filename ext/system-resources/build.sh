16fd520331dd7f69721f202ca95970802b8dcf11
blattersturm <peachypies@protonmail.ch>
2021-04-06 09:04:43 +0200

    tweak(build): remove webadmin for linux
    It's incompatible with a system with only .NET 3.1 SDKs installed, and there's no downlevel 2.1 package for newer Alpine with newer NPM.

--- ext/system-resources/build.sh
+++ ext/system-resources/build.sh
@@ -3,20 +3,9 @@ set -e
 
 apk add --no-cache nodejs npm
 
-cd ../webadmin/server/
-dotnet publish -c Release
-cd ../../system-resources/
-
 cd ../txAdmin/
 npm ci
 ./node_modules/.bin/webpack --config webpack.config.js --progress
 cd ../system-resources/
-
-mkdir -p data/webadmin/wwwroot/
-mkdir -p data/webadmin/server/bin/Release/netstandard2.0/publish/
-cp -a ../webadmin/fxmanifest.lua data/webadmin/
-
-cp -a ../webadmin/wwwroot/ data/webadmin/
-cp -a ../webadmin/server/bin/Release/netstandard2.0/publish/ data/webadmin/server/bin/Release/netstandard2.0/
 
 cp -a ../txAdmin/dist data/monitor
