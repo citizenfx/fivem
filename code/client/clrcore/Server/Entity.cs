627ece146f63e93a5bde20fba2b4149c80fbf772
Christopher <mrcm8263@gmail.com>
2021-04-08 16:22:36 +1000

    Updated Entity.cs

--- code/client/clrcore/Server/Entity.cs
+++ code/client/clrcore/Server/Entity.cs
@@ -1,15 +1,81 @@
 using System;
-using System.Linq;
 using CitizenFX.Core.Native;
 using System.Security;
 
 namespace CitizenFX.Core
 {
+    public enum ForceType
+    {
+        MinForce,
+        MaxForceRot,
+        MinForce2,
+        MaxForceRot2,
+        ForceNoRot,
+        ForceRotPlusForce
+    }
+
 	public abstract class Entity : PoolObject, IEquatable<Entity>, ISpatial
 	{
 		public Entity(int handle) : base(handle)
 		{
 		}
+
+        /// <summary>
+		/// Gets the health of this <see cref="Entity"/> as an <see cref="int"/>.
+		/// </summary>
+		/// <value>
+		/// The health from 0 - 100 as an integer.
+		/// </value>
+		public int Health
+        {
+            get
+            {
+                return API.GetEntityHealth(Handle) - 100;
+            }
+        }
+
+        /// <summary>
+        /// Gets the maximum health of this <see cref="Entity"/> as an <see cref="int"/>.
+        /// </summary>
+        /// <value>
+        /// The maximum health from 0 - 100 as an integer.
+        /// </value>
+        public int MaxHealth
+        {
+            get
+            {
+                return API.GetEntityMaxHealth(Handle) - 100;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Entity"/> is dead.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this <see cref="Entity"/> is dead; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsDead
+        {
+            get
+            {
+                // TODO: Replace with IsEntityDead() if that becomes available server side
+                return this.Health == 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Entity"/> is alive.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this <see cref="Entity"/> is alive; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsAlive
+        {
+            get
+            {
+                return !IsDead;
+            }
+        }
 
 		/// <summary>
 		/// Gets or sets the position of this <see cref="Entity"/>.
@@ -113,6 +179,50 @@ namespace CitizenFX.Core
 			}
 		}
 
+        /// <summary>
+        /// Determines whether this <see cref="Entity"/> is in range of a specified position
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="range">The maximum range.</param>
+        /// <returns>
+        ///   <c>true</c> if this <see cref="Entity"/> is in range of the <paramref name="position"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsInRangeOf(Vector3 position, float range)
+        {
+            return Vector3.Subtract(Position, position).LengthSquared() < range * range;
+        }
+
+        /// CM-TODO Add after adding Blip
+        /// <summary>
+        /// Creates a <see cref="Blip"/> on this <see cref="Entity"/>
+        /// </summary>
+        public Blip AttachBlip()
+        {
+            return new Blip(API.AddBlipForEntity(Handle));
+        }
+
+        /// <summary>
+        /// Applies a force to this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="direction">The direction to apply the force relative to world coords.</param>
+        /// <param name="rotation">The rotation force to apply</param>
+        /// <param name="forceType">Type of the force to apply.</param>
+        public void ApplyForce(Vector3 direction, Vector3 rotation = default(Vector3), ForceType forceType = ForceType.MaxForceRot2)
+        {
+            API.ApplyForceToEntity(Handle, (int)forceType, direction.X, direction.Y, direction.Z, rotation.X, rotation.Y, rotation.Z, 0, false, true, true, false, true);
+        }
+
+        /// <summary>
+        /// Applies a force to this <see cref="Entity"/>.
+        /// </summary>
+        /// <param name="direction">The direction to apply the force relative to this <see cref="Entity"/>s rotation</param>
+        /// <param name="rotation">The rotation force to apply</param>
+        /// <param name="forceType">Type of the force to apply.</param>
+        public void ApplyForceRelative(Vector3 direction, Vector3 rotation = default(Vector3), ForceType forceType = ForceType.MaxForceRot2)
+        {
+            API.ApplyForceToEntity(Handle, (int)forceType, direction.X, direction.Y, direction.Z, rotation.X, rotation.Y, rotation.Z, 0, true, true, true, false, true);
+        }
+
 		/// <summary>
 		/// Gets the network owner of the this <see cref="Entity"/>.
 		/// </summary>
@@ -138,6 +248,19 @@ namespace CitizenFX.Core
 				return API.NetworkGetNetworkIdFromEntity(this.Handle);
 			}
 		}
+
+        /// <summary>
+		/// Deletes this <see cref="Entity"/>
+		/// </summary>
+        [SecuritySafeCritical]
+        private void Delete()
+        {
+            // prevent the game from crashing if this is called on the player ped.
+            if (!API.IsPedAPlayer(this.Handle))
+            {
+                API.DeleteEntity(this.Handle);
+            }
+        }
 
 		/// <summary>
 		/// Gets the type of this <see cref="Entity"/>.
@@ -211,6 +334,25 @@ namespace CitizenFX.Core
 
 			return null;
 		}
+
+        /// <summary>
+        /// Determines whether this <see cref="Entity"/> exists.
+        /// </summary>
+        /// <returns><c>true</c> if this <see cref="Entity"/> exists; otherwise, <c>false</c></returns>
+        public bool Exists()
+        {
+            return API.DoesEntityExist(Handle);
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="Entity"/> exists.
+        /// </summary>
+        /// <param name="entity">The <see cref="Entity"/> to check.</param>
+        /// <returns><c>true</c> if the <see cref="Entity"/> exists; otherwise, <c>false</c></returns>
+        public static bool Exists(Entity entity)
+        {
+            return !ReferenceEquals(entity, null) && entity.Exists();
+        }
 
 		/// <summary>
 		/// Checks if two <see cref="Entity"/>s refer to the same <see cref="Entity"/>
