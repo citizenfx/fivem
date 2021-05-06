be0f2b3d27bef4ce5e63c11896243a9237be46e2
blattersturm <peachypies@protonmail.ch>
2021-05-06 10:23:03 +0200

    tweak(server/impl): rephrased ticket validation errors

--- code/components/citizen-server-impl/src/InitConnectMethod.cpp
+++ code/components/citizen-server-impl/src/InitConnectMethod.cpp
@@ -65,13 +65,18 @@ static std::mutex g_ticketMapMutex;
 static std::unordered_set<std::tuple<uint64_t, uint64_t>> g_ticketList;
 static std::chrono::milliseconds g_nextTicketGc;
 
-static bool VerifyTicket(const std::string& guid, const std::string& ticket)
+static bool VerifyTicket(const std::string& guid, const std::string& ticket, std::string* error = nullptr)
 {
 	auto ticketData = Botan::base64_decode(ticket);
 
 	// validate ticket length
 	if (ticketData.size() < 20 + 4 + 128)
 	{
+		if (error)
+		{
+			*error = "Invalid ticket length.";
+		}
+
 		return false;
 	}
 
@@ -79,6 +84,11 @@ static bool VerifyTicket(const std::string& guid, const std::string& ticket)
 
 	if (length != 16)
 	{
+		if (error)
+		{
+			*error = "Invalid ticket length. (2)";
+		}
+
 		return false;
 	}
 
@@ -94,6 +104,11 @@ static bool VerifyTicket(const std::string& guid, const std::string& ticket)
 	// verify
 	if (ticketExpiry < timeVal)
 	{
+		if (error)
+		{
+			*error = "Ticket expired. Please check your server's system time.";
+		}
+
 		console::DPrintf("server", "Connecting player: ticket expired\n");
 		return false;
 	}
@@ -103,6 +118,11 @@ static bool VerifyTicket(const std::string& guid, const std::string& ticket)
 
 	if (realGuid != ticketGuid)
 	{
+		if (error)
+		{
+			*error = "Mismatching GUID.";
+		}
+
 		console::DPrintf("server", "Connecting player: ticket GUID not matching\n");
 		return false;
 	}
@@ -112,6 +132,11 @@ static bool VerifyTicket(const std::string& guid, const std::string& ticket)
 
 		if (g_ticketList.find({ ticketExpiry, ticketGuid }) != g_ticketList.end())
 		{
+			if (error)
+			{
+				*error = "Reused ticket.";
+			}
+
 			return false;
 		}
 
@@ -129,6 +154,11 @@ static bool VerifyTicket(const std::string& guid, const std::string& ticket)
 
 	if (sigLength != 128)
 	{
+		if (error)
+		{
+			*error = "Invalid signature length.";
+		}
+
 		return false;
 	}
 
@@ -157,6 +187,11 @@ static bool VerifyTicket(const std::string& guid, const std::string& ticket)
 
 	if (!valid)
 	{
+		if (error)
+		{
+			*error = "Invalid ticket signature.";
+		}
+
 		console::DPrintf("server", "Connecting player: ticket RSA signature not matching\n");
 		return false;
 	}
@@ -468,15 +503,17 @@ static InitFunction initFunction([]()
 
 				if (ticketIt == postMap.end())
 				{
-					sendError("No CitizenFX ticket was specified. If this is an offline server, maybe set sv_lan?");
+					sendError("No authentication ticket was specified.");
 					return;
 				}
 
 				try
 				{
-					if (!VerifyTicket(guid, ticketIt->second))
+					std::string ticketError;
+
+					if (!VerifyTicket(guid, ticketIt->second, &ticketError))
 					{
-						sendError("CitizenFX ticket authorization failed.");
+						sendError(fmt::sprintf("Ticket authorization failed. %s", ticketError));
 						return;
 					}
 
@@ -484,7 +521,7 @@ static InitFunction initFunction([]()
 
 					if (!optionalTicket)
 					{
-						sendError("CitizenFX ticket authorization failed. (2)");
+						sendError("Ticket authorization failed. (2)");
 						return;
 					}
 
@@ -492,7 +529,7 @@ static InitFunction initFunction([]()
 				}
 				catch (const std::exception& e)
 				{
-					sendError(fmt::sprintf("Parsing error while verifying CitizenFX ticket. %s", e.what()));
+					sendError(fmt::sprintf("Parsing error while verifying ticket. %s", e.what()));
 					return;
 				}
 			}
