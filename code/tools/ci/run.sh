7b0f3a0273f917c575f62e46fae91a5146a13251
Routmoute <36932590+routmoute@users.noreply.github.com>
2021-06-01 16:27:54 +0200

    fix alpine integrated run.sh

--- code/tools/ci/run.sh
+++ code/tools/ci/run.sh
@@ -3,4 +3,6 @@
 cd /opt/cfx-server
 [ -d cache ] || mkdir cache
 
-exec /opt/cfx-server/FXServer $SERVER_ARGS $*
+exec /opt/cfx-server/ld-musl-x86_64.so.1 \
+    --library-path "/usr/lib/v8/:/lib/:/usr/lib/" -- \
+    /opt/cfx-server/FXServer +set citizen_dir /opt/cfx-server/citizen/ $SERVER_ARGS $*
