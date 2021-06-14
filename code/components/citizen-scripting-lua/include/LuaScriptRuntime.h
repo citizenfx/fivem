08212898f7e0260cdc12356e55bd76f50c502f97
Gottfried Leibniz <37632412+gottfriedleibniz@users.noreply.github.com>
2021-06-14 13:41:23 -0300

    feat(scripting/lua54): default GC generational mode

--- code/components/citizen-scripting-lua/include/LuaScriptRuntime.h
+++ code/components/citizen-scripting-lua/include/LuaScriptRuntime.h
@@ -80,6 +80,9 @@ public:
 	LuaStateHolder()
 	{
 		m_state = luaL_newstate();
+#if LUA_VERSION_NUM >= 504
+		lua_gc(m_state, LUA_GCGEN, 0, 0);  /* GC in generational mode */
+#endif
 	}
 
 	~LuaStateHolder()
