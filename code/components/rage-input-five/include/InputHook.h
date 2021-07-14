fab31a9522d0b6253e8cc8bc13b6e854472cbe8e
LWSS <bbchallenger100@gmail.com>
2021-07-13 21:25:37 -0700

    fix(rage-input-five): ioMouseStruct union-template for 2189 not working properly

--- code/components/rage-input-five/include/InputHook.h
+++ code/components/rage-input-five/include/InputHook.h
@@ -44,28 +44,19 @@ static constexpr uint32_t METHOD_RAW_INPUT = 0;
 static constexpr uint32_t METHOD_DIRECTINPUT = 1;
 static constexpr uint32_t METHOD_WINDOWS = 2;
 
-template <int Build>
-struct ioMouseStruct
-	{
+struct ioMouseStruct1604
+{
 	int32_t m_dZ; // mousewheel
-	IfElsePadding<(Build <= 1604), 16, 8> _pad;
+	char pad_0028[16];
 	uint32_t m_inputMethod;
 	char pad_4[4];
 	int32_t m_X; // Based on your monitor's resolution. Zero when alt-tabbed and Zero in launcher
 	int32_t m_Y; // ^^^^^^^^^^^
-
-	//[[no_unique_address]] (C++20) doesn't work on MSVC! Hack in a union for now :(
-	union
-	{
-		IfElsePadding<(Build > 1604), 8, 4> _4bytepad; // additional 4 bytes on 2189
-		int32_t m_dX; // in pixels
-	};
-
-	//int32_t m_dX; // in pixels
+	int32_t m_dX; // in pixels
 	int32_t m_lastDX;
 	int32_t m_dY;
 	int32_t m_lastDY;
-	IfElsePadding<(Build <= 1604), 76, 72> _pad3;
+	char pad_0050[76];
 	bool _unk;
 	bool shouldZeroMouseValues;
 	bool _unk2;
@@ -75,7 +66,7 @@ struct ioMouseStruct
 	int32_t cursorAbsY; // ^^^^^^^^^^^^^^
 	float cursorDiffX; // cursor pixel diff in float, but is always rounded to a whole number
 	float cursorDiffY; // ^^^^^^
-	int32_t _unk5;
+	int32_t scrollDiffTicks; // unsure about this
 	int32_t m_InternalButtonsState;
 	char pad_00BC[4];
 	int32_t m_LastButtons;
@@ -84,12 +75,45 @@ struct ioMouseStruct
 	float mouseDiffDirectionX; // -1.000[Left] -> 1.000[Right]
 	float mouseDiffDirectionY; // -1.000[Up] -> 1.000[Down]
 };
+struct ioMouseStruct2189
+{
+	int32_t m_dZ; // mousewheel
+	char pad_8[8];
+	uint32_t m_inputMethod;
+	char pad_4[4];
+	int32_t m_X; // Based on your monitor's resolution. Zero when alt-tabbed and Zero in launcher
+	int32_t m_Y; // ^^^^^^^^^^^
+	char pad_4_2[4];
+	int32_t m_dX; // in pixels
+	int32_t m_lastDX;
+	int32_t m_dY;
+	int32_t m_lastDY;
+	char pad_0050[72];
+	bool _unk;
+	bool shouldZeroMouseValues;
+	bool _unk2;
+	bool _align;
+	uint32_t _unk4;
+	int32_t cursorAbsX; // Based on your monitor's resolution. center of screen when alt-tabbed (ex: 1280/720 on a 1440p screen)
+	int32_t cursorAbsY; // ^^^^^^^^^^^^^^
+	float cursorDiffX; // cursor pixel diff in float, but is always rounded to a whole number
+	float cursorDiffY; // ^^^^^^
+	int32_t scrollDiffTicks; // unsure about this
+	int32_t m_InternalButtonsState;
+	char pad_00BC[4];
+	int32_t m_LastButtons;
+	int32_t m_Buttons;
+	int32_t N00000048; // some sort of frame ticker, only goes up while alt-tabbed
+	float mouseDiffDirectionX; // -1.000[Left] -> 1.000[Right]
+	float mouseDiffDirectionY; // -1.000[Up] -> 1.000[Down]
+};
+
 struct ioMouse
 {
 	union
 	{
-		ioMouseStruct<1604> impl1604;
-		ioMouseStruct<2189> impl2189;
+		ioMouseStruct1604 impl1604;
+		ioMouseStruct2189 impl2189;
 	};
 	DECLARE_ACCESSOR(m_Buttons);
 	DECLARE_ACCESSOR(m_lastDX);
