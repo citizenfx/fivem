a90f09ca5b1fe9682a1911429933734b953990f0
blattersturm <peachypies@protonmail.ch>
2021-07-15 21:15:51 +0200

    fix(mod-loader): revert 25310b994ce273871b50a0d15d782b14a07bed53

--- code/components/citizen-mod-loader-five/src/ModPackage.cpp
+++ code/components/citizen-mod-loader-five/src/ModPackage.cpp
@@ -98,11 +98,7 @@ void ModPackage::ParsePackage(const std::string& path)
 
 		m_metadata = ParseMetadata(doc.RootElement()->FirstChildElement("metadata"));
 
-		// don't load any content from known-broken mods
-		if (HashRageString(m_metadata.authorName.c_str()) != 0x04f243b9)
-		{
-			m_content = ParseContent(doc.RootElement()->FirstChildElement("content"));
-		}
+		m_content = ParseContent(doc.RootElement()->FirstChildElement("content"));
 	}
 }
 
