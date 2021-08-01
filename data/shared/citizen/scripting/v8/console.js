64ff84997380fbf5937157698e461e6d4c32c4f6
blattersturm <peachypies@protonmail.ch>
2021-08-01 12:04:03 +0200

    fix(scripting/v8): print 'undefined' arg correctly in console
    Fixes part of https://forum.cfx.re/t/4147934

--- data/shared/citizen/scripting/v8/console.js
+++ data/shared/citizen/scripting/v8/console.js
@@ -161,10 +161,6 @@
      * @param {*} args 
      */
     function format(message = undefined, ...args) {
-        if (typeof message === "undefined") {
-            return '';
-        }
-
         const totalArgs = args.length;
 
         if (totalArgs === 0) {
