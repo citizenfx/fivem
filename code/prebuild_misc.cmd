0d0092cc8af8d6844d1a94bbb5d9b8ef1ca08c34
blattersturm <peachypies@protonmail.ch>
2021-08-25 15:46:33 +0200

    fix(build): hotfix 2 - install `six`

--- code/prebuild_misc.cmd
+++ code/prebuild_misc.cmd
@@ -1,6 +1,6 @@
 @echo off
-py -m pip install Jinja2 MarkupSafe ply
+py -m pip install Jinja2 MarkupSafe ply six
 
 set PYTHONPATH=code\tools\idl\deps\
 mkdir code\tools\idl\deps
-py -m pip install Jinja2 MarkupSafe ply
+py -m pip install Jinja2 MarkupSafe ply six
