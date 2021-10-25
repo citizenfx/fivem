2570da378477d5bd6b2849f0ef708e23b0da66c6
blattersturm <peachypies@protonmail.ch>
2021-10-25 10:11:17 +0200

    fix(nui): draw 'fake' cursor in SDK, clean up code

--- code/components/nui-core/src/NUIRenderCallbacks.cpp
+++ code/components/nui-core/src/NUIRenderCallbacks.cpp
@@ -42,18 +42,22 @@ static HookFunction initFunction([] ()
 
 	g_nuiGi->OnRender.Connect([]()
 	{
+		// if we're in the main UI, make sure it has focus
 		if (nui::HasMainUI())
 		{
 			nui::GiveFocus("mpMenu", true);
 		}
 
+		// draw background
 		nui::OnDrawBackground(nui::HasMainUI());
 
+		// update windows from a render context
 		Instance<NUIWindowManager>::Get()->ForAllWindows([](fwRefContainer<NUIWindow> window)
 		{
 			window->UpdateFrame();
 		});
 
+		// collect all post-render windows
 		std::map<std::string, fwRefContainer<NUIWindow>> renderWindows;
 
 		Instance<NUIWindowManager>::Get()->ForAllWindows([&renderWindows](fwRefContainer<NUIWindow> window)
@@ -88,6 +92,7 @@ static HookFunction initFunction([] ()
 				continue;
 			}
 
+			// does the window have a full-screen surface? if so, draw it
 			if (window->GetTexture().GetRef())
 			{
 				g_nuiGi->SetTexture(window->GetTexture(), true);
@@ -97,6 +102,8 @@ static HookFunction initFunction([] ()
 
 				nui::ResultingRectangle rr;
 				rr.color = CRGBA(0xff, 0xff, 0xff, 0xff);
+
+				// the texture is usually upside down (GL->DX coord system), so we draw it as such
 				rr.rectangle = CRect(0, resY, resX, 0);
 
 				g_nuiGi->DrawRectangles(1, &rr);
@@ -104,16 +111,17 @@ static HookFunction initFunction([] ()
 				g_nuiGi->UnsetTexture();
 			}
 
+			// a popup (e.g. select dropdowns) has to be drawn separately
 			if (window->GetPopupTexture().GetRef())
 			{
 				g_nuiGi->SetTexture(window->GetPopupTexture(), true);
 
-
 				const CefRect& rect = window->GetPopupRect();
 
-				// we need to subtract 0.5f from each vertex coordinate (half a pixel after scaling) due to the usual half-pixel/texel issue
 				nui::ResultingRectangle rr;
 				rr.color = CRGBA(0xff, 0xff, 0xff, 0xff);
+
+				// also upside down here
 				rr.rectangle = CRect(rect.x, rect.y + rect.height, rect.x + rect.width, rect.y);
 
 				g_nuiGi->DrawRectangles(1, &rr);
@@ -122,36 +130,48 @@ static HookFunction initFunction([] ()
 			}
 		}
 
+		// are we in any situation where we need a cursor?
 		bool needsNuiCursor = (nui::HasMainUI() || g_hasCursor) && !g_shouldHideCursor;
 		g_nuiGi->SetHostCursorEnabled(needsNuiCursor);
 
+		// we set the host cursor above unconditionally- this is for cases where the host cursor isn't sufficient
 		if (needsNuiCursor)
 		{
-			POINT cursorPos = g_cursorPos;
+			// do we need to draw a non-host cursor?
+			bool shouldDrawCursorTexture = false;
 
-			if (launch::IsSDKGuest())
+			// if the game doesn't support a host cursor, draw the cursor texture
+			if (!g_nuiGi->CanDrawHostCursor())
 			{
-				static HostSharedData<ReverseGameData> rgd("CfxReverseGameData");
-
-				cursorPos.x = rgd->mouseAbsX;
-				cursorPos.y = rgd->mouseAbsY;
+				shouldDrawCursorTexture = true;
 			}
-			else
+			// if we're SDK guest, also draw the cursor texture
+			else if (launch::IsSDKGuest())
 			{
-				GetCursorPos(&cursorPos);
-				ScreenToClient(g_nuiGi->GetHWND(), &cursorPos);
+				shouldDrawCursorTexture = true;
 			}
 
-			if (g_cursorTexture.GetRef() && !g_nuiGi->CanDrawHostCursor())
+			// draw the cursor if we should, and if the texture exists
+			if (g_cursorTexture.GetRef() && shouldDrawCursorTexture)
 			{
-				g_nuiGi->SetTexture(g_cursorTexture);
+				POINT cursorPos = g_cursorPos;
 
-				uint32_t color = 0xFFFFFFFF;
-
-				if (g_isDragging)
+				// SDK wants to get cursor data from RGD
+				if (launch::IsSDKGuest())
 				{
-					color = 0xFFAAAAAA;
+					static HostSharedData<ReverseGameData> rgd("CfxReverseGameData");
+
+					cursorPos.x = rgd->mouseAbsX;
+					cursorPos.y = rgd->mouseAbsY;
 				}
+				else
+				{
+					// if not SDK, our cursor follows the host cursor
+					GetCursorPos(&cursorPos);
+					ScreenToClient(g_nuiGi->GetHWND(), &cursorPos);
+				}
+
+				g_nuiGi->SetTexture(g_cursorTexture);
 
 				nui::ResultingRectangle rr;
 				rr.color = (g_isDragging) ? CRGBA(0xaa, 0xaa, 0xaa, 0xff) : CRGBA(0xff, 0xff, 0xff, 0xff);
