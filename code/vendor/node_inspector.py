db07118aa089244b2dfbfdfd0af4c03dd77e2300
blattersturm <peachypies@protonmail.ch>
2021-12-20 13:23:14 +0100

    fix(build/node): node_inspector.py using parent's python

--- code/vendor/node_inspector.py
+++ code/vendor/node_inspector.py
@@ -9,7 +9,7 @@ try:
 except:
     pass
 
-subprocess.check_call(['python', 'tools/inspector_protocol/convert_protocol_to_json.py', 'src/inspector/node_protocol.pdl', 'gen/src/node_protocol.json'], cwd = noderoot)
-subprocess.check_call(['python', 'tools/inspector_protocol/code_generator.py', '--jinja_dir', 'tools/inspector_protocol','--output_base', 'gen/src/', '--config', 'src/inspector/node_protocol_config.json'], cwd = noderoot)
-subprocess.check_call(['python', 'tools/inspector_protocol/concatenate_protocols.py', 'deps/v8/include/js_protocol.pdl', 'gen/src/node_protocol.json', 'gen/src/node/inspector/protocol/concatenated_protocol.json'], cwd = noderoot)
-subprocess.check_call(['python', 'tools/compress_json.py', 'gen/src/node/inspector/protocol/concatenated_protocol.json', 'gen/src/node/inspector/protocol/v8_inspector_protocol_json.h'], cwd = noderoot)
+subprocess.check_call([sys.executable, 'tools/inspector_protocol/convert_protocol_to_json.py', 'src/inspector/node_protocol.pdl', 'gen/src/node_protocol.json'], cwd = noderoot)
+subprocess.check_call([sys.executable, 'tools/inspector_protocol/code_generator.py', '--jinja_dir', 'tools/inspector_protocol','--output_base', 'gen/src/', '--config', 'src/inspector/node_protocol_config.json'], cwd = noderoot)
+subprocess.check_call([sys.executable, 'tools/inspector_protocol/concatenate_protocols.py', 'deps/v8/include/js_protocol.pdl', 'gen/src/node_protocol.json', 'gen/src/node/inspector/protocol/concatenated_protocol.json'], cwd = noderoot)
+subprocess.check_call([sys.executable, 'tools/compress_json.py', 'gen/src/node/inspector/protocol/concatenated_protocol.json', 'gen/src/node/inspector/protocol/v8_inspector_protocol_json.h'], cwd = noderoot)
