739d31396b9b185029b62eafbc2299184872283f
blattersturm <peachypies@protonmail.ch>
2021-12-30 22:38:54 +0100

    fix(locales): make update_locale.cmd work again

--- code/update_locale.cmd
+++ code/update_locale.cmd
@@ -2,5 +2,12 @@
 
 set find=_find
 
+pushd %~dp0
 mkdir ..\data\client\citizen\locales
-%find% client shared components -type f -iname "*.cpp" -o -iname "*.c" -o -iname "*.h" | xargs xgettext --from-code utf-8 --no-wrap -C -d cfx -s -kgettext -o ../data/client/citizen/locales/cfx.pot 
+del ..\data\client\citizen\locales\cfx.pot
+
+%find% client shared components -type f -iname "*.cpp" -o -iname "*.c" -o -iname "*.h" > %temp%\pot.lst
+xgettext -f "%temp%\pot.lst" --from-code utf-8 --no-wrap -C -d cfx -s -kgettext -o ../data/client/citizen/locales/cfx.pot
+del "%temp%\pot.lst"
+
+popd
