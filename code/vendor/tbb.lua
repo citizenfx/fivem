30d02bdf0da2febcda326ea0ac53cb8ec51ac4ff
blattersturm <peachypies@protonmail.ch>
2022-01-06 13:19:31 +0100

    fix(build/vendor): add more build flags for tbb on linux

--- code/vendor/tbb.lua
+++ code/vendor/tbb.lua
@@ -7,7 +7,7 @@ return {
 		includedirs "../vendor/tbb/include/"
 
 		if os.istarget('linux') then
-			defines 'TBB_USE_GLIBCXX_VERSION=90200'
+			defines { 'TBB_USE_GLIBCXX_VERSION=90200', '__TBB_RESUMABLE_TASKS=0' }
 		end
 	end,
 
