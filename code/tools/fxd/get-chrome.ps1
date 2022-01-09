a46f5e0c3648accb880f0e0056442fc90628043f
blattersturm <peachypies@protonmail.ch>
2022-01-09 09:16:27 +0100

    internal(tools/fxd): `get-chrome` making build/ dir if needed

--- code/tools/fxd/get-chrome.ps1
+++ code/tools/fxd/get-chrome.ps1
@@ -6,6 +6,8 @@ Downloads the 64-bit CEF dependency to vendor/cef/.
 $WorkDir = "$PSScriptRoot\..\..\..\"
 $SaveDir = "$WorkDir\code\build\"
 
+if (!(Test-Path $SaveDir)) { New-Item -ItemType Directory -Force $SaveDir | Out-Null }
+
 $CefName = (Get-Content $PSScriptRoot\..\ci\build.ps1 | Select-String "CefName =") -replace ".*`"(.*?)`"", "`$1"
 
 if (!(Test-Path "$SaveDir\$CefName.zip")) {
