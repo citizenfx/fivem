42a61456edd2530f8d63069b49fb60f5b40703cc
blattersturm <peachypies@protonmail.ch>
2022-01-14 12:27:13 +0100

    tweak(formats): 'five->rdr3 potential exaggerated reflection fix'
    By @cpmodding. Fixes #1226.

--- code/components/rage-formats-x/include/convert/gtaDrawable_five_rdr3.h
+++ code/components/rage-formats-x/include/convert/gtaDrawable_five_rdr3.h
@@ -724,9 +724,13 @@ rdr3::grmShaderGroup* convert(five::grmShaderGroup* shaderGroup)
 				std::array<uint8_t, 16> value;
 				memcpy(value.data(), params[i].GetValue(), 16);
 
-				if (pn == HashString("Specular"))
+				if (pn == HashString("SpecularColor"))
 				{
-					*(float*)(value.data()) /= 40.f;
+					*(float*)(value.data()) /= 4.f;
+				}
+				else if (pn == HashString("Specular"))
+				{
+					*(float*)(value.data()) /= 160.f;
 				}
 
 				paramRefs.emplace_back(pn, value);
