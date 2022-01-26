dae83f643b2ea1e2488e1a57b3b1d53988a2128d
blattersturm <peachypies@protonmail.ch>
2022-01-26 09:12:55 +0100

    fix(client): correct 6d9e5e6520fe38e6bea4ddebaf599c8703bde42a to not add 1604/1311
    These being split would still pollute crash reporting with 'old' IDs.

--- code/client/shared/CfxSubProcess.h
+++ code/client/shared/CfxSubProcess.h
@@ -77,7 +77,11 @@ inline const wchar_t* MakeCfxSubProcess(const std::wstring& processType, const s
 	{
 #if defined(GTA_FIVE) || defined(IS_RDR3)
 		auto buildNumber = xbr::GetGameBuild();
-		productName += fmt::sprintf(L"b%d_", buildNumber);
+
+		if (buildNumber != 1604 && buildNumber != 1311)
+		{
+			productName += fmt::sprintf(L"b%d_", buildNumber);
+		}
 #endif
 #endif
 	}
