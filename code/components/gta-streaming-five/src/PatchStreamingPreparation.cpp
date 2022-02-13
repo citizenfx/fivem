3a576deb47781dbb89d6a09cab923cc61eb2b855
blattersturm <peachypies@protonmail.ch>
2022-02-13 16:48:01 +0100

    internal(streaming/five): remove misleading dead code
    PatchStreamingPreparation doesn't use this code anymore since it got
    refactored.

--- code/components/gta-streaming-five/src/PatchStreamingPreparation.cpp
+++ code/components/gta-streaming-five/src/PatchStreamingPreparation.cpp
@@ -36,9 +36,6 @@ hook::cdecl_stub<rage::fiCollection*()> getRawStreamer([]()
 	return hook::get_call(hook::get_pattern("48 8B D3 4C 8B 00 48 8B C8 41 FF 90 ? 01 00 00", -5));
 });
 
-static std::unordered_map<std::string, uint32_t> g_thisSeenRequests;
-static std::unordered_set<std::string> g_erasureQueue;
-
 static bool ProcessHandler(HANDLE sema, char* a1)
 {
 	bool isSignaled = WaitForSingleObject(sema, 0) == WAIT_OBJECT_0;
@@ -103,210 +100,6 @@ static void Hook_StreamingSema()
 		auto location = hook::get_pattern("48 8B 8C C6 18 0A 03 00 48", 12);
 		hook::call(location, weirdStub.GetCode());
 	}
-}
-
-#include <mmsystem.h>
-
-static void ProcessErasure()
-{
-	static uint32_t lastRanErasure;
-
-	if ((timeGetTime() - lastRanErasure) < 500)
-	{
-		return;
-	}
-
-	lastRanErasure = timeGetTime();
-
-	// this test isn't too accurate but it should at least erase abandoned handles from RCD
-	{
-		for (auto& request : g_thisSeenRequests)
-		{
-			if ((lastRanErasure - request.second) > 500)
-			{
-				auto it = g_handleMap.find(request.first);
-
-				if (it != g_handleMap.end())
-				{
-					g_erasureQueue.insert(request.first);
-				}
-			}
-			else
-			{
-				g_erasureQueue.erase(request.first);
-			}
-		}
-	}
-
-	// erasing these entries is deferred since closing an in-transit entry in ResourceCacheDevice will cause download corruption of sorts
-	{
-		for (auto it = g_erasureQueue.begin(); it != g_erasureQueue.end(); )
-		{
-			auto entry = *it;
-
-			// should be removed from the erasure queue?
-			bool erased = false;
-
-			// get the handle data
-			auto hIt = g_handleMap.find(entry);
-
-			if (hIt != g_handleMap.end())
-			{
-				auto[device, handle, ptr] = hIt->second;
-
-				// perform a status bulk read
-				char readBuffer[2048];
-				uint32_t numRead = device->ReadBulk(handle, ptr, readBuffer, 0xFFFFFFFD);
-
-				// download completed?
-				if (numRead != 0)
-				{
-					// erase the entry
-					device->CloseBulk(handle);
-					g_handleMap.erase(hIt);
-
-					erased = true;
-				}
-			}
-			else
-			{
-				// no longer relevant to us, remove from erasure queue
-				erased = true;
-			}
-
-			// increase iterator if needed
-			if (!erased)
-			{
-				it++;
-			}
-			else
-			{
-				it = g_erasureQueue.erase(it);
-			}
-		}
-	}
-}
-
-static int HandleObjectLoadWrap(streaming::Manager* streaming, int a2, int a3, int* requests, int a5, int a6, int a7)
-{
-	OPTICK_EVENT();
-
-	int remainingRequests = g_origHandleObjectLoad(streaming, a2, a3, requests, a5, a6, a7);
-
-	for (int i = remainingRequests - 1; i >= 0; i--)
-	{
-		bool shouldRemove = false;
-		int index = requests[i];
-
-		auto entry = &streaming->Entries[index];
-		auto pf = streaming::GetStreamingPackfileForEntry(entry);
-
-		rage::fiCollection* collection = nullptr;
-		
-		if (pf)
-		{
-			collection = reinterpret_cast<rage::fiCollection*>(pf->packfile);
-		}
-
-		if (!collection && (entry->handle >> 16) == 0)
-		{
-			collection = getRawStreamer();
-		}
-
-		if (collection)
-		{
-			char fileNameBuffer[1024];
-			strcpy(fileNameBuffer, "CfxRequest");
-
-			collection->GetEntryNameToBuffer(entry->handle & 0xFFFF, fileNameBuffer, sizeof(fileNameBuffer));
-
-			bool isCache = false;
-			std::string fileName;
-
-			if (strncmp(fileNameBuffer, "cache:/", 7) == 0)
-			{
-				fileName = std::string("cache_nb:/") + &fileNameBuffer[7];
-				isCache = true;
-			}
-
-			if (strncmp(fileNameBuffer, "compcache:/", 11) == 0)
-			{
-				fileName = std::string("compcache_nb:/") + &fileNameBuffer[11];
-				isCache = true;
-			}
-
-			if (isCache)
-			{
-				OPTICK_EVENT("isCache");
-
-				g_thisSeenRequests[fileName] = timeGetTime();
-
-				rage::fiDevice* device;
-				uint64_t handle;
-				uint64_t ptr;
-
-				if (g_handleMap.find(fileName) != g_handleMap.end())
-				{
-					std::tie(device, handle, ptr) = g_handleMap[fileName];
-				}
-				else
-				{
-					device = rage::fiDevice::GetDevice(fileName.c_str(), true);
-					handle = device->OpenBulk(fileName.c_str(), &ptr);
-				}
-
-				if (handle != -1)
-				{
-					char readBuffer[2048];
-					uint32_t numRead;
-
-					{
-						OPTICK_EVENT("readBulk");
-						numRead = device->ReadBulk(handle, ptr, readBuffer, 0xFFFFFFFE);
-					}
-
-					bool killHandle = true;
-
-					if (numRead == 0)
-					{
-						killHandle = false;
-						shouldRemove = true;
-
-						g_handleMap.insert({ fileName, { device, handle, ptr } });
-					}
-					else if (numRead == -1)
-					{
-						shouldRemove = true;
-
-						std::string error;
-						ICoreGameInit* init = Instance<ICoreGameInit>::Get();
-
-						init->GetData("gta-core-five:loadCaller", &error);
-
-						FatalError("Failed to request %s: %s. %s", fileName, vfs::GetLastError(vfs::GetDevice(fileName)), error);
-					}
-
-					if (killHandle)
-					{
-						device->CloseBulk(handle);
-						g_handleMap.erase(fileName);
-					}
-				}
-				else
-				{
-					shouldRemove = true;
-				}
-			}
-		}
-
-		if (shouldRemove)
-		{
-			memmove(&requests[i], &requests[i + 1], (remainingRequests - i - 1) * sizeof(int));
-			remainingRequests -= 1;
-		}
-	}
-
-	return remainingRequests;
 }
 
 struct datResourceChunk
@@ -716,15 +509,9 @@ static HookFunction hookFunction([] ()
 
 	Hook_StreamingSema();
 
-	// dequeue GTA streaming request function
-	//auto location = hook::get_pattern("89 7C 24 28 4C 8D 7C 24 40 89 44 24 20 E8", 13);
-	//hook::set_call(&g_origHandleObjectLoad, location);
-	//hook::call(location, HandleObjectLoadWrap);
-
 	OnMainGameFrame.Connect([]()
 	{
 		ProcessRemoval();
-		//ProcessErasure();
 	});
 
 	// redirect pgStreamer::Read for custom streaming reads
