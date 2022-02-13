citizenfx/fivem

commit 67d0d60fc2da64a82c8a0c08d5c1fe5d2c374940

tweak(core/five): don't mention gameconfig in pool errors
Even though this text was present in the original game's assertion code
according to builds.xml, showing this to users here is misleading: for
one, gameconfig.xml is handled by game compliance and therefore will
lead to game verification next launch, and secondly, users changing this
locally is very inconsistent and generally unwanted.

code/components/gta-core-five/src/PoolManagement.cpp
@@ -257,11 +257,7 @@ static void* PoolAllocateWrap(atPoolBase* pool)
 
 		AddCrashometry("pool_error", "%s (%d)", poolName, pool->GetSize());
 
-		std::string extraWarning = (poolName.find("0x") == std::string::npos)
-			? fmt::sprintf(" (you need to raise %s PoolSize in common/data/gameconfig.xml)", poolName)
-			: "";
-
-		FatalErrorNoExcept("%s Pool Full, Size == %d%s", poolName, pool->GetSize(), extraWarning);
+		FatalErrorNoExcept("%s Pool Full, Size == %d%s", poolName, pool->GetSize());
 	}
 
 	return value;

commit 82156103e9d66d4d850d70ece1c4e3c7036db183

fix(core/five): formatting error in pool error
This was introduced in 67d0d60fc2da64a82c8a0c08d5c1fe5d2c374940 and of
course leads to pool errors instead being replaced with a C++ exception
handler for `fmt::v8::format_error`.

Misleading, again.

code/components/gta-core-five/src/PoolManagement.cpp
@@ -257,7 +257,7 @@ static void* PoolAllocateWrap(atPoolBase* pool)
 
 		AddCrashometry("pool_error", "%s (%d)", poolName, pool->GetSize());
 
-		FatalErrorNoExcept("%s Pool Full, Size == %d%s", poolName, pool->GetSize());
+		FatalErrorNoExcept("%s Pool Full, Size == %d", poolName, pool->GetSize());
 	}
 
 	return value;