c1164f20d0ec37fd71f0c404a2089292578dc41e
blattersturm <peachypies@protonmail.ch>
2022-03-25 11:11:46 +0100

    tweak(server): add `http_perf` rate limiter
    A server owner requested adding this rate limiter in 'releng'.

--- code/components/citizen-server-impl/src/PerfHttpHandler.cpp
+++ code/components/citizen-server-impl/src/PerfHttpHandler.cpp
@@ -5,6 +5,9 @@
 
 #include <ServerPerfComponent.h>
 
+#include <TcpListenManager.h>
+#include <KeyedRateLimiter.h>
+
 #include <prometheus/serializer.h>
 #include <prometheus/text_serializer.h>
 
@@ -19,6 +22,24 @@ static InitFunction initFunction([]()
 	{
 		instance->GetComponent<fx::HttpServerManager>()->AddEndpoint("/perf/", [instance](const fwRefContainer<net::HttpRequest>& request, const fwRefContainer<net::HttpResponse>& response)
 		{
+			static auto limiter = instance->GetComponent<fx::PeerAddressRateLimiterStore>()->GetRateLimiter("http_perf", fx::RateLimiterDefaults{ 2.0, 5.0 });
+			auto address = request->GetRemotePeer();
+
+			bool cooldown = false;
+
+			if (!fx::IsProxyAddress(address) && !limiter->Consume(address, 1.0, &cooldown))
+			{
+				if (cooldown)
+				{
+					response->CloseSocket();
+					return;
+				}
+
+				response->SetStatusCode(429);
+				response->End("Rate limit exceeded.");
+				return;
+			}
+
 			auto promRegistry = instance->GetComponent<fx::ServerPerfComponent>()->GetRegistry();
 			auto metrics = promRegistry->Collect();
 
