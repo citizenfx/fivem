c7568664a321cf862be4693a386986c64254442a
blattersturm <peachypies@protonmail.ch>
2022-03-25 10:50:05 +0100

    tweak(system-resources): don't build webadmin
    This seems to have broken with some newer .NET deployments (nice compat)
    and since this is unmaintained and likely unused in general, we don't
    need to build it anymore.
    
    If anyone wants to use it, it should work if dropped in from older
    server builds.

--- ext/system-resources/build.cmd
+++ ext/system-resources/build.cmd
@@ -1,11 +1,5 @@
 @echo off
 
-where /q dotnet
-
-if errorlevel 1 (
-    exit /B 1
-)
-
 where /q node
 
 if errorlevel 1 (
@@ -13,17 +7,6 @@ if errorlevel 1 (
 )
 
 set SRRoot=%~dp0\data
-
-pushd ..\webadmin\server
-dotnet publish -c Release
-popd
-
-mkdir %SRRoot%\webadmin\wwwroot\
-mkdir %SRRoot%\webadmin\server\bin\Release\netstandard2.0\publish\
-copy /y ..\webadmin\fxmanifest.lua %SRRoot%\webadmin\
-
-xcopy /y /e ..\webadmin\wwwroot\. %SRRoot%\webadmin\wwwroot\
-xcopy /y /e ..\webadmin\server\bin\Release\netstandard2.0\publish\. %SRRoot%\webadmin\server\bin\Release\netstandard2.0\publish\
 
 pushd ..\txAdmin
 call npm install -g npm@7.19.1
