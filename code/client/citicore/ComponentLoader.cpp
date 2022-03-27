2d8235dba178d0856d98bea4d8eb611c01ff7a74
blattersturm <peachypies@protonmail.ch>
2022-03-27 18:12:07 +0200

    tweak(client): launch in insecure mode on WoA/Wine
    These environments currently break silently when running with adhesive.

--- code/client/citicore/ComponentLoader.cpp
+++ code/client/citicore/ComponentLoader.cpp
@@ -150,9 +150,41 @@ void ComponentLoader::Initialize()
 #endif
 
 #ifndef IS_FXSERVER
-		if (nameWide == L"adhesive" && (launch::IsSDK() || launch::IsSDKGuest()))
+		if (nameWide == L"adhesive")
 		{
-			continue;
+			if (launch::IsSDK() || launch::IsSDKGuest())
+			{
+				continue;
+			}
+
+			if (CfxIsWine() || GetModuleHandleW(L"xtajit64.dll") != nullptr)
+			{
+				auto cfxState = CfxState::Get();
+				if (cfxState->IsMasterProcess())
+				{
+					std::wstring environmentType;
+
+					if (CfxIsWine())
+					{
+						environmentType = L"Wine";
+					}
+					else if (GetModuleHandleW(L"xtajit64.dll") != nullptr)
+					{
+						environmentType = L"Windows on ARM";
+					}
+
+					MessageBoxW(NULL, 
+						va(
+							L"The game is running in insecure mode because %s is not supported by the anti-cheat components at this time.\n"
+							L"Most servers, as well as some authentication features will be unavailable.",
+							environmentType),
+						L"Cfx.re: Insecure mode", MB_OK | MB_ICONWARNING);
+				}
+
+				AddComponent(new DllGameComponent(va(PLATFORM_LIBRARY_STRING, L"sticky")));
+
+				continue;
+			}
 		}
 #endif
 
