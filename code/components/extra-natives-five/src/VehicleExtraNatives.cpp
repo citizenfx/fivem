citizenfx/fivem

commit f8424b7f8ec2f6b9d49660a73e1ee9077c9085ee

tweak(game/five): refactor fly through windscreen logic to be convar toggleable

code/components/extra-natives-five/src/VehicleExtraNatives.cpp
@@ -48,6 +48,9 @@ static std::unordered_set<fwEntity*> g_skipRepairVehicles{};
 
 static std::vector<FlyThroughWindscreenParam> g_flyThroughWindscreenParams{};
 
+static bool* g_flyThroughWindscreenDisabled;
+static bool isFlyThroughWindscreenEnabledConVar = false;
+
 template<typename T>
 inline static T readValue(fwEntity* ptr, int offset)
 {
@@ -381,6 +384,15 @@ static HookFunction initFunction([]()
 
 		DrawHandlerPtrOffset = *(uint8_t*)(location + 4);
 		HandlingDataPtrOffset = *(uint32_t*)(location - 35);
+	}
+
+	{
+		// replace netgame check for fly through windscreen with our variable
+		g_flyThroughWindscreenDisabled = (bool*)hook::AllocateStubMemory(1);
+		static ConVar<bool> enableFlyThroughWindscreen("game_enableFlyThroughWindscreen", ConVar_Replicated, false, &isFlyThroughWindscreenEnabledConVar);
+
+		auto location = hook::get_pattern<uint32_t>("45 33 ED 44 38 2D ? ? ? ? 4D", 6);
+		*location = (intptr_t)g_flyThroughWindscreenDisabled - (intptr_t)location - 4;
 	}
 
 	{
@@ -897,6 +909,11 @@ static HookFunction initFunction([]()
 		{
 			ResetFlyThroughWindscreenParams();
 		}
+	});
+
+	OnMainGameFrame.Connect([]()
+	{
+		*g_flyThroughWindscreenDisabled = !isFlyThroughWindscreenEnabledConVar;
 	});
 
 	OnKillNetworkDone.Connect([]()

code/components/gta-streaming-five/src/UnkStuff.cpp
@@ -775,9 +775,6 @@ static HookFunction hookFunction([]()
 		// netgame checks for vehicle fuel tank leaking
 		hook::nop(hook::get_pattern("48 83 65 7F 00 0F 29 45 27 48 8D 45 77", -0xE3), 7);
 		hook::nop(hook::get_call(hook::get_pattern<char>("40 53 48 83 EC 20 48 8B D9 E8 ? ? ? ? 84 C0 74 32 8A 83 D8", 9)) + 0x19, 2);
-
-		// netgame check for fly through windscreen
-		hook::nop(hook::get_pattern("45 33 ED 44 38 ? ? ? ? 02 4D 8B E1", 3), 7);
 	}
 
 	// increase the heap size for allocator 0

commit 838b4147d45853eb5e71bd9dca9e2526495a3566

fix(game/five): enable 'flying out of windscreen' for SP
The patches added in f8424b7f8ec2f6b9d49660a73e1ee9077c9085ee broke this
behavior in non-network games.

Instead, we now only set the 'fake' network flag to true if we are in
network mode in the first place.

code/components/extra-natives-five/src/VehicleExtraNatives.cpp
@@ -364,8 +364,12 @@ TrainDoor* GetTrainDoor(fwEntity* train, uint32_t index)
 	return &(*((TrainDoor**)(((char*)train) + TrainDoorArrayPointerOffset)))[index];
 }
 
+static bool* isNetworkGame;
+
 static HookFunction initFunction([]()
 {
+	isNetworkGame = hook::get_address<bool*>(hook::get_pattern("24 07 3C 03 75 12 40 38 35 ? ? ? ? 75 09 83", 9));
+
 	{
 		ModelInfoPtrOffset = *hook::get_pattern<uint8_t>("48 8B 40 ? 0F B6 80 ? ? ? ? 83 E0 1F", 3);
 		GravityOffset = *hook::get_pattern<uint32_t>("0F C6 F6 00 F3 0F 59 05", -4);
@@ -1188,7 +1192,7 @@ static HookFunction initFunction([]()
 
 	OnMainGameFrame.Connect([]()
 	{
-		*g_flyThroughWindscreenDisabled = !isFlyThroughWindscreenEnabledConVar;
+		*g_flyThroughWindscreenDisabled = *isNetworkGame && !isFlyThroughWindscreenEnabledConVar;
 	});
 
 	OnKillNetworkDone.Connect([]()