940e10ac6d9aea29fff84fcf80c6ac55eb435336
blattersturm <peachypies@protonmail.ch>
2022-04-22 11:39:24 +0200

    fix(shared): allow Disconnect of fxEvent from cb
    This is needed for the change after this one.

--- code/client/shared/EventCore.h
+++ code/client/shared/EventCore.h
@@ -391,8 +391,12 @@ public:
 			return true;
 		}
 
-		for (auto cb = m_callbacks.get(); cb; cb = cb->next.get())
+		decltype(m_callbacks.get()) next = {};
+
+		for (auto cb = m_callbacks.get(); cb; cb = next)
 		{
+			next = cb->next.get();
+
 			if (!std::invoke(cb->function, args...))
 			{
 				return false;
