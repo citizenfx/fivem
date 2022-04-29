citizenfx/fivem

commit 83051726e61a5c7ff33d999317b67179155c8fba

fix(level-loader/five): editor reloading
"networkTimedOut" can likely be removed; will require testing the
"Connection interrupted" bits beforehand.

code/components/citizen-level-loader-five/src/LevelLoader.cpp
@@ -35,13 +35,14 @@ static std::string g_overrideNextLoadedLevel;
 static std::string g_nextLevelPath;
 
 static bool g_wasLastLevelCustom;
+static bool g_gameUnloaded = false;
 
 static void(*g_origLoadLevelByIndex)(int);
 static void(*g_loadLevel)(const char* levelPath);
 
 enum NativeIdentifiers : uint64_t
 {
-	GET_PLAYER_PED = 0x43A66C31C68491C0,
+	PLAYER_PED_ID = 0xD80958FC74E988A6,
 	SET_ENTITY_COORDS = 0x621873ECE1178967,
 	LOAD_SCENE = 0x4448EB75B4904BDB,
 	SHUTDOWN_LOADING_SCREEN = 0x078EBE9809CCD637,
@@ -55,23 +56,21 @@ private:
 
 public:
 	SpawnThread()
+		: m_doInityThings(false)
 	{
-		m_doInityThings = true;
 	}
 
-	virtual rage::eThreadState Reset(uint32_t scriptHash, void* pArgs, uint32_t argCount) override
+	void ResetInityThings()
 	{
 		m_doInityThings = true;
-
-		return GtaThread::Reset(scriptHash, pArgs, argCount);
 	}
 
 	virtual void DoRun() override
 	{
-		uint32_t playerPedId = NativeInvoke::Invoke<GET_PLAYER_PED, uint32_t>(-1);
-
 		if (m_doInityThings)
 		{
+			uint32_t playerPedId = NativeInvoke::Invoke<PLAYER_PED_ID, uint32_t>();
+
 			NativeInvoke::Invoke<SHUTDOWN_LOADING_SCREEN, int>();
 			NativeInvoke::Invoke<DO_SCREEN_FADE_IN, int>(0);
 
@@ -226,7 +225,7 @@ static void LoadLevel(const char* levelName)
 	{
 		if ((!gameInit->HasVariable("storyMode") && !gameInit->HasVariable("localMode")) || gameInit->HasVariable("editorMode"))
 		{
-			rage::scrEngine::CreateThread(&spawnThread);
+			spawnThread.ResetInityThings();
 		}
 
 		gameInit->LoadGameFirstLaunch([] ()
@@ -242,9 +241,10 @@ static void LoadLevel(const char* levelName)
 		bool lm = gameInit->HasVariable("localMode");
 		bool em = gameInit->HasVariable("editorMode");
 
-		gameInit->KillNetwork((wchar_t*)1);
+		// This function should probably be cognizant of 'g_isNetworkKilled' in BlockLoadSetters.
+		//gameInit->KillNetwork((wchar_t*)1);
 
-		g_onShutdownQueue.push([gameInit, sm, lm, em]()
+		auto fEvent = ([gameInit, sm, lm, em]()
 		{
 			gameInit->ReloadGame();
 
@@ -260,12 +260,25 @@ static void LoadLevel(const char* levelName)
 
 			if (em)
 			{
+				gameInit->SetVariable("localMode"); // see editorModeCommand. 'ShouldSkipLoading' will return false otherwise.
 				gameInit->SetVariable("editorMode");
+				spawnThread.ResetInityThings();
 			}
 
 			gameInit->SetVariable("networkInited");
 			gameInit->ShAllowed = true;
 		});
+
+		if (g_gameUnloaded)
+		{
+			fEvent();
+
+			g_gameUnloaded = false;
+		}
+		else
+		{
+			g_onShutdownQueue.push(fEvent); // OnShutdownSession
+		}
 	}
 }
 
@@ -445,6 +458,16 @@ static InitFunction initFunction([] ()
 		LoadLevel(level.c_str());
 	});
 
+	rage::scrEngine::OnScriptInit.Connect([] ()
+	{
+		rage::scrEngine::CreateThread(&spawnThread);
+	});
+
+	Instance<ICoreGameInit>::Get()->OnGameRequestLoad.Connect([]()
+	{
+		g_gameUnloaded = false;
+	});
+
 	Instance<ICoreGameInit>::Get()->OnShutdownSession.Connect([]()
 	{
 		std::function<void()> fn;
@@ -453,5 +476,7 @@ static InitFunction initFunction([] ()
 		{
 			fn();
 		}
+
+		g_gameUnloaded = true;
 	}, INT32_MAX);
 });

code/components/gta-net-five/src/NetHook.cpp
@@ -966,6 +966,7 @@ static HookFunction initFunction([]()
 			OnKillNetwork.Connect([](const char*)
 			{
 				gameLoaded = false;
+				Instance<ICoreGameInit>::Get()->ClearVariable("networkTimedOut");
 			});
 
 			if (Instance<ICoreGameInit>::Get()->GetGameLoaded())

code/components/rage-scripting-five/src/scrEngine.cpp
@@ -512,14 +512,11 @@ static int ReturnTrueFromScript(void* a1, void* a2)
 	return true;
 }
 
-static void(*g_origResetOwnedThreads)();
+static void(*g_CTheScripts__Shutdown)(void);
 
-static void ResetOwnedThreads()
+static void CTheScripts__Shutdown()
 {
-	if (Instance<ICoreGameInit>::Get()->HasVariable("storyMode"))
-	{
-		return g_origResetOwnedThreads();
-	}
+	g_CTheScripts__Shutdown();
 
 	for (auto& thread : g_ownedThreads)
 	{
@@ -632,7 +629,8 @@ static HookFunction hookFunction([] ()
 
 		// replace `startup` initialization with resetting all owned threads
 		//hook::jump(hook::get_pattern("48 63 18 83 FB FF 0F 84 D6", -0x34), ResetOwnedThreads);
-		MH_CreateHook(hook::get_pattern("48 63 18 83 FB FF 0F 84 D6", -0x34), ResetOwnedThreads, (void**)&g_origResetOwnedThreads);
+		//MH_CreateHook(hook::get_pattern("48 63 18 83 FB FF 0F 84 D6", -0x34), ResetOwnedThreads, (void**)&g_origResetOwnedThreads);
+		MH_CreateHook(hook::get_pattern("48 8B 0D ? ? ? ? 33 D2 48 8B 01 FF 10", -0x58), CTheScripts__Shutdown, (void**)&g_CTheScripts__Shutdown);
 
 		MH_EnableHook(MH_ALL_HOOKS);
 	}

commit 79f337fcf44819519b2f6c071d2cb0be91f299d1

fix(level-loader/five): re-add reloading on mode switch
This issue was introduced in 83051726e61a5c7ff33d999317b67179155c8fba,
likely inadvertently.

Reported in https://forum.cfx.re/t/4845352.

code/components/citizen-level-loader-five/src/LevelLoader.cpp
@@ -215,7 +215,7 @@ static concurrency::concurrent_queue<std::function<void()>> g_onShutdownQueue;
 
 static void LoadLevel(const char* levelName)
 {
-	ICoreGameInit* gameInit = Instance<ICoreGameInit>::Get();
+	static auto gameInit = Instance<ICoreGameInit>::Get();
 
 	gameInit->SetVariable("networkInited");
 
@@ -241,10 +241,9 @@ static void LoadLevel(const char* levelName)
 		bool lm = gameInit->HasVariable("localMode");
 		bool em = gameInit->HasVariable("editorMode");
 
-		// This function should probably be cognizant of 'g_isNetworkKilled' in BlockLoadSetters.
-		//gameInit->KillNetwork((wchar_t*)1);
+		gameInit->KillNetwork((wchar_t*)1);
 
-		auto fEvent = ([gameInit, sm, lm, em]()
+		auto fEvent = ([sm, lm, em]()
 		{
 			gameInit->ReloadGame();
 

code/components/gta-core-five/src/BlockLoadSetters.cpp
@@ -97,7 +97,7 @@ static void WaitForInitLoopWrap()
 	WaitForInitLoop();
 }
 
-static volatile bool g_isNetworkKilled;
+volatile bool g_isNetworkKilled;
 
 enum LoadingScreenContext
 {

code/components/gta-core-five/src/GameInit.cpp
@@ -44,9 +44,15 @@ static hook::cdecl_stub<int(bool, int)> getWarningResult([] ()
 
 static bool g_showWarningMessage;
 static std::string g_warningMessage;
+extern volatile bool g_isNetworkKilled;
 
 void FiveGameInit::KillNetwork(const wchar_t* errorString)
 {
+	if (g_isNetworkKilled)
+	{
+		return;
+	}
+
 	if (errorString == (wchar_t*)1)
 	{
 		OnKillNetwork("Reloading game.");