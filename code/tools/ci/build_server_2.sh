bbc87951902ef7aa966ad99cd47e115dee93526e
blattersturm <peachypies@protonmail.ch>
2022-06-14 18:55:28 +0200

    fix(build): force Alpine build to Clang/LLVM 13
    Because of llvm/llvm-project#53269 breaking cURL (curl/curl#8293), the
    Linux build would fail.
    
    Also, removes the unneeded `python2` dependency so it won't be causing
    problems when we can bump LLVM again.
    
    Fixes #1487.

--- code/tools/ci/build_server_2.sh
+++ code/tools/ci/build_server_2.sh
@@ -9,6 +9,8 @@ JOB_SLOTS=${JOB_SLOTS:-24}
 # upgrade to edge (keep v3.12 for downgrades)
 echo http://dl-cdn.alpinelinux.org/alpine/v3.12/main > /etc/apk/repositories
 echo http://dl-cdn.alpinelinux.org/alpine/v3.14/main >> /etc/apk/repositories
+echo http://dl-cdn.alpinelinux.org/alpine/v3.16/main >> /etc/apk/repositories # for LLVM 13
+echo http://dl-cdn.alpinelinux.org/alpine/v3.16/community >> /etc/apk/repositories # for LLVM 13 ('lld')
 echo http://dl-cdn.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories
 echo http://dl-cdn.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories
 echo http://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories
@@ -39,7 +41,7 @@ apk del curl
 apk add --no-cache curl=7.72.0-r99 libssl1.1 libcrypto1.1 libunwind libstdc++ zlib c-ares v8~=9.3 musl-dbg libatomic
 
 # install compile-time dependencies
-apk add --no-cache --virtual .dev-deps lld curl-dev=7.72.0-r99 clang clang-dev build-base linux-headers openssl1.1-compat-dev python2 python3 py3-pip lua5.3 lua5.3-dev mono-reference-assemblies=5.16.1.0-r9991 mono-dev=5.16.1.0-r9991 libmono=5.16.1.0-r9991 mono-corlib=5.16.1.0-r9991 mono=5.16.1.0-r9991 mono-reference-assemblies-4.x=5.16.1.0-r9991 mono-reference-assemblies-facades=5.16.1.0-r9991 mono-csc=5.16.1.0-r9991 mono-runtime=5.16.1.0-r9991 c-ares-dev v8-dev~=9.3 nodejs-current nodejs-current-dev npm yarn clang-libs git
+apk add --no-cache --virtual .dev-deps lld~=13 curl-dev=7.72.0-r99 clang-dev~=13 clang~=13 build-base linux-headers openssl1.1-compat-dev python3 py3-pip lua5.3 lua5.3-dev mono-reference-assemblies=5.16.1.0-r9991 mono-dev=5.16.1.0-r9991 libmono=5.16.1.0-r9991 mono-corlib=5.16.1.0-r9991 mono=5.16.1.0-r9991 mono-reference-assemblies-4.x=5.16.1.0-r9991 mono-reference-assemblies-facades=5.16.1.0-r9991 mono-csc=5.16.1.0-r9991 mono-runtime=5.16.1.0-r9991 c-ares-dev v8-dev~=9.3 nodejs-current nodejs-current-dev npm yarn clang-libs~=13 git
 
 # install python deps
 python3 -m pip install ply six Jinja2 MarkupSafe
