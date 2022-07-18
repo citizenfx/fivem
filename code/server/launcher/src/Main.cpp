ded89bc6acf29a720a7686a1de70d28b62c75af8
blattersturm <peachypies@protonmail.ch>
2022-07-18 18:50:12 +0200

    Revert "Revert "tweak(server/launcher): remove Linux block message""
    This reverts commit a5267322cec1a7b30b159da4cb413388f8cf7efd.
    
    As of b296a83f556d468740e82b4f8a39108cad6865ed the Linux build may be
    fixed, finally.

--- code/server/launcher/src/Main.cpp
+++ code/server/launcher/src/Main.cpp
@@ -12,7 +12,6 @@
 #ifndef _WIN32
 #define _GNU_SOURCE
 #include <pthread.h>
-#include <stdio.h>
 #endif
 
 bool InitializeExceptionHandler(int argc, char* argv[]);
@@ -20,12 +19,6 @@ bool InitializeExceptionHandler(int argc, char* argv[]);
 int main(int argc, char* argv[])
 {
 #ifndef _WIN32
-	printf("- ERROR -\n");
-	printf("The Cfx.re platform server is currently not working on Linux.\n");
-	printf("Use platform version 5715 or below (or the current recommended) instead. This issue will be fixed in the near future.\n\n");
-	printf("See https://forum.cfx.re/t/4881227/6 for more information and context.\n");
-	return 2;
-
 	pthread_attr_t attrs;
 	if (pthread_getattr_default_np(&attrs) == 0)
 	{
