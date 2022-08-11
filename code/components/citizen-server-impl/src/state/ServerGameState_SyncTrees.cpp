b39090ac3daa4f2ad3ae676615bae662f8238e86
blattersturm <peachypies@protonmail.ch>
2022-08-11 15:03:18 +0200

    tweak(gamestate/server): miscellaneous correctness changes
    Since #1565 doesn't reproduce with these changes in, I'll dump in these
    changes from my local working tree.
    
    These aren't tested in any real-world scenario, and some may be wrong or
    otherwise unfinished, but this includes:
    
      1. An attempt at timestamp splitting for blender-related data nodes.
         This likely was an attempt to mitigate extrapolation issues caused
         by out-of-order data nodes. It's however incomplete and may worsen
         blending instead, as it didn't seem to have the desired effect.
      2. Refactoring first-frame updates to always be sent in the same sync
         packet. This was initially attempted as a fix for the script owner-
         ship issue, but it didn't fix it by itself.
    
         This change however is likely related to #1565.

--- code/components/citizen-server-impl/src/state/ServerGameState_SyncTrees.cpp
+++ code/components/citizen-server-impl/src/state/ServerGameState_SyncTrees.cpp
@@ -102,4 +102,128 @@ std::shared_ptr<sync::SyncTreeBase> MakeSyncTree(sync::NetObjEntityType objectTy
 
 	return {};
 }
+
+namespace sync
+{
+	template<typename TVisitor>
+	bool VisitSyncTree(const SyncEntityPtr& entity, TVisitor&& cb)
+	{
+		auto st = entity->syncTree.get();
+
+		switch (entity->type)
+		{
+#ifdef STATE_FIVE
+			case sync::NetObjEntityType::Automobile:
+				return static_cast<sync::CAutomobileSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Bike:
+				return static_cast<sync::CBikeSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Boat:
+				return static_cast<sync::CBoatSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Door:
+				return static_cast<sync::CDoorSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Heli:
+				return static_cast<sync::CHeliSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Object:
+				return static_cast<sync::CObjectSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Ped:
+				return static_cast<sync::CPedSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Pickup:
+				return static_cast<sync::CPickupSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::PickupPlacement:
+				return static_cast<sync::CPickupPlacementSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Plane:
+				return static_cast<sync::CPlaneSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Submarine:
+				return static_cast<sync::CSubmarineSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Player:
+				return static_cast<sync::CPlayerSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Trailer:
+				return static_cast<sync::CAutomobileSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Train:
+				return static_cast<sync::CTrainSyncTree*>(st)->VisitT(cb);
+#elif defined(STATE_RDR3)
+			case sync::NetObjEntityType::Animal:
+				return static_cast<sync::CAnimalSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Automobile:
+				return static_cast<sync::CAutomobileSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Bike:
+				return static_cast<sync::CBikeSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Boat:
+				return static_cast<sync::CBoatSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Door:
+				return static_cast<sync::CDoorSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Heli:
+				return static_cast<sync::CHeliSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Object:
+				return static_cast<sync::CObjectSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Ped:
+				return static_cast<sync::CPedSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Pickup:
+				return static_cast<sync::CPickupSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::PickupPlacement:
+				return static_cast<sync::CPickupPlacementSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Plane:
+				return static_cast<sync::CPlaneSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Submarine:
+				return static_cast<sync::CSubmarineSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Player:
+				return static_cast<sync::CPlayerSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Trailer:
+				return static_cast<sync::CAutomobileSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Train:
+				return static_cast<sync::CTrainSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::DraftVeh:
+				return static_cast<sync::CDraftVehSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::StatsTracker:
+				return static_cast<sync::CStatsTrackerSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::PropSet:
+				return static_cast<sync::CPropSetSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::AnimScene:
+				return static_cast<sync::CAnimSceneSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::GroupScenario:
+				return static_cast<sync::CGroupScenarioSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Herd:
+				return static_cast<sync::CHerdSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Horse:
+				return static_cast<sync::CHorseSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::WorldState:
+				return static_cast<sync::CWorldStateSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::WorldProjectile:
+				return static_cast<sync::CWorldProjectileSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Incident:
+				return static_cast<sync::CIncidentSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Guardzone:
+				return static_cast<sync::CGuardzoneSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::PedGroup:
+				return static_cast<sync::CPedGroupSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::CombatDirector:
+				return static_cast<sync::CCombatDirectorSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::PedSharedTargeting:
+				return static_cast<sync::CPedSharedTargetingSyncTree*>(st)->VisitT(cb);
+			case sync::NetObjEntityType::Persistent:
+				return static_cast<sync::CPersistentSyncTree*>(st)->VisitT(cb);
+#endif
+		}
+
+		return false;
+	}
+
+	void IterateTimestamps(const SyncEntityPtr& entity, fu2::unique_function<void(uint32_t)>&& cb)
+	{
+		if (!cb)
+		{
+			return;
+		}
+
+		cb(entity->timestamp);
+
+		VisitSyncTree(entity, [&cb](auto& node)
+		{
+			if constexpr (AffectsBlender<std::decay_t<decltype(node.node)>>())
+			{
+				cb(node.timestamp);
+			}
+		});
+	}
+}
 }
