67ccd636bd6235a62d815f53e5d72cf9e76344a7
blattersturm <peachypies@protonmail.ch>
2022-09-07 00:00:48 +0200

    fix(tools): clear out old cef/ in fxd get-chrome
    Keeping the old directory intact would lead to old files from libcef_dll
    remaining, breaking a build.

--- code/tools/fxd/get-chrome.ps1
+++ code/tools/fxd/get-chrome.ps1
@@ -14,6 +14,10 @@ if (!(Test-Path "$SaveDir\$CefName.zip")) {
 	curl.exe --create-dirs -Lo "$SaveDir\$CefName.zip" "https://runtime.fivem.net/build/cef/$CefName.zip"
 }
 
+if (Test-Path $WorkDir\vendor\cef) {
+	Get-ChildItem -Directory $WorkDir\vendor\cef | Remove-Item -Recurse -Force
+}
+
 & $env:WINDIR\system32\tar.exe -C $WorkDir\vendor\cef -xf "$SaveDir\$CefName.zip"
 Copy-Item -Force -Recurse $WorkDir\vendor\cef\$CefName\* $WorkDir\vendor\cef\
 Remove-Item -Recurse $WorkDir\vendor\cef\$CefName\
