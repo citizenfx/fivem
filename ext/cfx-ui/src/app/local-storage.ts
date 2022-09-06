2d4c7b3de59c0e2f9dcf4d95ad8ec3f112412994
blattersturm <peachypies@protonmail.ch>
2022-09-06 16:10:44 +0200

    tweak(ext): remove cfx-ui [1/3]
    Replacement... shortly.

--- ext/cfx-ui/src/app/local-storage.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const LocalStorage = new InjectionToken('localStorage');
--- ext/cfx-ui/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  web: false,
-  game: true
-};
