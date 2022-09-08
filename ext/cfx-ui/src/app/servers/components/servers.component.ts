1b3c91d8c0764b77a9448236b328d0166ecbb78e
blattersturm <peachypies@protonmail.ch>
2022-09-08 12:33:55 +0200

    tweak(ext): remove `ng` cfx-ui [2/4]

--- ext/cfx-ui/src/app/servers/components/servers.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-	moduleId: module.id,
-	selector: 'servers',
-	template: '<router-outlet></router-outlet>',
-	styles: [':host { overflow: hidden; height: 100% }'],
-})
-export class ServersComponent {}
