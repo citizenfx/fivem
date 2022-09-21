e5c488c4bfec8655cc7e4e81f261ee319c3fbab7
nihonium <nihonium@fivem.net>
2022-09-21 12:27:49 +0200

    tweak(ext/cfx-ui): minor exitter tweak

--- ext/cfx-ui/src/cfx/apps/mpMenu/parts/NavBar/Exitter/Exitter.tsx
+++ ext/cfx-ui/src/cfx/apps/mpMenu/parts/NavBar/Exitter/Exitter.tsx
@@ -1,5 +1,5 @@
+import ReactFocusLock from "react-focus-lock";
 import { mpMenu } from "cfx/apps/mpMenu/mpMenu";
-// import { playSfx, Sfx } from "cfx/apps/mpMenu/utils/sfx";
 import { $L } from "cfx/common/services/intl/l10n";
 import { Button, ButtonTheme } from "cfx/ui/Button/Button";
 import { Icons } from "cfx/ui/Icons";
@@ -8,19 +8,11 @@ import { AfterTitleOutlet } from "cfx/ui/outlets";
 import { Text } from "cfx/ui/Text/Text";
 import { Title } from "cfx/ui/Title/Title";
 import { useOpenFlag } from "cfx/utils/hooks";
-// import { useEffect } from "react";
-import ReactFocusLock from "react-focus-lock";
 import { NavBarState } from "../NavBarState";
 import s from './Exitter.module.scss';
 
 export function Exitter() {
   const [confirmerOpen, openConfirmer, closeConfirmer] = useOpenFlag(false);
-
-  // useEffect(() => {
-  //   if (confirmerOpen) {
-  //     playSfx(Sfx.Maybe);
-  //   }
-  // }, [confirmerOpen]);
 
   const buttonTheme: ButtonTheme = NavBarState.forceTransparentNav
     ? 'default'
@@ -42,23 +34,25 @@ export function Exitter() {
           <div className={s.root}>
             <div className={s.wrapper}>
               <ReactFocusLock disabled={!confirmerOpen}>
-                <Flex centered="axis">
+                <Flex centered="axis" gap="xlarge">
                   <Text size="xxlarge">
-                    Are you sure?
+                    {$L('#ExitToDesktopConfirmation')}
                   </Text>
 
-                  <Button
-                    theme="default-blurred"
-                    size="large"
-                    text={$L('#ExitToDesktop')}
-                    onClick={() => mpMenu.invokeNative('exit')}
-                  />
-                  <Button
-                    theme="primary"
-                    size="large"
-                    text="Cancel"
-                    onClick={closeConfirmer}
-                  />
+                  <Flex gap="large">
+                    <Button
+                      theme="default-blurred"
+                      size="large"
+                      text={$L('#ExitToDesktop')}
+                      onClick={() => mpMenu.invokeNative('exit')}
+                    />
+                    <Button
+                      theme="primary"
+                      size="large"
+                      text="Cancel"
+                      onClick={closeConfirmer}
+                    />
+                  </Flex>
                 </Flex>
               </ReactFocusLock>
             </div>
