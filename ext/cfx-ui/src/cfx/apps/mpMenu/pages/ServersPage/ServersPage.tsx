4f44e6181b3500a01bf0d51034a639eced302ed9
nihonium <nihonium@fivem.net>
2022-09-29 14:51:24 +0200

    tweak(ext/cfx-ui): set shrunk size for server filters when pinned servers are shown correctly

--- ext/cfx-ui/src/cfx/apps/mpMenu/pages/ServersPage/ServersPage.tsx
+++ ext/cfx-ui/src/cfx/apps/mpMenu/pages/ServersPage/ServersPage.tsx
@@ -10,6 +10,8 @@ import { Navigate } from "react-router-dom";
 import { ServerFiltersWithDirectConnect } from "./ServerFiltersWithDirectConnect/ServerFiltersWithDirectConnect";
 import { currentGameNameIs } from "cfx/base/gameRuntime";
 import { GameName } from "cfx/base/game";
+import { clsx } from "cfx/utils/clsx";
+import s from './ServersPage.module.scss';
 
 export interface ServersPageProps {
   listType: ServersListType,
@@ -27,20 +29,18 @@ export const MpMenuServersPage = observer(function ({ listType }: ServersPagePro
   const config = serversList.getConfig?.();
   const showPinned = currentGameNameIs(GameName.FiveM) && listType === ServersListType.All;
 
+  const navbarClassName = clsx(s.navbar, {
+    [s.shrink]: showPinned,
+  });
+
   return (
     <>
       <InsideNavBar>
-        <Flex gap="large">
+        <Flex gap="large" className={navbarClassName}>
           <ListTypeTabs />
 
           {!!config && (
-            <>
-              <ServerFiltersWithDirectConnect config={config} />
-
-              {showPinned && (
-                <PinnedServersFilterSpacer />
-              )}
-            </>
+            <ServerFiltersWithDirectConnect config={config} />
           )}
         </Flex>
       </InsideNavBar>
@@ -53,13 +53,3 @@ export const MpMenuServersPage = observer(function ({ listType }: ServersPagePro
     </>
   );
 });
-
-function PinnedServersFilterSpacer() {
-  return (
-    <div
-      style={{
-        width: 'calc(var(--width) * .283)'
-      }}
-    />
-  );
-}
