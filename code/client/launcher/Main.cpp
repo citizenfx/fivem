57f7bbd718d5d246414d7f1ad79ba7f807963d82
blattersturm <peachypies@protonmail.ch>
2022-10-05 20:58:48 +0200

    fix(client/launcher): format_error in 'don't run as admin'
    The changeset in 2afb5cb33348f813bf5adc0b5497c787c5ff9fcc seems to have
    had a PRODUCT_NAME too little.
    
    Related Sentry issue: FIVEM-CLIENT-1604-WF3J

--- code/client/launcher/Main.cpp
+++ code/client/launcher/Main.cpp
@@ -677,7 +677,7 @@ int RealMain()
 					{
 						if (elevationData == TokenElevationTypeFull)
 						{
-							const wchar_t* elevationComplaint = va(gettext(L"%s does not support running under elevated privileges. Please change your Windows settings to not run %s as administrator.\nThe game will exit now."), PRODUCT_NAME);
+							const wchar_t* elevationComplaint = va(gettext(L"%s does not support running under elevated privileges. Please change your Windows settings to not run %s as administrator.\nThe game will exit now."), PRODUCT_NAME, PRODUCT_NAME);
 							MessageBox(nullptr, elevationComplaint, PRODUCT_NAME, MB_OK | MB_ICONERROR);
 
 							return 0;
