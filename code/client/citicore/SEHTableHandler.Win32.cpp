citizenfx/fivem

commit eab7a55b8f98149ac76af234107e2952c13d4cbb

tweak(server): exception override for win32 server
As described in d8722bcf851ee7542748bbe2d79a57d9daf1c9f9, this changeset
adds this override to the server so that heap corruption/such exceptions
will at least invoke the crash handler rather than 'weirdly' hanging in
WER, which also tends to confuse txAdmin's process monitoring logic.

code/client/citicore/SEHTableHandler.Win32.cpp
@@ -7,8 +7,7 @@
 
 #include "StdInc.h"
 
-#ifndef IS_FXSERVER
-#include <minhook.h>
+#include <MinHook.h>
 #include "Hooking.Aux.h"
 
 #include <Error.h>
@@ -393,7 +392,9 @@ static BOOLEAN WINAPI RtlDispatchExceptionStub(EXCEPTION_RECORD* record, CONTEXT
 		{
 			inExceptionFallback = true;
 
+#ifndef IS_FXSERVER
 			AddCrashometry("exception_override", "true");
+#endif
 
 			EXCEPTION_POINTERS ptrs;
 			ptrs.ContextRecord = context;
@@ -411,7 +412,7 @@ static BOOLEAN WINAPI RtlDispatchExceptionStub(EXCEPTION_RECORD* record, CONTEXT
 	return success;
 }
 
-static NTSTATUS (*NTAPI g_origRtlReportException)(PEXCEPTION_RECORD ExceptionRecord, PCONTEXT ContextRecord, ULONG Flags);
+static NTSTATUS (NTAPI* g_origRtlReportException)(PEXCEPTION_RECORD ExceptionRecord, PCONTEXT ContextRecord, ULONG Flags);
 
 static NTSTATUS NTAPI RtlReportExceptionStub(PEXCEPTION_RECORD ExceptionRecord, PCONTEXT ContextRecord, ULONG Flags)
 {
@@ -421,7 +422,9 @@ static NTSTATUS NTAPI RtlReportExceptionStub(PEXCEPTION_RECORD ExceptionRecord, 
 	{
 		inExceptionFallback = true;
 
+#ifndef IS_FXSERVER
 		AddCrashometry("exception_override_2", "true");
+#endif
 
 		EXCEPTION_POINTERS ptrs = { 0 };
 		ptrs.ContextRecord = ContextRecord;
@@ -492,4 +495,3 @@ struct InitMHWrapper
 };
 
 InitMHWrapper mh;
-#endif

code/server/launcher/src/Server.cpp
@@ -12,6 +12,10 @@
 
 #include <cfx_version.h>
 
+#ifdef _WIN32
+void InitializeMiniDumpOverride();
+#endif
+
 class ServerMain
 {
 public:
@@ -29,6 +33,11 @@ namespace fx
 	void Server::Start(int argc, char* argv[])
 	{
 		ComponentLoader* loader = ComponentLoader::GetInstance();
+
+#ifdef _WIN32
+		InitializeMiniDumpOverride();
+#endif
+
 		loader->Initialize();
 
 		// combine argv to a separate command list

code/server/launcher/src/ServerMiniDump.cpp
@@ -12,6 +12,30 @@
 
 static google_breakpad::ExceptionHandler* g_exceptionHandler;
 
+using namespace google_breakpad;
+
+namespace google_breakpad
+{
+class AutoExceptionHandler
+{
+public:
+	static LONG HandleException(EXCEPTION_POINTERS* exinfo)
+	{
+		return ExceptionHandler::HandleException(exinfo);
+	}
+};
+}
+
+void InitializeMiniDumpOverride()
+{
+	auto CoreSetExceptionOverride = (void (*)(LONG(*)(EXCEPTION_POINTERS*)))GetProcAddress(GetModuleHandle(L"CoreRT.dll"), "CoreSetExceptionOverride");
+
+	if (CoreSetExceptionOverride)
+	{
+		CoreSetExceptionOverride(AutoExceptionHandler::HandleException);
+	}
+}
+
 static DWORD BeforeTerminateHandler(LPVOID arg)
 {
 	__try

commit 033324f62cd77aaa7d4686f65dc9f27432103cd8

fix(citicore): don't handle RtlReportException with flag 8
Delayed imports failing can lead to LdrpInitializeNode triggering this,
but it's not meant to be fatal (unless a debugger is attached).

This fixes #1781 which was a cade of N-API import usage crashing instead
of (somehow) working, introduced in eab7a55b8f98149ac76af234107e2952c13d4cbb.

code/client/citicore/SEHTableHandler.Win32.cpp
@@ -418,7 +418,7 @@ static NTSTATUS NTAPI RtlReportExceptionStub(PEXCEPTION_RECORD ExceptionRecord, 
 {
 	static bool inExceptionFallback;
 
-	if (!inExceptionFallback)
+	if (!inExceptionFallback && !(Flags & 8))
 	{
 		inExceptionFallback = true;
 