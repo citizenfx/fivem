citizenfx/fivem

commit 524ec8ca56a2bfbd8c88fe3e1a3460e6a881351e

tweaK(nui/core): less contention on D3D11 mutex
This mutex likely existed just to prevent Flush and device creation at
the same time to prevent an NVIDIA bug/deadlock, so instead we use a
shared_mutex to prevent only that so that we can still try to somewhat
create multiple devices at once.

code/components/nui-core/src/NUIInitialize.cpp
@@ -924,7 +924,8 @@ void VHook(intptr_t& ref, TFnLeft fn, TFnRight out)
 	ref = (intptr_t)fn;
 }
 
-static std::recursive_mutex g_d3d11Mutex;
+// primarily to prevent Flush() calls while another thread is creating a device
+static std::shared_mutex g_d3d11Mutex;
 static void (__stdcall *g_origFlush)(void*);
 
 static void __stdcall FlushHook(void* cxt)
@@ -1064,7 +1065,7 @@ static HMODULE g_sysD3D11;
 
 static HRESULT D3D11CreateDeviceAndSwapChainHook(_In_opt_ IDXGIAdapter* pAdapter, D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, _In_reads_opt_(FeatureLevels) CONST D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, _In_opt_ CONST DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, _COM_Outptr_opt_ IDXGISwapChain** ppSwapChain, _COM_Outptr_opt_ ID3D11Device** ppDevice, _Out_opt_ D3D_FEATURE_LEVEL* pFeatureLevel, _COM_Outptr_opt_ ID3D11DeviceContext** ppImmediateContext)
 {
-	std::unique_lock _(g_d3d11Mutex);
+	std::shared_lock _(g_d3d11Mutex);
 
 	PatchAdapter(&pAdapter);
 
@@ -1083,7 +1084,7 @@ static HRESULT D3D11CreateDeviceAndSwapChainHook(_In_opt_ IDXGIAdapter* pAdapter
 
 static HRESULT D3D11CreateDeviceHook(_In_opt_ IDXGIAdapter* pAdapter, D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, _In_reads_opt_(FeatureLevels) CONST D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, _COM_Outptr_opt_ ID3D11Device** ppDevice, _Out_opt_ D3D_FEATURE_LEVEL* pFeatureLevel, _COM_Outptr_opt_ ID3D11DeviceContext** ppImmediateContext)
 {
-	std::unique_lock _(g_d3d11Mutex);
+	std::shared_lock _(g_d3d11Mutex);
 
 	// if this is the OS calling us, we need to be special and *somehow* convince any hook to give up their true colors
 	// since D3D11CoreCreateDevice is super obscure, we'll use *that*

commit 282744aa42b197e07eb7428fd9b2312379c99936

fix(nui/core): D3D init deadlock with in-process GPU
Introduced in 524ec8ca56a2bfbd8c88fe3e1a3460e6a881351e, this would hit
a deadlock if this code ran with the Flush mutex held (such as when the
NUI in-process GPU option is enabled) as shared_mutex does not directly
have recursive ownership support.

Thanks to PichotM for the report + memory dump!

code/components/nui-core/src/NUIInitialize.cpp
@@ -1063,9 +1063,38 @@ static std::unique_ptr<ModuleData> g_libgl;
 static std::unique_ptr<ModuleData> g_d3d11;
 static HMODULE g_sysD3D11;
 
+// helper to prevent recursive shared acquiring of the lock
+static thread_local bool inDeviceCreation;
+
+struct DeviceLock
+{
+	DeviceLock()
+	{
+		if (!inDeviceCreation)
+		{
+			inDeviceCreation = true;
+
+			hadLock = true;
+			lock = std::move(std::shared_lock(g_d3d11Mutex));
+		}
+	}
+
+	~DeviceLock()
+	{
+		if (hadLock)
+		{
+			inDeviceCreation = false;
+		}
+	}
+
+private:
+	bool hadLock = false;
+	std::shared_lock<std::shared_mutex> lock;
+};
+
 static HRESULT D3D11CreateDeviceAndSwapChainHook(_In_opt_ IDXGIAdapter* pAdapter, D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, _In_reads_opt_(FeatureLevels) CONST D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, _In_opt_ CONST DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, _COM_Outptr_opt_ IDXGISwapChain** ppSwapChain, _COM_Outptr_opt_ ID3D11Device** ppDevice, _Out_opt_ D3D_FEATURE_LEVEL* pFeatureLevel, _COM_Outptr_opt_ ID3D11DeviceContext** ppImmediateContext)
 {
-	std::shared_lock _(g_d3d11Mutex);
+	DeviceLock _;
 
 	PatchAdapter(&pAdapter);
 
@@ -1084,7 +1113,7 @@ static HRESULT D3D11CreateDeviceAndSwapChainHook(_In_opt_ IDXGIAdapter* pAdapter
 
 static HRESULT D3D11CreateDeviceHook(_In_opt_ IDXGIAdapter* pAdapter, D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, _In_reads_opt_(FeatureLevels) CONST D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, _COM_Outptr_opt_ ID3D11Device** ppDevice, _Out_opt_ D3D_FEATURE_LEVEL* pFeatureLevel, _COM_Outptr_opt_ ID3D11DeviceContext** ppImmediateContext)
 {
-	std::shared_lock _(g_d3d11Mutex);
+	DeviceLock _;
 
 	// if this is the OS calling us, we need to be special and *somehow* convince any hook to give up their true colors
 	// since D3D11CoreCreateDevice is super obscure, we'll use *that*