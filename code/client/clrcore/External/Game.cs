aa93c9c96a6643c3e72ab52378efc1345bd288b4
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-01-18 21:35:04 +0100

    tweak(clrcore/game): updated radioNames

--- code/client/clrcore/External/Game.cs
+++ code/client/clrcore/External/Game.cs
@@ -154,27 +154,31 @@ namespace CitizenFX.Core
 	{
 		#region Fields
 		internal static readonly string[] _radioNames = {
-			"RADIO_01_CLASS_ROCK",
-			"RADIO_02_POP",
-			"RADIO_03_HIPHOP_NEW",
-			"RADIO_04_PUNK",
-			"RADIO_05_TALK_01",
-			"RADIO_06_COUNTRY",
-			"RADIO_07_DANCE_01",
-			"RADIO_08_MEXICAN",
-			"RADIO_09_HIPHOP_OLD",
+			"RADIO_36_AUDIOPLAYER",
 			"RADIO_11_TALK_02",
+			"RADIO_37_MOTOMAMI",
+			"RADIO_35_DLC_HEI4_MLR",
 			"RADIO_12_REGGAE",
 			"RADIO_13_JAZZ",
 			"RADIO_14_DANCE_02",
 			"RADIO_15_MOTOWN",
+			"RADIO_20_THELAB",
 			"RADIO_16_SILVERLAKE",
+			"RADIO_32_DLC_HEI4_KULT",
 			"RADIO_17_FUNK",
 			"RADIO_18_90S_ROCK",
-			"RADIO_19_USER",
-			"RADIO_20_THELAB",
 			"RADIO_21_DLC_XM17",
 			"RADIO_22_DLC_BATTLE_MIX1_RADIO",
+			"RADIO_23_DLC_XM19_RADIO",
+			"RADIO_01_CLASS_ROCK",
+			"RADIO_02_POP",
+			"RADIO_03_HIPHOP_NEW",
+			"RADIO_04_PUNK",
+			"RADIO_06_COUNTRY",
+			"RADIO_07_DANCE_01",
+			"RADIO_08_MEXICAN",
+			"RADIO_09_HIPHOP_OLD",
+			"RADIO_19_USER",
 			"RADIO_OFF"
 		};
 
@@ -183,7 +187,7 @@ namespace CitizenFX.Core
 
 		static Game()
 		{
-			Version = GameVersion.v1_0_2699_0_NoSteam;
+			Version = GameVersion.v1_0_2802_0;
 		}
 
 		/// <summary>
