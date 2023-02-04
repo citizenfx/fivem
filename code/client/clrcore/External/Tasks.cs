6b2a89f9d8a24298259e9b9285c93279c4406671
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-02-04 21:05:49 +0100

    feat(clrcore/tasks): missing firing patterns

--- code/client/clrcore/External/Tasks.cs
+++ code/client/clrcore/External/Tasks.cs
@@ -31,7 +31,12 @@ namespace CitizenFX.Core
 		BurstFireHeli = 2437838959u,
 		BurstFireMicro = 1122960381u,
 		BurstFireBursts = 1122960381u,
-		BurstFireTank = 3804904049u
+		BurstFireTank = 3804904049u,
+		TampaMortar = 2452873343u,
+		HunterBarrage = 2905356422u,
+		AkulaBarrage = 1392378214u,
+		ChernoBarrage = 703122589u,
+		Pounder2Barrage = 2228901467u,
 	}
 	[Flags]
 	public enum AnimationFlags
