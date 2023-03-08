965cbc7ddbdc73851fa922c79d2e4e5465dc9d18
blattersturm <peachypies@protonmail.ch>
2023-03-08 15:05:17 +0100

    fix(client): nvidia shader cache workaround race condition
    Even the DRS APIs have a race condition that at times leads to profiles
    being saved that for whatever reason don't contain the shader disk cache
    override.
    
    This wasn't reproducible, but reworking this function (also adding logs
    and renaming the profile to be more visible in NVIDIA profile viewers)
    to add a mutex may help here.

--- code/client/citigame/NvCacheWorkaround.cpp
+++ code/client/citigame/NvCacheWorkaround.cpp
@@ -3,6 +3,61 @@
 #include <nvapi.h>
 #include <NvApiDriverSettings.h>
 
+#include <strsafe.h>
+
+static auto StatusToString(NvAPI_Status status) -> std::string
+{
+	NvAPI_ShortString s;
+	if (NvAPI_GetErrorMessage(status, s) == NVAPI_OK)
+	{
+		return s;
+	}
+
+	return "";
+}
+
+struct SessionDtor
+{
+	explicit SessionDtor(NvDRSSessionHandle session)
+		: session(session)
+	{
+
+	}
+
+	~SessionDtor()
+	{
+		if (session)
+		{
+			NvAPI_DRS_DestroySession(session);
+			session = nullptr;
+		}
+	}
+
+private:
+	NvDRSSessionHandle session;
+};
+
+struct MutexDtor
+{
+	explicit MutexDtor(HANDLE handle)
+		: handle(handle)
+	{
+	}
+
+	~MutexDtor()
+	{
+		if (handle)
+		{
+			ReleaseMutex(handle);
+			CloseHandle(handle);
+			handle = NULL;
+		}
+	}
+
+private:
+	HANDLE handle;
+};
+
 void DisableNvCache()
 {
 	if (NvAPI_Initialize() != NVAPI_OK)
@@ -10,24 +65,36 @@ void DisableNvCache()
 		return;
 	}
 
+	HANDLE hMutex = CreateMutexW(NULL, FALSE, L"CfxNvCacheMutex");
+	WaitForSingleObject(hMutex, INFINITE);
+
+	MutexDtor mutexDtor(hMutex);
+
 	NvAPI_Status status = NVAPI_OK;
 	NvDRSSessionHandle session = { 0 };
 
 	status = NvAPI_DRS_CreateSession(&session);
 	if (status != NVAPI_OK)
 	{
+		trace("%s: NvAPI_DRS_CreateSession returned %s\n", __func__, StatusToString(status));
 		return;
 	}
+
+	SessionDtor dtor(session);
 
 	status = NvAPI_DRS_LoadSettings(session);
 	if (status != NVAPI_OK)
 	{
+		trace("%s: NvAPI_DRS_LoadSettings returned %s\n", __func__, StatusToString(status));
 		return;
 	}
 
 	// try finding a profile, if existent
+	wchar_t appName1[1024] = { 0 };
+	GetModuleFileNameW(GetModuleHandleW(NULL), appName1, std::size(appName1));
+
 	wchar_t appName[1024] = { 0 };
-	GetModuleFileNameW(GetModuleHandleW(NULL), appName, std::size(appName));
+	GetFullPathNameW(appName1, std::size(appName), appName, NULL);
 
 	for (wchar_t& ch : appName)
 	{
@@ -36,8 +103,6 @@ void DisableNvCache()
 			ch = '/';
 		}
 	}
-
-	uint32_t appHash = HashString(ToNarrow(appName).c_str());
 
 	NVDRS_APPLICATION application = {};
 	application.version = NVDRS_APPLICATION_VER_V4;
@@ -48,36 +113,48 @@ void DisableNvCache()
 	NvDRSProfileHandle profile = { 0 };
 	status = NvAPI_DRS_FindApplicationByName(session, name, &profile, &application);
 
+	if (status == NVAPI_OK)
+	{
+		// delete any old-named profile
+		if (wcsstr((wchar_t*)application.userFriendlyName, L"Cfx.re EXE: 0x"))
+		{
+			NvAPI_DRS_DeleteProfile(session, profile);
+			status = NVAPI_ERROR;
+		}
+	}
+
 	if (status != NVAPI_OK)
 	{
 		// make a new profile, okay
 		NVDRS_PROFILE profileInfo = {0};
 		profileInfo.version = NVDRS_PROFILE_VER;
 		profileInfo.isPredefined = 0;
-		wcscpy((wchar_t*)profileInfo.profileName, va(L"Cfx.re: 0x%08x", appHash));
+		StringCchCopyW((wchar_t*)profileInfo.profileName, NVAPI_UNICODE_STRING_MAX, va(L"Cfx.re: %s", appName));
 
 		status = NvAPI_DRS_CreateProfile(session, &profileInfo, &profile);
 
 		if (status != NVAPI_OK)
 		{
+			trace("%s: NvAPI_DRS_CreateProfile returned %s\n", __func__, StatusToString(status));
 			return;
 		}
 
 		application.version = NVDRS_APPLICATION_VER_V4;
 		application.isPredefined = 0;
 		memcpy(application.appName, name, sizeof(application.appName));
-		wcscpy((wchar_t*)application.userFriendlyName, va(L"Cfx.re EXE: 0x%08x", appHash));
+		StringCchCopyW((wchar_t*)application.userFriendlyName, NVAPI_UNICODE_STRING_MAX, va(L"Cfx.re EXE: %s", appName));
 
 		status = NvAPI_DRS_CreateApplication(session, profile, &application);
 
 		if (status != NVAPI_OK)
 		{
+			trace("%s: NvAPI_DRS_CreateApplication returned %s\n", __func__, StatusToString(status));
 			return;
 		}
 	}
 
 	auto settings = {
-		// important: disable shader disk cache
+		// important (nvidia drivers have a race condition that leads to these files corrupting): disable shader disk cache
 		std::make_tuple(PS_SHADERDISKCACHE_ID, (DWORD)PS_SHADERDISKCACHE_OFF), // 'Enables/Disables strategy' -> Off
 
 		// mobile GPUs that use profile overrides need this manually replaced
@@ -101,9 +178,15 @@ void DisableNvCache()
 		status = NvAPI_DRS_SetSetting(session, profile, &setting);
 		if (status != NVAPI_OK)
 		{
+			trace("%s: NvAPI_DRS_SetSetting returned %s\n", __func__, StatusToString(status));
 			continue;
 		}
 	}
 
-	NvAPI_DRS_SaveSettings(session);
+	status = NvAPI_DRS_SaveSettings(session);
+
+	if (status != NVAPI_OK)
+	{
+		trace("%s: NvAPI_DRS_SaveSettings returned %s\n", __func__, StatusToString(status));
+	}
 }
