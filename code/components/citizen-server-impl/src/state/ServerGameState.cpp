citizenfx/fivem

commit a760c73c539fe3cd906e4bedfcfddb492519f4cc

fix(gamestate/server): migrate/cull trains as a singular 'entity'
This fixes the reproduction case from https://forum.cfx.re/t/3430416.

A future change could be allowing trains to be made 'global' as with the
use case from https://forum.cfx.re/t/5036639/17 to match R* behavior -
this would however require limiting the amount of global trains existent
at once to an amount that would make sense for the 'random trains' logic
and all.

Also, this aligns CTrainGameStateDataNode to match names from X360 TU0 a
little more (+ uses the generic serializer).

code/components/citizen-server-impl/src/state/ServerGameState.cpp
@@ -809,6 +809,33 @@ void sync::SyncCommandList::Execute(const fx::ClientSharedPtr& client)
 	scs.Reset();
 }
 
+#ifdef STATE_FIVE
+static auto GetTrain(fx::ServerGameState* sgs, uint32_t objectId) -> fx::sync::SyncEntityPtr
+{
+	if (objectId != 0)
+	{
+		auto entity = sgs->GetEntity(0, objectId);
+
+		if (entity->type == sync::NetObjEntityType::Train && entity->syncTree)
+		{
+			return entity;
+		}
+	}
+
+	return {};
+};
+
+static auto GetNextTrain(fx::ServerGameState* sgs, const fx::sync::SyncEntityPtr& entity) -> fx::sync::SyncEntityPtr
+{
+	if (auto trainState = entity->syncTree->GetTrainState())
+	{
+		return GetTrain(sgs, trainState->linkedToBackwardId);
+	}
+
+	return {};
+};
+#endif
+
 void ServerGameState::Tick(fx::ServerInstanceBase* instance)
 {
 	// #TOOD1SBIG: limit tick rate divisor somewhat more sanely (currently it's 'only' 12.5ms as tick rate was upped from 30fps to 50fps)
@@ -1011,7 +1038,7 @@ void ServerGameState::Tick(fx::ServerInstanceBase* instance)
 				isRelevant = true;
 			}
 
-			if (!isRelevant)
+			auto isRelevantViaPos = [&, this](const fx::sync::SyncEntityPtr& entity, const glm::vec3& entityPos)
 			{
 				if (playerEntity)
 				{
@@ -1023,36 +1050,34 @@ void ServerGameState::Tick(fx::ServerInstanceBase* instance)
 						float distSquared = (diffX * diffX) + (diffY * diffY);
 						if (distSquared < entity->GetDistanceCullingRadius(clientDataUnlocked->GetPlayerCullingRadius()))
 						{
-							isRelevant = true;
-							break;
+							return true;
 						}
 					}
-					
-					if (!isRelevant)
+
+					// are we owning the world grid in which this entity exists?
+					int sectorX = std::max(entityPos.x + 8192.0f, 0.0f) / 150;
+					int sectorY = std::max(entityPos.y + 8192.0f, 0.0f) / 150;
+
+					auto selfBucket = clientDataUnlocked->routingBucket;
+
+					std::shared_lock _(m_worldGridsMutex);
+					const auto& grid = m_worldGrids[selfBucket];
+
+					if (grid && sectorX >= 0 && sectorY >= 0 && sectorX < 256 && sectorY < 256)
 					{
-						// are we owning the world grid in which this entity exists?
-						int sectorX = std::max(entityPos.x + 8192.0f, 0.0f) / 150;
-						int sectorY = std::max(entityPos.y + 8192.0f, 0.0f) / 150;
-
-						auto selfBucket = clientDataUnlocked->routingBucket;
-
-						std::shared_lock _(m_worldGridsMutex);
-						const auto& grid = m_worldGrids[selfBucket];
-
-						if (grid && sectorX >= 0 && sectorY >= 0 && sectorX < 256 && sectorY < 256)
+						if (grid->accel.netIDs[sectorX][sectorY] == netId)
 						{
-							if (grid->accel.netIDs[sectorX][sectorY] == netId)
-							{
-								isRelevant = true;
-							}
+							return true;
 						}
 					}
 				}
-				else
-				{
-					// can't really say otherwise if the player entity doesn't exist
-					isRelevant = false;
-				}
+
+				return false;
+			};
+
+			if (!isRelevant)
+			{
+				isRelevant = isRelevantViaPos(entity, entityPos);
 			}
 
 			// #TODO1S: improve logic for what should and shouldn't exist based on game code
@@ -1099,6 +1124,53 @@ void ServerGameState::Tick(fx::ServerInstanceBase* instance)
 					}
 				}
 			}
+
+#ifdef STATE_FIVE
+			// train chain linking: become relevant if any part of the chain is relevant
+			if (!isRelevant && entity->type == sync::NetObjEntityType::Train)
+			{
+				auto trainState = entity->syncTree->GetTrainState();
+
+				if (auto engine = GetTrain(this, trainState->engineCarriage))
+				{
+					{
+						float position[3];
+						engine->syncTree->GetPosition(position);
+
+						glm::vec3 entityPosition(position[0], position[1], position[2]);
+						if (isRelevantViaPos(engine, entityPosition))
+						{
+							isRelevant = true;
+						}
+					}
+
+					// if not via the engine, try the next-train chain
+					if (!isRelevant)
+					{
+						for (auto link = GetNextTrain(this, engine); link; link = GetNextTrain(this, link))
+						{
+							float position[3];
+							link->syncTree->GetPosition(position);
+
+							glm::vec3 entityPosition(position[0], position[1], position[2]);
+
+							if (isRelevantViaPos(link, entityPosition))
+							{
+								isRelevant = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+#endif
+
+			// -------------------------------------------
+			// -- DON'T CHANGE isRelevant TO TRUE BELOW --
+			// -------------------------------------------
+			//
+			// this is a final pass, NO game logic
+			// (only ownership overrides beyond this point)
 
 			// don't route entities that haven't passed filter to others
 			if (!entity->passedFilter && !ownsEntity)
@@ -2519,12 +2591,41 @@ void ServerGameState::ReassignEntity(uint32_t entityHandle, const fx::ClientShar
 			entIt->second.forceUpdate = true;
 		}
 	});
+
 	// when deleted, we want to make this object ID return to the global pool, not to the player who last owned it
 	// therefore, mark it as stolen
 	{
 		std::unique_lock lock(m_objectIdsMutex);
 		m_objectIdsStolen.set(entityHandle);
 	}
+
+	// if this is a train, we want to migrate the entire train chain
+	// this matches the logic in CNetObjTrain::_?TestProximityMigration
+#ifdef STATE_FIVE
+	if (entity->type == sync::NetObjEntityType::Train && entity->syncTree && oldClientRef != targetClient)
+	{
+		// game code works as follows:
+		// -> if train isEngine, enumerate the entire list backwards and migrate that one along
+		// -> if not isEngine, migrate the engine
+		//
+		// since we have the old->target check above and this serves as a recursion breaker we can
+		// do the exact same
+		if (auto trainState = entity->syncTree->GetTrainState())
+		{
+			if (trainState->isEngine)
+			{
+				for (auto link = GetNextTrain(this, entity); link; link = GetNextTrain(this, link))
+				{
+					ReassignEntity(link->handle, targetClient);
+				}
+			}
+			else if (trainState->engineCarriage)
+			{
+				ReassignEntity(trainState->engineCarriage, targetClient);
+			}
+		}
+	}
+#endif
 }
 
 bool ServerGameState::MoveEntityToCandidate(const fx::sync::SyncEntityPtr& entity, const fx::ClientSharedPtr& client)

code/components/citizen-server-impl/include/state/ServerGameState.h
@@ -506,8 +506,37 @@ struct CDynamicEntityGameStateNodeData
 struct CTrainGameStateDataNodeData
 {
 	int engineCarriage;
+	int linkedToBackwardId;
+	int linkedToForwardId;
 
+	float distanceFromEngine;
+
+	int trainConfigIndex;
 	int carriageIndex;
+
+	int trackId;
+	float cruiseSpeed;
+
+	int trainState;
+
+	bool isEngine;
+	bool isCaboose;
+
+	bool unk12;
+
+	bool direction;
+
+	bool unk14;
+
+	bool renderDerailed;
+
+	// 2372 {
+	bool unk198;
+	bool unk224;
+	bool unk199;
+	// }
+
+	bool forceDoorsOpen;
 };
 
 struct CPlayerGameStateNodeData

code/components/citizen-server-impl/include/state/SyncTrees_Five.h
@@ -2368,57 +2368,51 @@ struct CSubmarineControlDataNode
 	}
 };
 
-struct CTrainGameStateDataNode
+struct CTrainGameStateDataNode : GenericSerializeDataNode<CTrainGameStateDataNode>
 {
 	CTrainGameStateDataNodeData data;
 
-	bool Parse(SyncParseState& state)
+	template<typename TSerializer>
+	bool Serialize(TSerializer& s)
 	{
-		int engineCarriage = state.buffer.Read<int>(13);
-		data.engineCarriage = engineCarriage;
+		// the object ID of the 'engine' carriage
+		s.Serialize(13, data.engineCarriage);
 
-		// What carriage is attached to this carriage
-		int connectedCarriage = state.buffer.Read<int>(13);
+		// the object ID of the carriage attached to this carriage
+		s.Serialize(13, data.linkedToBackwardId);
 
-		// What this carriage is attached to
-		int connectedToCarriage = state.buffer.Read<int>(13);
+		// the object ID of the carriage this carriage is attached to
+		s.Serialize(13, data.linkedToForwardId);
 
 		// Offset from the engine carriage?
-		float engineOffset = state.buffer.ReadSignedFloat(32, 1000.0f);
+		s.SerializeSigned(32, 1000.0f, data.distanceFromEngine);
 
-		int trainConfigIndex = state.buffer.Read<int>(8);
-
-		int carriageIndex = state.buffer.Read<int>(8);
-		data.carriageIndex = carriageIndex;
+		s.Serialize(8, data.trainConfigIndex);
+		s.Serialize(8, data.carriageIndex);
 
 		// 0 = Main Line, 3 = Metro line
-		int trackId = state.buffer.Read<int>(8);
+		s.Serialize(8, data.trackId);
 
-		float cruiseSpeed = state.buffer.ReadSignedFloat(8, 30.0f);
+		s.SerializeSigned(8, 30.0f, data.cruiseSpeed);
 
 		// 0 = Moving, 1 = Slowing down, 2 = Doors opening, 3 = Stopped, 4 = Doors closing, 5 = Before depart
-		int trainState = state.buffer.Read<int>(3);
+		s.Serialize(3, data.trainState);
 
-		bool isStartCarriage = state.buffer.ReadBit();
-
-		bool isEndCarriage = state.buffer.ReadBit();
-
-		bool unk12 = state.buffer.ReadBit();
-
-		bool direction = state.buffer.ReadBit();
-
-		bool unk14 = state.buffer.ReadBit();
-
-		bool renderDerailed = state.buffer.ReadBit();
+		s.Serialize(data.isEngine);
+		s.Serialize(data.isCaboose);
+		s.Serialize(data.unk12);
+		s.Serialize(data.direction);
+		s.Serialize(data.unk14);
+		s.Serialize(data.renderDerailed);
 
 		if (Is2372()) // Sequence of bits need to be verified for 2732
 		{
-			auto unk198 = state.buffer.ReadBit();
-			auto unk224 = state.buffer.ReadBit();
-			auto unk199 = state.buffer.ReadBit();
+			s.Serialize(data.unk198);
+			s.Serialize(data.unk224);
+			s.Serialize(data.unk199);
 		}
 
-		bool forceDoorsOpen = state.buffer.ReadBit();
+		s.Serialize(data.forceDoorsOpen);
 
 		return true;
 	}

commit 5c3023142e01c2a4f7a52bba91de49419f4da6c8

fix(gamestate/server): null ptr check in GetTrain
This is a regression introduced in a760c73c539fe3cd906e4bedfcfddb492519f4cc
where if a train refers to a nonexistent forward/backward/engine link it
would end up with a null pointer dereference.

Routine oversight.

code/components/citizen-server-impl/src/state/ServerGameState.cpp
@@ -816,7 +816,7 @@ static auto GetTrain(fx::ServerGameState* sgs, uint32_t objectId) -> fx::sync::S
 	{
 		auto entity = sgs->GetEntity(0, objectId);
 
-		if (entity->type == sync::NetObjEntityType::Train && entity->syncTree)
+		if (entity && entity->type == sync::NetObjEntityType::Train && entity->syncTree)
 		{
 			return entity;
 		}