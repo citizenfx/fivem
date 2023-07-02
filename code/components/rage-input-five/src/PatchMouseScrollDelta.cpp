6698373e10fdc61674e8a3fc027754d188932597
blattersturm <peachypies@protonmail.ch>
2023-07-02 21:37:49 +0200

    feat(input/five): precision trackpad scroll support
    Modern trackpads do not typically send 'full' scrolls but allow precise
    scrolling with deltas often smaller than 120.
    
    The game would use an integer division to convert these deltas, leading
    to it being impossible to handle smooth scrolling in 'native' game code
    for trackpad users.
    
    With this change, the IOMS_MOUSE_WHEEL -> IOM_AXIS_WHEEL_DELTA mapper
    can return values other than '1.0', '2.0' and other whole numbers, as
    seen in the following example code showing values other than multiples
    of '120' if used on a laptop trackpad.
    
    ```lua
    local INPUT_FRONTEND_RIGHT_AXIS_Y = 198
    local scrollDelta = (GetDisabledControlUnboundNormal(0, INPUT_FRONTEND_RIGHT_AXIS_Y) * 120)
    
    if scrollDelta ~= 0 then
        print(scrollDelta)
    end
    ```

---
+++ code/components/rage-input-five/src/PatchMouseScrollDelta.cpp
@@ -0,0 +1,169 @@
+#include "StdInc.h"
+
+#include <jitasm.h>
+#include "Hooking.h"
+
+void DoPatchMouseScrollDelta()
+{
+	// remove the `/ 120` in window procedure code handling mouse wheel delta
+	auto matches = hook::pattern("F7 E9 03 D1 C1 FA 06 8B C2 C1 E8 1F 03 D0 01").count(2);
+
+	for (int i = 0; i < matches.size(); i++)
+	{
+		auto location = matches.get(i).get<char>(0);
+		hook::nop(location, 14);
+		hook::put<uint16_t>(location, 0xCA89); // mov edx, ecx
+	}
+
+	// same, but in DInput mouse wheel delta
+	{
+		auto location = hook::get_pattern<char>("B8 89 88 88 88 F7 2D");
+		hook::nop(location, 0x1B);
+		hook::nop(location + 0x2E, 6);
+	}
+
+	// shared divisor scale
+	static alignas(16) const float scale[4] = { 1 / 120.f, 1 / 120.f, 1 / 120.f, 1 / 120.f };
+
+	// rage::ioMapper::Update, int->float conversion for rage::ioMouse::m_dZ (add a / 120.0 there)
+	{
+		static struct : jitasm::Frontend
+		{
+			uintptr_t returnAddr = 0;
+
+			virtual void InternalMain() override
+			{
+				mov(rax, (uintptr_t)(void*)scale);
+
+				movaps(xmm0, xmmword_ptr[rax]);
+				cvtdq2ps(xmm6, xmm6);
+				mulps(xmm6, xmm0);
+
+				mov(rax, returnAddr);
+				jmp(rax);
+			}
+		} patch;
+
+		auto location = hook::get_pattern<char>("0F 5B F6 E9 ? ? ? ? 8D 87");
+		patch.returnAddr = hook::get_address<uintptr_t>(location + 3, 1, 5);
+		hook::nop(location, 3 + 5);
+		hook::jump(location, patch.GetCode());
+	}
+
+	// adding / 120.0 to CGalleryMenu
+
+	// 1st
+	{
+		static struct : jitasm::Frontend
+		{
+			uintptr_t returnAddr = 0;
+
+			virtual void InternalMain() override
+			{
+				mov(byte_ptr[rbp + 0x1C8], dl);
+
+				mov(rax, (uintptr_t)(void*)scale);
+
+				movaps(xmm0, xmmword_ptr[rax]);
+				cvtdq2ps(xmm8, xmm8);
+				mulps(xmm8, xmm0);
+
+				mov(rax, returnAddr);
+				jmp(rax);
+			}
+		} patch;
+
+		auto location = hook::get_pattern<char>("88 95 C8 01 00 00 45 0F 5B C0");
+		patch.returnAddr = (uintptr_t)(location + 10);
+		hook::nop(location, 10);
+		hook::jump(location, patch.GetCode());
+	}
+
+	// 2nd
+	{
+		static struct : jitasm::Frontend
+		{
+			uintptr_t returnAddr = 0;
+
+			virtual void InternalMain() override
+			{
+				mov(byte_ptr[rsp + 0x50], cl);
+
+				mov(rax, (uintptr_t)(void*)scale);
+
+				movaps(xmm0, xmmword_ptr[rax]);
+				cvtdq2ps(xmm6, xmm6);
+				mulps(xmm6, xmm0);
+
+				mov(rax, returnAddr);
+				jmp(rax);
+			}
+		} patch;
+
+		auto location = hook::get_pattern<char>("88 4C 24 50 0F 5B F6");
+		patch.returnAddr = (uintptr_t)(location + 7);
+		hook::nop(location, 7);
+		hook::jump(location, patch.GetCode());
+	}
+
+	// adding / 120.0 to CMapMenu
+	{
+		static struct : jitasm::Frontend
+		{
+			uintptr_t returnAddr = 0;
+
+			virtual void InternalMain() override
+			{
+				mov(rbx, rcx);
+
+				mov(rax, (uintptr_t)(void*)scale);
+
+				movaps(xmm3, xmmword_ptr[rax]);
+				cvtdq2ps(xmm0, xmm0);
+				mulps(xmm0, xmm3);
+
+				mov(rax, returnAddr);
+				jmp(rax);
+			}
+		} patch;
+
+		auto location = hook::get_pattern<char>("48 8B D9 0F 5B C0 F3 0F");
+		patch.returnAddr = (uintptr_t)(location + 6);
+		hook::nop(location, 6);
+		hook::jump(location, patch.GetCode());
+	}
+
+	// adding / 120.0 to scaleform input submission
+	{
+		static struct : jitasm::Frontend
+		{
+			uintptr_t returnAddr = 0;
+
+			virtual void InternalMain() override
+			{
+				mov(qword_ptr[rbp - 0x29], r12);
+
+				mov(rcx, (uintptr_t)(void*)scale);
+
+				movaps(xmm3, xmmword_ptr[rcx]);
+				cvtdq2ps(xmm0, xmm0);
+				mulps(xmm0, xmm3);
+
+				mov(rcx, returnAddr);
+
+				// return jump to avoid using rcx after we restore it
+				push(rcx);
+				mov(rcx, rdi);
+
+				ret();
+			}
+		} patch;
+
+		auto location = hook::get_pattern<char>("4C 89 65 D7 0F 5B C0");
+		patch.returnAddr = (uintptr_t)(location + 7);
+		hook::nop(location, 7);
+		hook::jump_rcx(location, patch.GetCode());
+	}
+}
+
+// no HookFunction, this is in InputHook.cpp's setOffsetsHookFunction
