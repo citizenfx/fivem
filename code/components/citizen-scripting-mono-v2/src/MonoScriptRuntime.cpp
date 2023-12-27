bf0ea3f6c1453e7e8d6dfb0520461d8d88bfd026
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2023-12-27 19:09:39 +0100

    Bump mono v2 flag date
    The monkey v2 flag will expire in 4 days, it would be useful to increase it for another 4 months as was done last time.

--- code/components/citizen-scripting-mono-v2/src/MonoScriptRuntime.cpp
+++ code/components/citizen-scripting-mono-v2/src/MonoScriptRuntime.cpp
@@ -268,6 +268,7 @@ int MonoScriptRuntime::HandlesFile(char* filename, IScriptHostWithResourceData* 
 	// Allowed values for mono_rt2
 	constexpr std::string_view allowedValues[] = {
 		// put latest on top, right here ↓
+		"Prerelease expiring 2024-04-30. See https://aka.cfx.re/mono-rt2-preview for info."sv,
 		"Prerelease expiring 2023-12-31. See https://aka.cfx.re/mono-rt2-preview for info."sv,
 		"Prerelease expiring 2023-08-31. See https://aka.cfx.re/mono-rt2-preview for info."sv,
 		"Prerelease expiring 2023-06-30. See https://aka.cfx.re/mono-rt2-preview for info."sv,
