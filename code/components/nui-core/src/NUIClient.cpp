d0ec48a3a355ab3bfbd656690f27a943b7310f3b
manic <manic@mani.cc>
2024-02-29 20:10:53 +0000

    fix(nui/core): set user-agent for imgur URLs

--- code/components/nui-core/src/NUIClient.cpp
+++ code/components/nui-core/src/NUIClient.cpp
@@ -410,6 +410,25 @@ auto NUIClient::OnBeforeResourceLoad(CefRefPtr<CefBrowser> browser, CefRefPtr<Ce
 		}
 	}
 
+	// Imgur started blocking CitizenFX in the user-agent
+	{
+		CefURLParts parts;
+		if (CefParseURL(request->GetURL(), parts))
+		{
+			auto hostString = CefString(&parts.host).ToString();
+
+			if (boost::algorithm::ends_with(hostString, "imgur.com"))
+			{
+				CefRequest::HeaderMap headers;
+				request->GetHeaderMap(headers);
+	
+				headers.erase("User-Agent");
+				headers.emplace("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+				request->SetHeaderMap(headers);
+			}
+		}
+	}
+
 #if !defined(_DEBUG)
 	if (frame->IsMain())
 	{
