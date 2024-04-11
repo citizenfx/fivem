4cbf89e2a8a552d4d38bba84e1d21a18d66544b1
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-04-11 14:57:24 +0200

    feat(tests): add ascii random string gen

--- code/tests/TestUtils.cpp
+++ code/tests/TestUtils.cpp
@@ -11,3 +11,35 @@ uint64_t fx::TestUtils::u64Random(const uint64_t range)
 	std::uniform_int_distribution<uint64_t> dist(0, range);
 	return dist(eng);
 }
+
+std::string fx::TestUtils::asciiRandom(const uint64_t count)
+{
+	static std::seed_seq ssq{Catch::getSeed()};
+	static std::default_random_engine eng{ssq};
+	std::string val;
+	val.resize(count);
+	std::uniform_int_distribution<uint64_t> dist(0, 61);
+	for (uint64_t i = 0; i < count; ++i)
+	{
+		const uint64_t generatedValue = dist(eng);
+		// 'a' - 'z'
+		if (generatedValue < 26)
+		{
+			val[i] = static_cast<char>('a' + generatedValue);
+			continue;
+		}
+		// 'A' - 'Z'
+		if (generatedValue < 52)
+		{
+			val[i] = static_cast<char>('A' + generatedValue - 26);
+			continue;
+		}
+		// '0' - '9'
+		if (generatedValue < 62)
+		{
+			val[i] = static_cast<char>('0' + generatedValue - 52);
+		}
+	}
+
+	return val;
+}
