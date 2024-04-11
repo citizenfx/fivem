820ace6da3fd7b65f5572a0e584883d8a9a626ae
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-04-11 14:59:47 +0200

    tweak(server): make WithOutOfBand testable

--- code/components/citizen-server-impl/include/decorators/WithOutOfBand.h
+++ code/components/citizen-server-impl/include/decorators/WithOutOfBand.h
@@ -9,8 +9,14 @@ namespace fx
 		template<typename... TOOB>
 		const fwRefContainer<fx::GameServer>& WithOutOfBand(const fwRefContainer<fx::GameServer>& server)
 		{
+			return WithOutOfBand<TOOB...>(server);
+		}
+		
+		template<typename ServerImpl, typename... TOOB>
+		const fwRefContainer<ServerImpl>& WithOutOfBandImpl(const fwRefContainer<ServerImpl>& server)
+		{
 			// this is static because GCC seems to get really confused when things are wrapped in a lambda
-			static std::map<std::string, std::function<void(const fwRefContainer<fx::GameServer>& server, const net::PeerAddress& from, const std::string_view& data)>, std::less<>> processors;
+			static std::map<std::string, std::function<void(const fwRefContainer<ServerImpl>& server, const net::PeerAddress& from, const std::string_view& data)>, std::less<>> processors;
 
 			([&]()
 			{
@@ -34,8 +40,8 @@ namespace fx
 				}
 
 				// workaround a VS15.7 compiler bug that drops `const` qualifier in the std::function
-				fwRefContainer<fx::GameServer> tempServer = server;
-
+				fwRefContainer<ServerImpl> tempServer = server;
+				
 				if (receivedDataLength >= 4 && *reinterpret_cast<const int*>(receivedData) == -1)
 				{
 					auto begin = receivedData + 4;
