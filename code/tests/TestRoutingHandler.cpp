39acea761e0110ad9ef0202158185e10f854a2b6
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-04-29 16:41:00 +0200

    tweak(tests): add ClientRegistry and ServerInstance
    Required for specific game server related tests.

--- code/tests/TestRoutingHandler.cpp
+++ code/tests/TestRoutingHandler.cpp
@@ -3,97 +3,10 @@
 #include <catch_amalgamated.hpp>
 
 #include "GameServer.h"
+#include "ServerInstance.h"
 #include "packethandlers/RoutingPacketHandler.h"
 
 #include "TestUtils.h"
-
-class ServerInstance : public fx::ServerInstanceBase
-{
-	const std::string& GetRootPath() override
-	{
-		return "test";
-	}
-};
-
-struct FarLockImpl : fx::FarLock::ImplBase
-{
-	// actual implementation uses folly's shared mutex
-	std::shared_mutex mutex;
-	
-	bool TryLock() override
-	{
-		return mutex.try_lock();
-	}
-
-	void LockShared() override
-	{
-		return mutex.lock_shared();
-	}
-
-	void Lock() override
-	{
-		return mutex.lock();
-	}
-
-	void Unlock() override
-	{
-		return mutex.unlock();
-	}
-
-	void UnlockShared() override
-	{
-		return mutex.unlock_shared();
-	}
-};
-
-fx::FarLock::FarLock()
-	: impl(std::make_unique<FarLockImpl>())
-{
-
-}
-
-fx::FarLock::~FarLock()
-{
-	
-}
-
-fx::ClientRegistry::ClientRegistry()
-	: m_hostNetId(-1), m_curNetId(1), m_instance(nullptr)
-{
-	m_clientsBySlotId.resize(MAX_CLIENTS);
-}
-
-fx::ClientSharedPtr fx::ClientRegistry::MakeClient(const std::string& guid)
-{
-	fx::ClientSharedPtr client = fx::ClientSharedPtr::Construct(guid);
-	fx::ClientWeakPtr weakClient(client);
-
-	m_clientsBySlotId[1] = weakClient;
-
-	return client;
-}
-
-void fx::ClientRegistry::HandleConnectingClient(const fx::ClientSharedPtr& client)
-{
-}
-
-void fx::ClientRegistry::HandleConnectedClient(const fx::ClientSharedPtr& client, uint32_t oldNetID)
-{
-}
-
-fx::ClientSharedPtr fx::ClientRegistry::GetHost()
-{
-	return fx::ClientSharedPtr{};
-}
-
-void fx::ClientRegistry::SetHost(const fx::ClientSharedPtr& client)
-{
-}
-
-void fx::ClientRegistry::AttachToObject(ServerInstanceBase* instance)
-{
-	m_instance = instance;
-}
 
 struct ParseGameStatePacketData
 {
@@ -146,7 +59,7 @@ TEST_CASE("Routing handler test")
 	// test is only implemented for onesync
 	REQUIRE(fx::IsOneSync() == true);
 
-	ServerInstance* serverInstance = new ServerInstance();
+	fx::ServerInstanceBase* serverInstance = ServerInstance::Create();
 	serverInstance->SetComponent(new fx::ClientRegistry());
 	serverInstance->SetComponent<fx::ServerGameStatePublic>(new GameState());
 
