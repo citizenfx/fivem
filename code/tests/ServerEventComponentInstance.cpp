61015126dbafa06a79b14a12a09f0c41f7cb8a68
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-05-04 15:47:52 +0200

    feat(tests): add server command tests

---
+++ code/tests/ServerEventComponentInstance.cpp
@@ -0,0 +1,24 @@
+﻿#include <StdInc.h>
+
+#include "ServerEventComponent.h"
+
+#include "ServerEventComponentInstance.h"
+
+void fx::ServerEventComponent::TriggerClientEvent(const std::string_view& eventName, const void* data, size_t dataLen,
+                                                  const std::optional<std::string_view>& targetSrc)
+{
+	std::optional<std::string> targetSourceStr;
+	if (auto target = targetSrc)
+	{
+		targetSourceStr.emplace(target.value());
+	}
+	ServerEventComponentInstance::lastClientEvent.emplace(std::string(eventName),
+	                                                      std::vector(static_cast<const uint8_t*>(data),
+	                                                                  static_cast<const uint8_t*>(data) + dataLen),
+	                                                      targetSourceStr);
+}
+
+fx::ServerEventComponent* fx::ServerEventComponentInstance::Create()
+{
+	return new ServerEventComponent();
+}
---
+++ code/tests/ServerEventComponentInstance.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+namespace fx
+{
+	class ServerEventComponent;
+
+	class ServerEventComponentInstance
+	{
+	public:
+		struct ClientEventData
+		{
+			std::string eventName;
+			std::vector<uint8_t> data;
+			std::optional<std::string> targetSrc;
+
+			ClientEventData(std::string event_name, const std::vector<uint8_t>& data,
+			                const std::optional<std::string>& target_src)
+				: eventName(std::move(event_name)),
+				  data(data),
+				  targetSrc(target_src)
+			{
+			}
+		};
+		
+		static inline std::optional<ClientEventData> lastClientEvent;
+		
+		static ServerEventComponent* Create();
+	};
+}
