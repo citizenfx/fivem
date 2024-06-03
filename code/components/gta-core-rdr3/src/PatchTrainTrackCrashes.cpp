ed73eebcb8a37ded9084cd31bc5b2b1e938576c0
Dylan Ascencio <empressglatia@gmail.com>
2024-06-03 03:06:59 -0400

    Add more crash fixes for train tracks

--- code/components/gta-core-rdr3/src/PatchTrainTrackCrashes.cpp
+++ code/components/gta-core-rdr3/src/PatchTrainTrackCrashes.cpp
@@ -18,6 +18,8 @@
 
 constexpr uint8_t NODE_FLAG_JUNCTION = 0x08;
 constexpr uint8_t NODE_FLAG_0x10 = 0x10;
+constexpr uint8_t JUNCTION_MAX = 20;
+constexpr uint8_t TRAIN_TRACK_MAX = 50;
 
 struct sTrainTrackJunction
 {
@@ -32,7 +34,7 @@ struct sTrainTrackJunction
 
 struct sTrainTrackJunctionPool
 {
-	sTrainTrackJunction Junctions[20];
+	sTrainTrackJunction Junctions[JUNCTION_MAX];
 	uint32_t Count;
 };
 
@@ -69,14 +71,17 @@ struct sTrainTrack
 
 struct sTrainTrackPool
 {
-	sTrainTrack Tracks[50];
+	sTrainTrack Tracks[TRAIN_TRACK_MAX];
 	uint32_t Count;
 };
 
 // Pointer to the train tracks pool.
 static sTrainTrackPool* g_trainTrackPool;
+
 // Pointer to the original function that sets up track junctions.
 static void (*g_origPostProcessJunctions)(sTrainTrackPool*);
+// Internal function for spawning a train as a mission vehicle.
+static int64_t (*g_origCreateMissionTrain)(uint32_t, float*, bool, bool, bool, bool);
 
 // Internal function for getting a track's index from its name hash. Returns -1 if the hash was not in the tracks pool.
 static hook::cdecl_stub<int8_t(uint32_t)> _getTrackIndexFromHash([]()
@@ -124,8 +129,92 @@ static void PostProcessJunctions(sTrainTrackPool* tracks)
 	g_origPostProcessJunctions(tracks);
 }
 
+static int64_t CreateMissionTrain(uint32_t config, float* position, bool direction, bool passengers, bool p4, bool conductor)
+{
+	if (g_trainTrackPool->Count == 0)
+	{
+		trace("CreateMissionTrain() failed: Track pool is empty!\n");
+		return 0;
+	}
+
+	return g_origCreateMissionTrain(config, position, direction, passengers, p4, conductor);
+}
+
+static bool IsTrackPoolFull()
+{
+	if (g_trainTrackPool->Count >= TRAIN_TRACK_MAX)
+	{
+		trace("Track pool is full, no more tracks can be loaded. (Count: %i)\n", g_trainTrackPool->Count);
+		return false;
+	}
+
+	return true;
+}
+
 static HookFunction hookFunction([]()
 {
-	auto location = hook::get_call(hook::get_pattern("E8 ? ? ? ? 48 8B CD E8 ? ? ? ? 4C 8D 9C 24 ? ? ? ? 49 8B 5B ? 49 8B 6B ? 49 8B 73 ? 49 8B 7B ? 49 8B E3 41 5F"));
-	g_origPostProcessJunctions = hook::trampoline(location, PostProcessJunctions);
+	g_trainTrackPool = hook::get_address<sTrainTrackPool*>(hook::get_pattern("48 8D 15 ? ? ? ? 49 8B C9 E8 ? ? ? ? 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8D 0D", 3));
+
+	// Fixes crash with junctions that point to invalid tracks
+	{
+		auto location = hook::get_call(hook::get_pattern("E8 ? ? ? ? 48 8B CD E8 ? ? ? ? 4C 8D 9C 24 ? ? ? ? 49 8B 5B ? 49 8B 6B ? 49 8B 73 ? 49 8B 7B ? 49 8B E3 41 5F"));
+		g_origPostProcessJunctions = hook::trampoline(location, PostProcessJunctions);
+	}
+
+	// Prevents trains from being spawned when no tracks exist
+	{
+		auto location = hook::get_pattern("48 8B C4 48 89 58 ? 48 89 68 ? 48 89 70 ? 48 89 78 ? 41 56 48 81 EC ? ? ? ? 33 DB 41 8A E9");
+		g_origCreateMissionTrain = hook::trampoline(location, CreateMissionTrain);
+	}
+
+	static struct : jitasm::Frontend
+	{
+		intptr_t retSuccess = 0;
+		intptr_t retFail = 0;
+
+		void Init(intptr_t success, intptr_t fail)
+		{
+			this->retSuccess = success;
+			this->retFail = fail;
+		}
+
+		virtual void InternalMain() override
+		{
+			// Original check for a valid XML element.
+			test(rbx, rbx);
+			jz("fail");
+
+			// New check for track count < 50.
+			mov(rax, reinterpret_cast<uintptr_t>(IsTrackPoolFull));
+			call(rax);
+			test(al, al);
+			jz("fail");
+
+			// We have a valid XML child and the track pool isn't full, load the next track!
+			mov(rax, retSuccess);
+			jmp(rax);
+
+			// Can't load another track, exit the loading loop.
+			L("fail");
+			mov(rax, retFail);
+			jmp(rax);
+		}
+	} patchStub;
+
+	// Prevents the game from loading more than 50 tracks
+	{
+		auto location = hook::get_pattern<char>("48 85 DB 0F 85 ? ? ? ? 49 8B CF");
+
+		// Grab the offset of the body loop from jnz.
+		int32_t loopBodyPos = *(int32_t*)(location + 5);
+		// loopBodyPos is relative to the end of jnz, which is test + jnz (3 + 6 bytes).
+		const auto successPtr = reinterpret_cast<intptr_t>(location) + 9 + loopBodyPos;
+
+		// Skip the original test + jnz instructions.
+		const auto failPtr = reinterpret_cast<intptr_t>(location) + 9;
+
+		patchStub.Init(successPtr, failPtr);
+		hook::nop(location, 9);
+		hook::jump(location, patchStub.GetCode());
+	}
 });
