39e1a834064779263f527b4e2ce4b243fcf5b398
Peter Marton <peter.marton@rockstargames.com>
2024-07-10 11:45:39 +0200

    fix(five): game events for 3258

--- code/components/gta-streaming-five/src/GameEventHooks.cpp
+++ code/components/gta-streaming-five/src/GameEventHooks.cpp
@@ -102,7 +102,11 @@ static const char* GetEventName(rage::fwEvent* ev)
 {
 	int eventId = ev->GetId();
 
-	if (xbr::IsGameBuildOrGreater<2612>())
+	if (xbr::IsGameBuildOrGreater<3258>())
+	{
+		return (eventId < _countof(g_eventNames3258)) ? g_eventNames3258[eventId] : nullptr;
+	}
+	else if (xbr::IsGameBuildOrGreater<2612>())
 	{
 		return (eventId < _countof(g_eventNames2612)) ? g_eventNames2612[eventId] : nullptr;
 	}
