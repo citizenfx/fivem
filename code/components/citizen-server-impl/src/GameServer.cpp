962be8882e7011866bed25cdbe42ebdbdce0b253
Daniel GP <dani.dcshop.gg@gmail.com>
2024-07-25 20:49:08 +0200

    feat(server/impl): add onServerStop event

--- code/components/citizen-server-impl/src/GameServer.cpp
+++ code/components/citizen-server-impl/src/GameServer.cpp
@@ -127,8 +127,12 @@ namespace fx
 
 		m_mainThreadCallbacks = std::make_unique<CallbackListNng>("inproc://main_client", 0);
 
-		instance->OnRequestQuit.Connect([this](const std::string& reason)
+		instance->OnRequestQuit.Connect([this, instance](const std::string& reason)
 		{
+			instance
+					->GetComponent<fx::ResourceManager>()
+					->GetComponent<fx::ResourceEventManagerComponent>()
+					->TriggerEvent2("onServerStop", {});
 			m_clientRegistry->ForAllClients([this, &reason](const fx::ClientSharedPtr& client)
 			{
 				DropClient(client, "Server shutting down: %s", reason);
