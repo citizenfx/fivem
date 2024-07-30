faabaa757e60af73accf94eecf471c93b241b7a5
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-07-30 21:03:19 +0200

    feat(server): new time sync packet

--- code/components/citizen-server-impl/include/packethandlers/TimeSyncReqPacketHandler.h
+++ code/components/citizen-server-impl/include/packethandlers/TimeSyncReqPacketHandler.h
@@ -6,6 +6,10 @@
 
 #include <ServerTime.h>
 
+#include "ByteReader.h"
+#include "ByteWriter.h"
+#include "TimeSync.h"
+
 class TimeSyncReqPacketHandler
 {
 public:
@@ -16,16 +20,36 @@ public:
 	template<typename Client = fx::ClientSharedPtr>
 	void Handle(fx::ServerInstanceBase* instance, const Client& client, net::Buffer& packet)
 	{
-		const uint32_t reqTime = packet.Read<uint32_t>();
-		const uint32_t reqSeq = packet.Read<uint32_t>();
+		static size_t kMaxRequestSize = net::SerializableComponent::GetSize<net::packet::TimeSyncRequest>();
+		static size_t kMaxResponseSize = net::SerializableComponent::GetSize<net::packet::TimeSyncResponsePacket>();
+		thread_local net::packet::TimeSyncRequest timeSyncRequest;
+		thread_local net::packet::TimeSyncResponsePacket timeSyncResponse;
 
-		net::Buffer netBuffer;
-		netBuffer.Write<uint32_t>(HashRageString("msgTimeSync"));
-		netBuffer.Write<uint32_t>(reqTime);
-		netBuffer.Write<uint32_t>(reqSeq);
-		netBuffer.Write<uint32_t>((msec().count()) & 0xFFFFFFFF);
+		if (packet.GetRemainingBytes() != kMaxRequestSize)
+		{
+			return;
+		}
 
-		client->SendPacket(1, netBuffer, NetPacketType_Reliable);
+		net::ByteReader reader { packet.GetRemainingBytesPtr(), packet.GetRemainingBytes() };
+		if (!timeSyncRequest.Process(reader))
+		{
+			return;
+		}
+
+		timeSyncResponse.data.request = timeSyncRequest;
+		timeSyncResponse.data.serverTimeMillis = msec().count() & 0xFFFFFFFF;
+
+		net::Buffer responseBuffer(kMaxResponseSize);
+		net::ByteWriter writer{ responseBuffer.GetBuffer(), kMaxResponseSize };
+		if (!timeSyncResponse.Process(writer))
+		{
+			trace("Serialization of the server time sync response failed. Please report this error at https://github.com/citizenfx/fivem.\n");
+			return;
+		}
+
+		responseBuffer.Seek(writer.GetOffset());
+
+		client->SendPacket(1, responseBuffer, NetPacketType_Reliable);
 	}
 
 	static constexpr const char* GetPacketId()
