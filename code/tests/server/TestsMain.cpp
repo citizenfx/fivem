4aacbb4f443cb000a021553b210be96d90e4072e
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-09-10 21:47:52 +0200

    tweak(shared): allow overriding component load
    fix rdr3

---
+++ code/tests/server/TestsMain.cpp
@@ -0,0 +1,21 @@
+﻿#include <StdInc.h>
+
+#include <catch_amalgamated.hpp>
+
+#include <ComponentLoader.h>
+
+int main(const int argc, char* argv[])
+{
+	ComponentLoader* loader = ComponentLoader::GetInstance();
+
+	loader->Initialize();
+
+	Catch::Session session;
+	const int returnCode = session.applyCommandLine(argc, argv);
+	if (returnCode != 0)
+	{
+		return returnCode;
+	}
+	const int numFailed = session.run();
+	return numFailed;
+}
