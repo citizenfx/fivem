554a98bf9d3d1894d4e2b5e8272e2cfb6ebb5e8b
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-09-16 20:21:41 +0200

    tweak(tests): add net event serialization/deserialization benchmarks

--- code/tests/shared/TestNetEvent.cpp
+++ code/tests/shared/TestNetEvent.cpp
@@ -14,6 +14,32 @@ TEST_CASE("net event serialization test")
 	fx::TestUtils::fillVectorU8Random(eventData);
 	std::string eventName = fx::TestUtils::asciiRandom(128);
 
+	net::Buffer outBuffer;
+	BENCHMARK("old net event serialization") {
+		// build the target event
+		outBuffer.Reset();
+		outBuffer.Write(0x7337FD7A);
+
+		// source netId
+		outBuffer.Write<uint16_t>(-1);
+
+		// event name
+		outBuffer.Write<uint16_t>(eventName.size() + 1);
+		outBuffer.Write(eventName.data(), eventName.size());
+		outBuffer.Write<uint8_t>(0);
+
+		// payload
+		outBuffer.Write(eventData.data(), eventData.size());
+	};
+
+	std::vector<uint8_t> writeBuffer(net::SerializableComponent::GetSize<net::packet::ClientServerEvent>());
+	BENCHMARK("new net event serialization") {
+		net::ByteWriter writer{writeBuffer.data(), writeBuffer.size()};
+		net::packet::ClientServerEvent clientServerEvent;
+		clientServerEvent.eventName.SetValue({reinterpret_cast<uint8_t*>(eventName.data()), eventName.size()});
+		clientServerEvent.eventData.SetValue({eventData.data(), eventData.size()});
+		clientServerEvent.Process(writer);
+	};
 
 	std::vector<uint8_t> eventBuffer(net::SerializableComponent::GetSize<net::packet::ClientServerEvent>());
 	net::ByteWriter writer {eventBuffer.data(), eventBuffer.size()};
@@ -21,11 +47,14 @@ TEST_CASE("net event serialization test")
 	clientServerEvent.eventName.SetValue({reinterpret_cast<uint8_t*>(eventName.data()), eventName.size()});
 	clientServerEvent.eventData.SetValue({eventData.data(), eventData.size()});
 	REQUIRE(clientServerEvent.Process(writer) == true);
-	
+
 	BENCHMARK("new net event deserialization") {
 		net::ByteReader reader {eventBuffer.data(), eventBuffer.size()};
 		net::packet::ClientServerEvent clientServerEventRead;
-		clientServerEventRead.Process(writer);
+		clientServerEventRead.Process(reader);
+		if (clientServerEventRead.eventName.GetValue().size() == 0) {
+			REQUIRE(false);
+		}
 	};
 
 	BENCHMARK("old net event deserialization") {
