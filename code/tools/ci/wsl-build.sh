72e7bfda403a6e7faacd433824e1fdd6f2608c6b
tens0rfl0w <maximilian.wittfeld@tu-dortmund.de>
2024-10-01 21:43:39 +0200

    fix(build/linux): WSL build adjustments
    - Update WSL build script to use new build stages
    - Remove 'svadhesive' from components if custom build

--- code/tools/ci/wsl-build.sh
+++ code/tools/ci/wsl-build.sh
@@ -15,18 +15,21 @@ ln -s $PWD/../ /src
 
 # check if we need to run initial setup steps
 if [ "$COMMIT" != "$LAST_COMMIT" ]; then
-	cp -a tools/ci/build_server_2.sh /tmp/setup_server.sh
-	sed -i '/^## SETUP-CUTOFF/,$ d' /tmp/setup_server.sh
+	cp -a tools/ci/docker-builder/proot_prepare.sh /tmp/setup_server.sh
 
 	SKIP_NATIVES=1 sh /tmp/setup_server.sh
 
 	echo -n "$COMMIT" > /.fxs_commit
 fi
 
-cp -a tools/ci/build_server_2.sh /tmp/build_server.sh
+cp -a tools/ci/build_server_proot_alpine.sh /tmp/build_server.sh
 sed -i -e '1,/^## SETUP-CUTOFF/d' -e '/^## BUILD-CUTOFF/,$ d' -e 's/.*make clean.*//g' -e 's/config=release/config=debug/g' -e 's/linux\/release/linux\/debug/g' /tmp/build_server.sh
 
 JOB_SLOTS=$(nproc) sh -e /tmp/build_server.sh
+
+if [ ! -f "/opt/cfx-server/libsvadhesive.so" ]; then
+    grep -v '"svadhesive"' "/opt/cfx-server/components.json" > tmp.json && mv tmp.json "/opt/cfx-server/components.json"
+fi
 
 echo '------------------'
 echo 'Finished building.'
