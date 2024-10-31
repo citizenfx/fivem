793d90bfbb6ac1db911af3b288b6f43b84486c0b
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-10-31 15:13:02 +0000

    tweak(cfx/nui): hide new convar `sv_replaceExeToSwitchBuilds`
    Revert "tweak(cfx/nui): hide new convar `sv_replaceExeToSwitchBuilds`"
    
    This reverts commit 4dcae821cdb23241f165188cd9d39d60bbf76751.
    
    tweak(cfx/nui): Centralize convar filtering logic with `shouldVarBeShown` helper

--- ext/cfx-ui/src/cfx/common/services/servers/transformers.ts
+++ ext/cfx-ui/src/cfx/common/services/servers/transformers.ts
@@ -187,6 +187,22 @@ function getSortableName(searchableName: string): string {
     .toLowerCase();
 }
 
+function shouldVarBeShown(key: string): boolean {
+  const convars: string[] = [
+    'sv_lan',
+    'mapname', 
+    'onesync', 
+    'gametype', 
+    'sv_maxClients',
+    'sv_poolSizesIncrease', 
+    'sv_enhancedHostSupport',  
+    'sv_disableClientReplays', 
+    'sv_replaceExeToSwitchBuilds',
+  ];
+
+  return !convars.includes(key);
+}
+
 type VarsView = Partial<
   Pick<
     IServerView,
@@ -294,21 +310,11 @@ export function processServerDataVariables(vars?: IServer['data']['vars']): Vars
       }
       case key === 'sv_pureLevel': {
         view.pureLevel = value as ServerPureLevel;
-
         continue;
       }
-
-      case key === 'sv_poolSizesIncrease':
-      case key === 'sv_disableClientReplays':
-      case key === 'onesync':
-      case key === 'gametype':
-      case key === 'mapname':
-      case key === 'sv_enhancedHostSupport':
-      case key === 'sv_lan':
-      case key === 'sv_maxClients': {
+      case !shouldVarBeShown(key): {
         continue;
       }
-
       case lckey.includes('banner_'):
       case lckey.includes('sv_project'):
       case lckey.includes('version'):
