c99424c59a9e387bf803db77a9c38f0df7cebd54
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-11-06 17:20:48 +0100

    feat(server): add token auth to profileData.json endpoint

--- code/components/citizen-server-impl/src/GameServer.cpp
+++ code/components/citizen-server-impl/src/GameServer.cpp
@@ -114,6 +114,7 @@ namespace fx
 
 		m_rconPassword = instance->AddVariable<std::string>("rcon_password", ConVar_ReadOnly, "");
 		m_playersToken = instance->AddVariable<std::string>("sv_playersToken", ConVar_None, "");
+		m_profileDataToken = instance->AddVariable<std::string>("sv_profileDataToken", ConVar_None, "");
 		m_hostname = instance->AddVariable<std::string>("sv_hostname", ConVar_ServerInfo, "default FXServer");
 		m_masters[0] = instance->AddVariable<std::string>("sv_master1", ConVar_None, kDefaultServerList);
 		m_masters[1] = instance->AddVariable<std::string>("sv_master2", ConVar_None, "");
