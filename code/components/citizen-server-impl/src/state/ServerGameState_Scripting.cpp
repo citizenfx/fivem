citizenfx/fivem

commit 8957209b72626bacd970f07412f3dff6ccac2bf0

tweak(server/state): fix logic dealing with setting state for trains

code/components/citizen-server-impl/src/state/ServerGameState_Scripting.cpp
@@ -264,14 +264,31 @@ static void Init()
 			throw std::runtime_error(va("Tried to access invalid entity: %d", id));
 		}
 
-		auto orphanMode = context.GetArgument<int>(1);
+		int rawOrphanMode = context.GetArgument<int>(1);
 
-		if (orphanMode < 0 || orphanMode > fx::sync::KeepEntity)
+		if (rawOrphanMode < 0 || rawOrphanMode > fx::sync::KeepEntity)
 		{
-			throw std::runtime_error(va("Tried to set entities (%d) orphan mode to an invalid orphan mode: %d", id, orphanMode));
+			throw std::runtime_error(va("Tried to set entities (%d) orphan mode to an invalid orphan mode: %d", id, rawOrphanMode));
 		}
 
-		entity->orphanMode = static_cast<fx::sync::EntityOrphanMode>(orphanMode);
+
+		fx::sync::EntityOrphanMode entityOrphanMode = static_cast<fx::sync::EntityOrphanMode>(rawOrphanMode);
+
+#ifdef STATE_FIVE
+		if (entity->type == fx::sync::NetObjEntityType::Train)
+		{
+			// recursively apply orphan mode to all of the trains children/parents
+			gameState->IterateTrainLink(entity, [entityOrphanMode](fx::sync::SyncEntityPtr& train) {
+				train->orphanMode = entityOrphanMode;
+
+				return true;
+			});
+		}
+		else
+#endif
+		{
+			entity->orphanMode = entityOrphanMode;
+		}
 
 		// if they set the orphan mode to `DeleteOnOwnerDisconnect` and the entity already doesn't have an owner then treat this as a `DELETE_ENTITY` call
 		if (entity->orphanMode == fx::sync::DeleteOnOwnerDisconnect && entity->firstOwnerDropped)
@@ -1281,6 +1298,18 @@ static void Init()
 		auto gameState = instance->GetComponent<fx::ServerGameState>();
 
 		gameState->DeleteEntity(entity);
+
+		return 0;
+	}));
+
+	fx::ScriptEngine::RegisterNativeHandler("DELETE_TRAIN", makeEntityFunction([](fx::ScriptContext& context, const fx::sync::SyncEntityPtr& entity)
+	{
+		auto resourceManager = fx::ResourceManager::GetCurrent();
+		auto instance = resourceManager->GetComponent<fx::ServerInstanceBaseRef>()->Get();
+		auto gameState = instance->GetComponent<fx::ServerGameState>();
+
+		// ignore the engine checks, this will recursively delete the entire train
+		gameState->DeleteEntity<true>(entity);
 
 		return 0;
 	}));

code/components/citizen-server-impl/include/state/ServerGameState.h
@@ -857,7 +857,6 @@ enum EntityOrphanMode : uint8_t
 	DeleteOnOwnerDisconnect = 1,
 	KeepEntity = 2,
 };
-
 struct SyncEntityState
 {
 	using TData = std::variant<int, float, bool, std::string>;
@@ -1357,6 +1356,10 @@ public:
 	
 	void GetFreeObjectIds(const fx::ClientSharedPtr& client, uint8_t numIds, std::vector<uint16_t>& freeIds);
 
+#ifdef STATE_FIVE
+	void IterateTrainLink(const sync::SyncEntityPtr& train, std::function<bool(sync::SyncEntityPtr&)> fn, bool callOnInitialEntity = true);
+#endif
+
 	void ReassignEntity(uint32_t entityHandle, const fx::ClientSharedPtr& targetClient, std::unique_lock<std::shared_mutex>&& lock = {});
 
 	bool SetEntityStateBag(uint8_t playerId, uint16_t objectId, std::function<std::shared_ptr<StateBag>()> createStateBag) override;
@@ -1371,7 +1374,51 @@ private:
 	void ReassignEntityInner(uint32_t entityHandle, const fx::ClientSharedPtr& targetClient, std::unique_lock<std::shared_mutex>&& lock = {});
 
 public:
-	void DeleteEntity(const fx::sync::SyncEntityPtr& entity);
+
+	template<bool IgnoreTrainChecks = false>
+	void DeleteEntity(const fx::sync::SyncEntityPtr& entity)
+	{
+		if (entity->type == sync::NetObjEntityType::Player || !entity->syncTree)
+		{
+			return;
+		}
+
+		// can only be used on FiveM, RDR doesn't have its sync nodes filled out
+#ifdef STATE_FIVE
+		// this will be ignored by DELETE_TRAIN so calling on any part of the train will delete the entire thing
+		if constexpr (!IgnoreTrainChecks)
+		{
+
+			if (entity->type == sync::NetObjEntityType::Train;
+				auto trainState = entity->syncTree->GetTrainState())
+			{
+				// don't allow the deletion of carriages until we can modify sync node data and overwrite the linked forward/linked backwards state
+				if (trainState->engineCarriage && trainState->engineCarriage != entity->handle)
+				{
+					return;
+				}
+			}
+		}
+#endif
+
+		gscomms_execute_callback_on_sync_thread([=]()
+		{
+#ifdef STATE_FIVE
+			if (entity->type == sync::NetObjEntityType::Train)
+			{
+				// recursively delete every part of the train
+				IterateTrainLink(entity, [=](fx::sync::SyncEntityPtr& train)
+				{
+					RemoveClone({}, train->handle);
+
+					return true;
+				});
+				return;
+			}
+#endif
+			RemoveClone({}, entity->handle);
+		});
+	}
 
 	void ClearClientFromWorldGrid(const fx::ClientSharedPtr& targetClient);
 

code/components/citizen-server-impl/src/state/ServerGameState.cpp
@@ -1166,34 +1166,16 @@ void ServerGameState::Tick(fx::ServerInstanceBase* instance)
 
 				if (auto engine = GetTrain(this, trainState->engineCarriage))
 				{
-					{
+					IterateTrainLink(entity, [&isRelevant, isRelevantViaPos](sync::SyncEntityPtr& train) {
 						float position[3];
-						engine->syncTree->GetPosition(position);
+						train->syncTree->GetPosition(position);
 
 						glm::vec3 entityPosition(position[0], position[1], position[2]);
-						if (isRelevantViaPos(engine, entityPosition))
-						{
-							isRelevant = true;
-						}
-					}
+						isRelevant = isRelevantViaPos(train, entityPosition);
 
-					// if not via the engine, try the next-train chain
-					if (!isRelevant)
-					{
-						for (auto link = GetNextTrain(this, engine); link; link = GetNextTrain(this, link))
-						{
-							float position[3];
-							link->syncTree->GetPosition(position);
-
-							glm::vec3 entityPosition(position[0], position[1], position[2]);
-
-							if (isRelevantViaPos(link, entityPosition))
-							{
-								isRelevant = true;
-								break;
-							}
-						}
-					}
+						// if we're not still relevant then we should keep going
+						return !isRelevant;
+					});
 				}
 			}
 #endif
@@ -2645,6 +2627,65 @@ void ServerGameState::ReassignEntityInner(uint32_t entityHandle, const fx::Clien
 	}
 }
 
+#ifdef STATE_FIVE
+/// <summary>
+/// Takes the initialTrain and if its the engine train, iterates down the train link from the train, if it's not then it will try to get the engine
+/// </summary>
+/// <param name="initialTrain">The initial to start the iteration from, this will get the engine entity internally, if the engine doesn't exist it will early return as there's no valid part in the link to start from.</param>
+/// <param name="fn">The function to call, if the function returns `true` it will keep iterating, if it returns `false` it will stop</param>
+/// <param name="callOnInitialEntity">Whether the function should do the `fn` call on the initialTrain</param>
+void ServerGameState::IterateTrainLink(const sync::SyncEntityPtr& initialTrain, std::function<bool(sync::SyncEntityPtr&)> fn, bool callOnInitialEntity)
+{
+	// for most stuff we want to call on the intial entity
+	if (callOnInitialEntity)
+	{
+		if (!fn(const_cast<sync::SyncEntityPtr&>(initialTrain)))
+		{
+			return;
+		}
+	}
+
+	if (auto trainState = initialTrain->syncTree->GetTrainState())
+	{
+		auto recurseTrain = [=](const fx::sync::SyncEntityPtr& train)
+		{
+			for (auto link = GetNextTrain(this, train); link; link = GetNextTrain(this, link))
+			{
+				// this is expected to make sure that the initial train & the link trains are not called twice
+				// since this could lead to double locking the client mutex in ReassignEntity
+				// we also ignore the train sent via the call to `recurseTrain` as we should've called `fn` before here
+				if (link->handle == initialTrain->handle)
+				{
+					continue;
+				}
+
+				// if the function returns true then we should stop iterating
+				if (!fn(link))
+				{
+					return;
+				}
+			}
+		};
+
+		if (trainState->isEngine)
+		{
+			recurseTrain(initialTrain);
+		}
+		else if (trainState->engineCarriage && trainState->engineCarriage != initialTrain->handle)
+		{
+			if (auto engine = GetTrain(this, trainState->engineCarriage))
+			{
+				if (!fn(engine))
+				{
+					return;
+				}
+				recurseTrain(engine);
+			}
+		}
+	}
+}
+#endif
+
 void ServerGameState::ReassignEntity(uint32_t entityHandle, const fx::ClientSharedPtr& targetClient, std::unique_lock<std::shared_mutex>&& lock)
 {
 	ReassignEntityInner(entityHandle, targetClient, std::move(lock));
@@ -2657,39 +2698,19 @@ void ServerGameState::ReassignEntity(uint32_t entityHandle, const fx::ClientShar
 		// game code works as follows:
 		// -> if train isEngine, enumerate the entire list backwards and migrate that one along
 		// -> if not isEngine, migrate the engine
-		if (auto trainState = train->syncTree->GetTrainState())
-		{
-			auto reassignEngine = [this, &targetClient, entityHandle](const fx::sync::SyncEntityPtr& train)
-			{
-				for (auto link = GetNextTrain(this, train); link; link = GetNextTrain(this, link))
-				{
-					// this check should prevent the following two states:
-					// 1. double-locking clientMutex
-					// 2. reassigning the same entity twice
-					if (link->handle != entityHandle)
-					{
-						// we directly use ReassignEntityInner here to ensure no infinite recursion
-						ReassignEntityInner(link->handle, targetClient);
-					}
-				}
-			};
 
-			if (trainState->isEngine)
-			{
-				reassignEngine(train);
-			}
-			else if (trainState->engineCarriage && trainState->engineCarriage != entityHandle)
-			{
-				// reassign the engine carriage
-				ReassignEntityInner(trainState->engineCarriage, targetClient);
 
-				// get the engine and reassign based on that
-				if (auto engine = GetTrain(this, trainState->engineCarriage))
-				{
-					reassignEngine(engine);
-				}
-			}
-		}
+		// This call expects link-handle != entityHandle
+		// This will prevent
+		// 1. double-locking clientMutex
+		// 2. reassigning the same entity twice
+		IterateTrainLink(train, [=](const fx::sync::SyncEntityPtr& link) {
+
+			// we directly use ReassignEntityInner here to ensure no infinite recursion
+			ReassignEntityInner(link->handle, targetClient);
+
+			return true;
+		}, false);
 	}
 #endif
 }
@@ -4399,16 +4420,6 @@ void ServerGameState::HandleGameStateAck(fx::ServerInstanceBase* instance, const
 	}
 }
 
-void ServerGameState::DeleteEntity(const fx::sync::SyncEntityPtr& entity)
-{
-	if (entity->type != sync::NetObjEntityType::Player && entity->syncTree)
-	{
-		gscomms_execute_callback_on_sync_thread([=]() 
-		{
-			RemoveClone({}, entity->handle);
-		});
-	}
-}
 
 void ServerGameState::SendPacket(int peer, net::packet::StateBagPacket& packet)
 {

commit 8560a4ab56cef69439a4d02c93021579722e998b

fix(state): fix not being able to use any native calls in 'entityRemoved'
Fixes an oversight introduced in 8957209b72626bacd970f07412f3dff6ccac2bf0

code/components/citizen-server-impl/src/state/ServerGameState_Scripting.cpp
@@ -20,8 +20,11 @@ void DisownEntityScript(const fx::sync::SyncEntityPtr& entity);
 static void Init()
 {
 
+
+	// If the entity is in its deleting/finalizing state we should not allow access to them
+	// This should only be used for natives that are not expected to work when an entity is actively being deleted
+	// i.e. setters, getting entity by network id / does exist, or any pool getter natives
 	static auto IsEntityValid = [](const fx::sync::SyncEntityPtr& entity) {
-		// if we're deleting or finalizing our deletion then we don't want to be included in the list
 		return entity && !entity->deleting && !entity->finalizing;
 	};
 
@@ -49,7 +52,7 @@ static void Init()
 
 			auto entity = gameState->GetEntity(id);
 
-			if (!IsEntityValid(entity))
+			if (!entity)
 			{
 				throw std::runtime_error(va("Tried to access invalid entity: %d", id));
 