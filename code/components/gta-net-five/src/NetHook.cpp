68b06d04e4ee45f6b3153b2706f431c528365987
Hamidreza Anbaraki <anbarakihamidreza@gmail.com>
2025-02-28 12:06:15 +0330

    tweak(gta-net-five): remove 'reason' argument from quit command

--- code/components/gta-net-five/src/NetHook.cpp
+++ code/components/gta-net-five/src/NetHook.cpp
@@ -1436,7 +1436,7 @@ static HookFunction hookFunction([] ()
 
 	static ConsoleCommand quitCommand("quit", [](const std::string& message)
 	{
-		g_quitMsg = "Quit: " + message;
+		g_quitMsg = "Quit";
 		ExitProcess(-1);
 	});
 
