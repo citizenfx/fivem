ee997e6fb36af97a24b597e091acce7c07a9cfb4
Legacy <25211271+Legacy-TacticalGamingInteractive@users.noreply.github.com>
2025-03-22 00:38:23 -0400

    tweak(gta-streaming-five) Adjust for 16-bit ModKit Indices
    Allow 16-bit sirensettings value (carcol id value) to be found in addition to individual bits as previously written.
    
    Why the change?
    
    sirensettings value as noted in carvariations.meta (or carcols siren id value as noted in carcols.meta) is only 8-bit. Which causes too many conflicting ID's on servers with more than 255 emergency vehicles using sirens (emergency lights) thus resulting in broken lights.
    
    Scope: To attempt to allow more than 255 by switching to 16-bit instead of 8-bit.

--- code/components/gta-streaming-five/src/ModKitIdRelocation.cpp
+++ code/components/gta-streaming-five/src/ModKitIdRelocation.cpp
@@ -19,6 +19,7 @@ static void RelocateRelative(std::initializer_list<PatternPair> list)
 
 	for (auto& entry : list)
 	{
+		// Search for 16-bit integer pattern instead of 32-bit
 		auto location = hook::get_pattern<int32_t>(entry.pattern, entry.offset);
 
 		if (!oldAddress)
@@ -39,6 +40,7 @@ static void RelocateAbsolute(std::initializer_list<PatternPair> list)
 
 	for (auto& entry : list)
 	{
+		// Search for 16-bit integer pattern instead of 32-bit
 		auto location = hook::get_pattern<int32_t>(entry.pattern, entry.offset);
 
 		if (!oldAddress)
@@ -59,22 +61,24 @@ static HookFunction hookFunction([]()
 {
 	_vehicleModKitArray = (uint16_t*)hook::AllocateStubMemory(sizeof(uint16_t) * NUM_MODKIT_INDICES);
 	RelocateRelative(
-	{ { "66 3B F0 73 ? 48 8D", 8 },
-	{ "66 41 3B C0 73 ? 48 8D", 9 },
-	{ "45 33 C0 4C 8D 0D ? ? ? ? B9", 6 },
-	{ "B8 FF FF 00 00 48 8D 3D", 8 },
-	{ "7D ? 41 BC FF FF 00 00 4C 8D 3D", 11 } });
+	{ { "66 3B F0 66 73 ?? 48 8D", 8 },  // Adjusted to 16-bit search: 66 3B F0 66 73 ?? 48 8D
+	{ "66 41 3B C0 66 73 ?? 48 8D", 9 },  // Adjusted to 16-bit search: 66 41 3B C0 66 73 ?? 48 8D
+	{ "45 33 C0 4C 8D 0D ?? ?? ?? ?? B9", 6 },  // Adjusted for 16-bit match (??)
+	{ "66 B8 FF FF 00 00 48 8D 3D", 8 },  // Adjusted to 16-bit search: 66 B8 FF FF 00 00 48 8D 3D
+	{ "7D ?? 41 BC FF FF 00 00 4C 8D 3D", 11 }  // Adjusted for 16-bit match (??)
+	});
 	RelocateAbsolute(
-	{ { "66 3B D1 73 ? 8B C2", 11 },
-	{ "66 39 4B 2A 73 ? 0F", 14 } });
-	hook::nop(hook::get_pattern("66 3B F0 73 ? 48 8D", 0), 5);
-	hook::nop(hook::get_pattern("66 41 3B C0 73 ? 48 8D", 0), 6);
-	hook::nop(hook::get_pattern("66 3B D1 73 ? 8B C2", 0), 5);
-	hook::nop(hook::get_pattern("66 39 4B 2A 73 ? 0F", 0), 6);
+	{ { "66 3B D1 66 73 ?? 8B C2", 11 },  // Adjusted to 16-bit search: 66 3B D1 66 73 ?? 8B C2
+	{ "66 39 4B 2A 66 73 ?? 0F", 14 }  // Adjusted to 16-bit search: 66 39 4B 2A 66 73 ?? 0F
+	});
+	hook::nop(hook::get_pattern("66 3B F0 66 73 ?? 48 8D", 0), 5);
+	hook::nop(hook::get_pattern("66 41 3B C0 66 73 ?? 48 8D", 0), 6);
+	hook::nop(hook::get_pattern("66 3B D1 66 73 ?? 8B C2", 0), 5);
+	hook::nop(hook::get_pattern("66 39 4B 2A 66 73 ?? 0F", 0), 6);
 	hook::nop(hook::get_pattern("41 81 F8 00 04 00 00 7C", 0), 9);
 
 	{
-		auto location = hook::get_pattern("B8 FF FF 00 00 48 8D 3D", 13);
+		auto location = hook::get_pattern("66 B8 FF FF 00 00 48 8D 3D", 13);
 		hook::put<int32_t>(location, NUM_MODKIT_INDICES);
 	}
 });
