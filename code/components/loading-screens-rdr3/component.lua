0e54383af679ad43f569e5547fa089b7cecb2412
Ktoś <43894510+Ktos93@users.noreply.github.com>
2025-04-13 16:09:49 +0200

    feat(rdr3): loading screen support

---
+++ code/components/loading-screens-rdr3/component.lua
@@ -0,0 +1,8 @@
+return function()
+	filter {}
+
+	files {
+		'components/loading-screens-five/src/HookLoadingScreens.cpp',
+        'components/loading-screens-five/src/LoadingScreens.cpp',
+	}
+end
