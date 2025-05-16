b23bfe37d86cb84a7e146d8679d478fe6c2c4bf6
Niels <pindakaastosti111@gmail.com>
2025-05-16 11:34:42 +0200

    tweak(graphics/rdr3): print out enum + description on failure

--- code/components/rage-graphics-rdr3/src/RenderHooks.cpp
+++ code/components/rage-graphics-rdr3/src/RenderHooks.cpp
@@ -7,7 +7,12 @@
 
 #include <CoreConsole.h>
 
+#include <VulkanHelper.h>
+#include <CrossBuildRuntime.h>
+
 #pragma comment(lib, "vulkan-1.lib")
+
+static bool g_enableVulkanValidation = false;
 
 // Function to print the output of the validation layers
 VKAPI_ATTR VkBool32 VKAPI_CALL DebugMessageCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
@@ -31,11 +36,11 @@ VKAPI_ATTR VkBool32 VKAPI_CALL DebugMessageCallback(VkDebugUtilsMessageSeverityF
 		prefix = "ERROR: ";
 	}
 
-	trace("%s [%i][%s]: %s\n", prefix, pCallbackData->messageIdNumber, pCallbackData->pMessageIdName, pCallbackData->pMessage);
+	trace("%s[%i][%s]: %s\n", prefix, pCallbackData->messageIdNumber, pCallbackData->pMessageIdName, pCallbackData->pMessage);
 	return VK_FALSE;
 }
 
-static inline std::string GetGPUType(VkPhysicalDeviceType type)
+static inline std::string_view GetGPUType(VkPhysicalDeviceType type)
 {
 	switch (type)
 	{
@@ -72,120 +77,132 @@ static inline bool IsDedicatedGPU(VkPhysicalDeviceType type)
 static VkResult __stdcall vkCreateInstanceHook(VkInstanceCreateInfo* pCreateInfo, VkAllocationCallbacks* pAllocator, VkInstance* pInstance)
 {
 	// the validation layers should be disabled when playing, but enabled when encountering a crash
-	// if we disable these now, we fallback to dx12 for some reason
-#if 0
-	std::vector<const char*> originalLayers(pCreateInfo->enabledLayerCount + 1);
+	// we keep the data here, to ensure the data does not go out of scope
+	std::vector<const char*> originalLayers(pCreateInfo->enabledLayerCount);
+	std::vector<const char*> originalExtensions(pCreateInfo->enabledExtensionCount);
 
-	for (size_t i = 0; i < originalLayers.size() - 1; i++)
+	if (g_enableVulkanValidation)
 	{
-		originalLayers[i] = pCreateInfo->ppEnabledLayerNames[i];
-		trace("Vulkan Instance layers: %s\n", originalLayers[i]);
+
+		for (size_t i = 0; i < originalLayers.size(); i++)
+		{
+			originalLayers[i] = pCreateInfo->ppEnabledLayerNames[i];
+			trace("Vulkan Instance layers: %s\n", originalLayers[i]);
+		}
+
+		originalLayers.push_back("VK_LAYER_KHRONOS_validation");
+
+		pCreateInfo->enabledLayerCount = static_cast<uint32_t>(originalLayers.size());
+		pCreateInfo->ppEnabledLayerNames = originalLayers.data();
+
+		for (size_t i = 0; i < originalExtensions.size(); i++)
+		{
+			originalExtensions[i] = pCreateInfo->ppEnabledExtensionNames[i];
+			trace("Vulkan Instance Extensions: %s\n", originalExtensions[i]);
+		}
+
+		originalExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+		pCreateInfo->enabledExtensionCount = static_cast<uint32_t>(originalExtensions.size());
+		pCreateInfo->ppEnabledExtensionNames = originalExtensions.data();
+
+		VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = {};
+
+		debugCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		debugCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		debugCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		debugCreateInfo.pfnUserCallback = DebugMessageCallback;
+
+		pCreateInfo->pNext = &debugCreateInfo;
 	}
 
-	originalLayers.push_back("VK_LAYER_KHRONOS_validation");
-
-	pCreateInfo->enabledLayerCount = static_cast<uint32_t>(originalLayers.size());
-	pCreateInfo->ppEnabledLayerNames = originalLayers.data();
-
-	std::vector<const char*> originalExtensions(pCreateInfo->enabledExtensionCount + 1);
-
-	for (size_t i = 0; i < originalExtensions.size() - 1; i++)
-	{
-		originalExtensions[i] = pCreateInfo->ppEnabledExtensionNames[i];
-		trace("Vulkan Instance Extensions: %s\n", originalExtensions[i]);
-	}
-
-	originalExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-
-	pCreateInfo->enabledExtensionCount = static_cast<uint32_t>(originalExtensions.size());
-	pCreateInfo->ppEnabledExtensionNames = originalExtensions.data();
-
-	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = {};
-
-	debugCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-	debugCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-	debugCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-	debugCreateInfo.pfnUserCallback = DebugMessageCallback;
-
-	pCreateInfo->pNext = &debugCreateInfo;
-#endif
 	VkResult result = vkCreateInstance(pCreateInfo, pAllocator, pInstance);
 
 	if (result != VK_SUCCESS)
 	{
-		trace("Vulkan instance creation returned: %i\nSee the vulkan docs for the error code (VkResult)\n", result);
+		trace("Vulkan instance creation returned: %s\n", ResultToString(result));
 	}
 
 	// we let rage handle the error (probably defaults back to dx12)
 	return result;
 }
 
-static HRESULT vkCreateDeviceHook(VkPhysicalDevice physicalDevice, VkDeviceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDevice* pDevice)
+static inline void SetVulkanCrashometry(const VkPhysicalDevice physicalDevice)
 {
-	// Disabled for now
-#if 0
-	// force validation layers for vulkan, but dont replace the original layers
-	std::vector<const char*> originalLayers(pCreateInfo->enabledLayerCount + 1);
+	static bool isInitialized = false;
 
-	for (size_t i = 0; i < originalLayers.size() - 1; i++)
+	if (isInitialized)
+		return;
+
+	VkPhysicalDeviceProperties props = {};
+
+	vkGetPhysicalDeviceProperties(physicalDevice, &props);
+
+	// version encoding: https://registry.khronos.org/vulkan/specs/latest/html/vkspec.html#extendingvulkan-coreversions-versionnumbers
+	uint32_t major = static_cast<uint32_t>(props.apiVersion) >> 22U;
+	uint32_t minor = (static_cast<uint32_t>(props.apiVersion) >> 12U) & 0x3FFU;
+	uint32_t patch = static_cast<uint32_t>(props.apiVersion) & 0xFFFU;
+
+	AddCrashometry("gpu_name", "%s", props.deviceName);
+	AddCrashometry("gpu_id", "%04x:%04x", props.vendorID, props.deviceID);
+	AddCrashometry("vulkan_api_version", "v%u.%u.%u", major, minor, patch);
+	AddCrashometry("validation_layers_enabled", "%i", static_cast<int>(g_enableVulkanValidation));
+
+	// driver version encoding: https://github.com/SaschaWillems/vulkan.gpuinfo.org/blob/1e6ca6e3c0763daabd6a101b860ab4354a07f5d3/functions.php#L298-L321
+	// vendor ids: https://www.reddit.com/r/vulkan/comments/4ta9nj/is_there_a_comprehensive_list_of_the_names_and/
+	// NVIDIA
+	if (props.vendorID == 0x10DE)
 	{
-		originalLayers[i] = pCreateInfo->ppEnabledLayerNames[i];
-		trace("Vulkan Device Layer: %s", originalLayers[i]);
+		AddCrashometry("nvidia_driver", "v%u.%u.%u.%u", (props.driverVersion >> 22) & 0x3ff, (props.driverVersion >> 14) & 0x0ff, (props.driverVersion >> 6) & 0x0ff, (props.driverVersion) & 0x003f);
+	}
+	// INTEL
+	else if (props.vendorID == 0x8086)
+	{
+		AddCrashometry("intel_driver", "v%u.%u", (props.driverVersion >> 14), (props.driverVersion) & 0x3fff);
+	}
+	else
+	{
+		AddCrashometry("driver", "v%u.%u.%u", (props.driverVersion >> 22), (props.driverVersion >> 12) & 0x3ff, props.driverVersion & 0xfff);
 	}
 
-	originalLayers.push_back("VK_LAYER_KHRONOS_validation");
+	trace("GPU Name: %s\nGPU type: %s\nDriver version: %u\nAPI version: %u.%u.%u\n", props.deviceName, GetGPUType(props.deviceType), props.driverVersion, major, minor, patch);
 
-	pCreateInfo->enabledLayerCount = static_cast<uint32_t>(originalLayers.size());
-	pCreateInfo->ppEnabledLayerNames = originalLayers.data();
-#endif
-
-	static auto _ = ([&physicalDevice]
+	if (!IsDedicatedGPU(props.deviceType))
 	{
-		VkPhysicalDeviceProperties props = {};
+		trace("[WARNING] RedM is using a non dedicated GPU. The GPU type that is in use is: %s", GetGPUType(props.deviceType));
+	}
 
-		vkGetPhysicalDeviceProperties(physicalDevice, &props);
+	isInitialized = true;
+}
 
-		// version encoding: https://registry.khronos.org/vulkan/specs/latest/html/vkspec.html#extendingvulkan-coreversions-versionnumbers
-		uint32_t major = static_cast<uint32_t>(props.apiVersion) >> 22U;
-		uint32_t minor = (static_cast<uint32_t>(props.apiVersion) >> 12U) & 0x3FFU;
-		uint32_t patch = static_cast<uint32_t>(props.apiVersion) & 0xFFFU;
+static HRESULT vkCreateDeviceHook(VkPhysicalDevice physicalDevice, VkDeviceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDevice* pDevice)
+{
+	// we keep the data here, to ensure the data does not go out of scope
+	std::vector<const char*> originalLayers(pCreateInfo->enabledLayerCount);
 
-		AddCrashometry("gpu_name", "%s", props.deviceName);
-		AddCrashometry("gpu_id", "%04x:%04x", props.vendorID, props.deviceID);
-		AddCrashometry("vulkan_api_version", "v%u.%u.%u", major, minor, patch);
+	if (g_enableVulkanValidation)
+	{
+		// force validation layers for vulkan, but dont replace the original layers
 
-		// driver version encoding: https://github.com/SaschaWillems/vulkan.gpuinfo.org/blob/1e6ca6e3c0763daabd6a101b860ab4354a07f5d3/functions.php#L298-L321
-		// vendor ids: https://www.reddit.com/r/vulkan/comments/4ta9nj/is_there_a_comprehensive_list_of_the_names_and/
-		// NVIDIA
-		if (props.vendorID == 0x10DE)
+		for (size_t i = 0; i < originalLayers.size(); i++)
 		{
-			AddCrashometry("nvidia_driver", "v%u.%u.%u.%u", (props.driverVersion >> 22) & 0x3ff, (props.driverVersion >> 14) & 0x0ff, (props.driverVersion >> 6) & 0x0ff, (props.driverVersion) & 0x003f);
-		}
-		// INTEL
-		else if (props.vendorID == 0x8086)
-		{
-			AddCrashometry("intel_driver", "v%u.%u", (props.driverVersion >> 14), (props.driverVersion) & 0x3fff);
-		}
-		else
-		{
-			AddCrashometry("driver", "v%u.%u.%u", (props.driverVersion >> 22), (props.driverVersion >> 12) & 0x3ff, props.driverVersion & 0xfff);
+			originalLayers[i] = pCreateInfo->ppEnabledLayerNames[i];
+			trace("Vulkan Device Layer: %s", originalLayers[i]);
 		}
 
-		trace("GPU Name: %s\nGPU type: %s\nDriver version: %u\nAPI version: %u.%u.%u\n", props.deviceName, GetGPUType(props.deviceType).c_str(), props.driverVersion, major, minor, patch);
+		originalLayers.push_back("VK_LAYER_KHRONOS_validation");
 
-		if (!IsDedicatedGPU(props.deviceType))
-		{
-			console::DPrintf("Vulkan GPU selection", "[WARNING] RedM is using a non dedicated GPU. The GPU type that is in use is: %s", GetGPUType(props.deviceType));
-		}
+		pCreateInfo->enabledLayerCount = static_cast<uint32_t>(originalLayers.size());
+		pCreateInfo->ppEnabledLayerNames = originalLayers.data();
+	}
 
-		return true;
-	})();
+	SetVulkanCrashometry(physicalDevice);
 
 	VkResult result = vkCreateDevice(physicalDevice, pCreateInfo, pAllocator, pDevice);
 
 	if (result != VK_SUCCESS)
 	{
-		trace("Vulkan device creation returned: %i\nSee the vulkan docs for the error code (VkResult)", result);
+		trace("Vulkan device creation returned: %s\n", ResultToString(result));
 	}
 
 	return result;
@@ -193,6 +210,9 @@ static HRESULT vkCreateDeviceHook(VkPhysicalDevice physicalDevice, VkDeviceCreat
 
 static HookFunction hookFunction([]()
 {
+	std::wstring fpath = MakeRelativeCitPath(L"CitizenFX.ini");
+	g_enableVulkanValidation = static_cast<bool>(GetPrivateProfileInt(L"Game", L"EnableVulkanValidation", 0, fpath.c_str()));
+
 	// Vulkan create instance hook
 	{
 		auto location = hook::get_pattern("FF D0 8B F0 89 84 24", -7);
