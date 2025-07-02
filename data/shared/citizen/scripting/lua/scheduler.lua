c3665287a23a60ee65eea91e5828a4c4088b13f0
Leonardo Emanuele <manups4e@gmail.com>
2025-07-02 10:15:05 +0200

    feat(clrcore-v2): use lua provided functions

--- data/shared/citizen/scripting/lua/scheduler.lua
+++ data/shared/citizen/scripting/lua/scheduler.lua
@@ -599,7 +599,7 @@ funcref_mt = msgpack.extend({
 
 			rvs[1].__cfx_async_retval(function(r, e)
 				if r then
-					p:resolve(table.pack(r))
+					p:resolve(table_pack(r))
 				elseif e then
 					p:reject(e)
 				end
