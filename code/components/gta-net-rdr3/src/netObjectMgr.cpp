42a33bf452e6dcb35dcd3b08e1d00f43f3f0624f
Ehbw <ehbwwww@gmail.com>
2025-07-04 11:48:52 +0100

    tweak(net/rdr3): no-op unused remote obj update
    This function makes use of 32-sized arrays that aren't given info in onesync, similar to UpdateAllNetworkObjects

--- code/components/gta-net-rdr3/src/netObjectMgr.cpp
+++ code/components/gta-net-rdr3/src/netObjectMgr.cpp
@@ -38,9 +38,21 @@ static void netObjectMgr__updateAllNetworkObjects(void* objMgr)
 	g_updateAllNetworkObjects(objMgr);
 }
 
+static void (*g_updateAllRemoteObjects)(void*);
+static void netObjectMgr__updateAllRemoteObjects(void* objMgr)
+{
+	if (icgi->OneSyncEnabled)
+	{
+		return;
+	}
+
+	g_updateAllRemoteObjects(objMgr);
+}
+
 static HookFunction hookFunction([]()
 {
 	g_objectMgr = hook::get_address<rage::netObjectMgr**>(hook::get_pattern("45 0F 57 C0 48 8B 35 ? ? ? ? 0F 57 FF", 7));
-	// Don't run function in onesync. (playerObjects is 32-sized and isn't given information in onesync)
-	g_updateAllNetworkObjects = hook::trampoline(hook::get_call(hook::get_pattern("E8 ? ? ? ? 44 8B 35 ? ? ? ? 33 C0")), netObjectMgr__updateAllNetworkObjects);
+	// Don't run legacy update functions in onesync. (playerObjects is 32-sized and isn't given information in onesync)
+	g_updateAllNetworkObjects = hook::trampoline(hook::get_pattern("4C 8B F9 89 BC 24", -34), netObjectMgr__updateAllNetworkObjects);
+	g_updateAllRemoteObjects = hook::trampoline(hook::get_pattern("48 8B FA 48 8B F1 E8 ? ? ? ? 45 33 FF 44 89 BD", -42), netObjectMgr__updateAllRemoteObjects);
 });
