7f0e4f621aee2c5a6edd6081dec58ee35a47d243
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2025-07-11 16:07:53 +0200

    feat(rage-device-rd3): filter relative devices files

--- code/components/rage-device-rdr3/component.lua
+++ code/components/rage-device-rdr3/component.lua
@@ -1,5 +1,8 @@
 return function()
 	filter {}
-	files "components/rage-device-five/src/CitizenMount.Shared.cpp"
+	files {
+		"components/rage-device-five/src/CitizenMount.Shared.cpp",
+		"components/rage-device-five/src/CitizenMount.cpp"
+	}
 	add_dependencies { 'vendor:minhook', 'vendor:openssl_crypto' }
 end
