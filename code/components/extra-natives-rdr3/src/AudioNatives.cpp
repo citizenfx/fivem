5cd11fc057ea36b1810706fd9843d43d9994fbaa
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2025-09-28 18:30:47 +0200

    feat(extra-natives-rdr3): add `SET_DISABLE_DEATH_AUDIO_SCENE` native

---
+++ code/components/extra-natives-rdr3/src/AudioNatives.cpp
@@ -0,0 +1,110 @@
+#include "StdInc.h"
+
+#include "fxScripting.h"
+#include "Hooking.Patterns.h"
+#include "Resource.h"
+#include "ScriptEngine.h"
+
+static bool g_DISABLE_DEATH_AUDIO_SCENE = false;
+
+static HookFunction hookFunction([]()
+{
+	// Disable the audDynamicMixer::StartScene calls to prevent audio from cutting out on death.
+	{
+		// 0x48A61B5F, ambient sounds
+		{
+			auto location = hook::get_pattern<char>("C7 45 ? ? ? ? ? E8 ? ? ? ? 48 8D 4D ? E8 ? ? ? ? 8A 83 ? ? ? ? 48 8B 0D ? ? ? ? BF");
+			auto failLocation = location + 34;
+			
+			static struct : jitasm::Frontend
+			{
+				uintptr_t successLocation;
+				uintptr_t failLocation;
+
+				void Init(uintptr_t success, uintptr_t fail)
+				{
+					successLocation = success;
+					failLocation = fail;
+				}
+				
+				virtual void InternalMain() override
+				{
+					mov(dword_ptr[rbp+0x20], 0x48A61B5F);
+
+					mov(rax, (uintptr_t)&g_DISABLE_DEATH_AUDIO_SCENE);
+					movzx(eax, byte_ptr[rax]);
+					test(eax, eax);
+					jz("success");
+
+					mov(rax, failLocation);
+					jmp(rax);
+
+					L("success");
+					mov(rax, successLocation);
+					jmp(rax);
+				}
+			} stub;
+			
+			stub.Init((uintptr_t)location + 0x7, (uintptr_t)failLocation);
+			hook::nop(location, 0x7);
+			hook::jump(location, stub.GetCode());
+		}
+
+		// 0x8B8B8CB1, related to scripted sounds?
+		{
+			auto location = hook::get_pattern("BA ? ? ? ? 49 89 73");
+			auto failLocation = hook::get_pattern("48 8D 55 ? 0F 29 45 ? 48 8B CB E8 ? ? ? ? 4C 8D 9C 24", 16);
+			
+			static struct : jitasm::Frontend
+			{
+				uintptr_t successLocation;
+				uintptr_t failLocation;
+
+				void Init(uintptr_t success, uintptr_t fail)
+				{
+					successLocation = success;
+					failLocation = fail;
+				}
+				
+				virtual void InternalMain() override
+				{
+					mov(edx, 0x8B8B8CB1);
+
+					mov(rax, (uintptr_t)&g_DISABLE_DEATH_AUDIO_SCENE);
+					movzx(eax, byte_ptr[rax]);
+					test(eax, eax);
+					jz("success");
+
+					mov(rax, failLocation);
+					jmp(rax);
+
+					L("success");
+					mov(rax, successLocation);
+					jmp(rax);
+				}
+			} stub;
+			
+			stub.Init((uintptr_t)location + 0x5, (uintptr_t)failLocation);
+			hook::nop(location, 0x5);
+			hook::jump(location, stub.GetCode());
+		}
+	}
+
+	fx::ScriptEngine::RegisterNativeHandler("SET_DISABLE_DEATH_AUDIO_SCENE", [](fx::ScriptContext& context) 
+	{
+		bool value = context.GetArgument<bool>(0);
+
+		g_DISABLE_DEATH_AUDIO_SCENE = value;
+
+		fx::OMPtr<IScriptRuntime> runtime;
+		if (FX_SUCCEEDED(fx::GetCurrentScriptRuntime(&runtime)))
+		{
+			fx::Resource* resource = reinterpret_cast<fx::Resource*>(runtime->GetParentObject());
+
+			resource->OnStop.Connect([]()
+			{
+				g_DISABLE_DEATH_AUDIO_SCENE = false;
+			});
+		}
+	});
+});
