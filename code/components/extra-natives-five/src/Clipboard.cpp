a25cefae8861d76641d4547d24eadcf9165d9be5
Till Schreiber <till@tillschreiber.de>
2026-05-24 23:55:42 +0200

    feat(extra-natives-five): Add COPY_TEXT_TO_CLIPBOARD

---
+++ code/components/extra-natives-five/src/Clipboard.cpp
@@ -0,0 +1,44 @@
+#include "StdInc.h"
+
+#include <ScriptEngine.h>
+
+static InitFunction initFunction([]()
+{	
+	fx::ScriptEngine::RegisterNativeHandler("COPY_TEXT_TO_CLIPBOARD", [](fx::ScriptContext& context)
+	{
+		std::string text = context.GetArgument<const char*>(0);
+		
+		if (text.empty())
+		{
+			return;
+		}
+		
+		const size_t textSize = text.size() + 1;
+		
+		if (!OpenClipboard(nullptr))
+		{
+			return;
+		}
+		
+		EmptyClipboard();
+		
+		HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, textSize);
+		if (hMem)
+		{
+			char* ptr = static_cast<char*>(GlobalLock(hMem));
+
+			if (ptr)
+			{
+				memcpy(ptr, text.c_str(), textSize);
+
+				GlobalUnlock(hMem);
+				SetClipboardData(CF_TEXT, hMem);
+			}
+			else
+			{
+				GlobalFree(hMem);
+			}
+		}
+		CloseClipboard();
+	});
+});
